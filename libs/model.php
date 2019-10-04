0d94df6669571fb58c2dcc7e7e313d8e5ffd6189
tjaitil <kjetil@baksaas.no>
2019-10-04 15:21:53 +0200

    More functions to model

--- libs/model.php
+++ libs/model.php
@@ -1,6 +1,10 @@
 <?php
     class model {
         protected $db;
+        protected $UpdateGamedata;
+        protected $artefactModel;
+        // $updateGamedata is the variable that holds updateGamedata_model if it is being instantiated with $this->loadModel from foo_model
+        // $artefactModel is the variable that holds updateGamedata_model if it is being instantiated with $this->loadModel from foo_model
         function __construct() {
             $this->includeDB();
         }
@@ -15,14 +19,23 @@
                 $_SESSION['gamedata']['log'] = array_values($_SESSION['gamedata']['log']);
             }
         }
-        protected function reportError($file, $line, $error_message) {
-            $message = "Error on: " . $file . ' Line ' . $line . ' ' . $error_message;
+        protected function reportError($file, $line, $error_message, $ajax = true) {
+            $message = "Error on: " . $file . ' Line ' . $line . ' ' . $error_message . ' ' . $ajax;
             $test = mail('miner123@hotmail.no', 'ERROR', $message, 'FROM: <system@adventurous.no');
         }
         protected function includeDB() {
             if(!isset($this->db)) {
                 /*require_once();*/
                 $this->db = new database();
+            }
+        }
+        protected function loadModel($model, $directoryUP = true) {
+            $model = $model . '_model';
+            if(class_exists($model)) {
+                return new $model($this->session);
+            }
+            else {
+                $this->reportError('model', false, 'model not found' . $model, false);
             }
         }
     }
