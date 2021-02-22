915a8f04ee1cd4059b70ef2cd22638204f4309f0
tjaitil <kjetil@baksaas.no>
2021-02-23 00:05:17 +0100

    Changed style and updated format on news

--- views/news.php
+++ views/news.php
@@ -16,12 +16,12 @@
             <div class="news_container">
                 <?php
                     $arr = array();
-                    $arr[] = array("news_type" => "game update", "news_title" => "alpha test", "news_introduction"  =>
+                    $arr[] = array("news_type" => "game update", "news_title" => "news test", "news_introduction"  =>
                                    "Alpha test is soon underway. Players who want to participate...");
                     for($i = 0; $i < count($arr); $i++): ?>
                     <div class="news_element">
                         <img src="<?php echo constant('ROUTE_IMG') . $arr[$i]['news_type'];?>" />
-                        <h4 class="news_title"><?php echo $arr[$i]['news_title'];?></h4>
+                        <h4 class="news_title"><?php echo ucwords([$i]['news_title']);?></h4>
                         <p class="news_introduction">
                         <?php
                          if(strlen($arr[$i]['news_introduction']) > 40) {
