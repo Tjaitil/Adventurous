50fccaa7a208c87bb680d3f743c2da990604fa4c
tjaitil <kjetil@baksaas.no>
2021-02-24 18:47:29 +0100

    New script containing onload function for determining layout)

---
+++ public/js/checkSite.js
@@ -0,0 +1,11 @@
+     window.addEventListener("load", function() {
+        // Make section cover whole width of page by setting the gridColumn to 1 / 3 from 2 / 3. See layout.css
+        if(document.getElementsByTagName("aside").length == 0) {
+            let divWrapper = document.getElementById("wrapper");
+            divWrapper.style.gridTemplateColumns = "auto";
+            document.getElementsByTagName("section")[0].style.gridColumn = "1 / 3";
+        }
+        else {
+            return false;
+        }
+    });
