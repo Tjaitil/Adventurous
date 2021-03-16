47a075e7c1135a20ce2145cb98c0f37c06a1e7be
tjaitil <kjetil@baksaas.no>
2021-03-16 15:45:19 +0100

    Fixed error site

--- views/error.php
+++ views/error.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-          <title>Error! Page not found</title>
-          <link rel="stylesheet" type="text/css" href="public/css/error.css" />
-        </head>
 
-        <body>
-        
-            <section>
-              <h3> OH no! Looks you found a page that doesn't exists!</h3></br>
-              <a href="/main"> Back to main page</a></br>
-              <h5> Please let us know how you got here: <a href="#"> Send email!</a></h5> <!-- Add hreF="mailto:example@gmail.com"> --->
-            </section>
-        </body>
-    </html>
+        <h3> OH no! Looks you found a page that doesn't exists!</h3></br>
+        <a href="/main"> Back to main page</a></br>
+        <h5> Please let us know how you got here: <a href="#"> Send email!</a></h5> <!-- Add hreF="mailto:example@gmail.com"> --->
