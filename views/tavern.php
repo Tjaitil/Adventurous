161f16ec1827ce9d7ccb040b7a6179adf9a4bed4
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-03-29 15:14:20 +0200

    Currently disabled person container

--- views/tavern.php
+++ views/tavern.php
@@ -1,9 +1,9 @@
             tavern.css|tavern.js|
             <h3 class="page_title"> Tavern </h3>
             <div id="persons">
-                <?php if(empty($this->data['user_tavern_data']['persons'])):?>
-                <div> No persons of relevance to talk to in tavern </div>
-                <?php endif; ?>
+                <?php /*if(empty($this->data['user_tavern_data']['persons'])) */ ?>
+                <!--<div> No persons of relevance to talk to in tavern </div>-->
+                <?php /*endif;*/ ?>
                 <?php /*foreach($this->data['user_tavern_data']['persons'] as $key): ?>
                     <div><img src="#" width="50px" height="50px" /><?php echo ucfirst($key); ?>
                     <button onclick="talk('<?php echo ucfirst($key); ?>', '0');">Talk</button></div>
