4226a94ea0ef18ca22517845605e1e7835641ca5
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-04-01 23:46:02 +0200

    Moved content to new file and added url check

--- public/js/general.js
+++ public/js/general.js
@@ -92,8 +92,9 @@
             document.getElementById("inv_toggle_button").addEventListener("click", inventorySidebarMob.toggleInventory);
         }
         clock();
-        checkMessages();
-        setTimeout(updateCountdownTab, 120000);
+        if(location.href.indexOf("gameguide") == -1) {
+            checkMessages();   
+        }
     });
     /*window.addEventListener("scroll", function(e) {
         let element = e.target;
@@ -681,30 +682,6 @@
             }
         }
     };
-    function updateCountdownTab() {
-        let data = "model=SidebarUpdater" + "&method=calculateCountdowns";
-        ajaxJS(data, function(response) {
-            if(response[0] !== false) {
-                document.getElementById("tab_2").innerHTML = response[1];
-            }
-        });
-        /*setTimeout(updateCountdownTab, 120000);*/
-    }
-    function updateDiplomacyTab() {
-        let data = "model=SidebarUpdater" + "&method=getDiplomacy";
-        ajaxJS(data, function(response) {
-            if(response[0] !== false) {
-                let responseText = JSON.parse(response[1]);
-                console.log(responseText);
-                let trs = document.getElementById("tab_3").querySelectorAll("tbody")[0].querySelectorAll("tr");
-                for(let i = 0; i < trs.length; i++) {
-                    let tdText = trs[i].querySelectorAll("td")[1].innerHTML;
-                    let numberDifference = parseFloat(tdText) - parseFloat(responseText[i]);
-                    trs[i].querySelectorAll("td")[1].innerText = responseText[i] + " (" + numberDifference + ")";
-                }
-            }
-        });
-    }
     newLevel = {
         skillElement: null,
         skillData: null,
