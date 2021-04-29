1027ea68c0dd8328c9e3ee6a10b4a15d2f009ac8
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-04-29 23:09:27 +0200

    Removed link code

--- public/js/workforcelodge.js
+++ public/js/workforcelodge.js
@@ -1,9 +1,3 @@
     if(document.getElementById("news_content").children[2] != null) {
-        let buttons = document.getElementById("news_content_main_content").querySelectorAll("button");
-        buttons[0].addEventListener("click", function() {
-            game.fetchBuilding('tavern'); 
-        });
-        buttons[1].addEventListener("click", function() {
-            game.fetchBuilding('citycentre');
-        });
+
     }
