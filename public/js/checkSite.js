3b108b3f5816957588a3dc951a088bf55e2ece77
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-05-10 20:41:18 +0200

    Style changes and also add id when aside is not present

--- public/js/checkSite.js
+++ public/js/checkSite.js
@@ -2,9 +2,9 @@
         // Make section cover whole width of page by setting the gridColumn to 1 / 3 from 2 / 3. See layout.css
         if(document.getElementsByTagName("aside").length == 0) {
             let divWrapper = document.getElementById("wrapper");
-            divWrapper.style.gridTemplateColumns = "auto";
-            document.getElementsByTagName("section")[0].style.gridColumn = "1 / 3";
-            document.getElementsByTagName("footer")[0].style.gridColumn = "1 / 3";
+            divWrapper.id = "wrapper wrapperOneColumn";
+            document.getElementsByTagName("section")[0].style.gridColumn = "1 / span 2";
+            document.getElementsByTagName("footer")[0].style.gridColumn = "1 / span 2";
         }
         else {
             return false;
