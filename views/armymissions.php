b0c57db06e648a2dcbbfe96c803f8fd4e556304b
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-06-05 14:57:58 +0200

    Minor layout changes

--- views/armymissions.php
+++ views/armymissions.php
@@ -1,16 +1,18 @@
             armymissions.css|armymissions.js|
             <h3 class="page_title"> Army Missions </h3>
-            <div id="missions">
-                <button class="button_link"> << Back to army camp</button>
+            <div id="missions" class="cont_close">
+                <button> Back to army camp</button>
                 <div id="current_mission">
                     <p> Mission:</p><p id="time"></p>
                     <button> Cancel mission </button>
                 </div>
                 <div id="mission_enabled">
-                    <button onclick="exit();">Exit</button>
+                <img class="cont_exit" src="<?php echo constant("ROUTE_IMG") . 'exit.png';?>"  width="20px" height="20px" 
+                    onclick="exit();" />
                     <table id="mission_table">
                                                 
                     </table>
+                    <p>Selected warriors: <span id="selected_warrior_amount">0</span></p>
                     <p>Select Warriors:</p>
                     <button id="mission_button" onclick="doMission();"> Do Mission </button>
                 </div>
@@ -30,7 +32,7 @@
                             <td><?php echo ucfirst($key['difficulty']);?></td>
                             <td>
                                 <?php echo $key['reward'];?>
-                                <img width="15" height="15" src="<?php echo constant('ROUTE_IMG') . 'gold.png';?>"/>
+                                <img class="gold" src="<?php echo constant('ROUTE_IMG') . 'gold.png';?>"/>
                             </td>
                             <td><?php echo $key['time'];?></td>
                             <td><button onclick="prepareMission(this, <?php echo $key['mission_id']; ?>);"> Do mission</button></td>
