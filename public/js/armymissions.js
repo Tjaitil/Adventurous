41bfe553eb40e589b5270b442efc9d0fab966064
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-06-05 15:06:53 +0200

    Minor coding changes and bug fixes
    Removed unused days in getcountdown. Changed var to let. Bug fix in updateMission where getCountdown was not called

--- public/js/armymissions.js
+++ public/js/armymissions.js
@@ -13,17 +13,17 @@
                 var time = data[0] * 1000;
                 var mission = data[1];
                 var x = setInterval (function() {
-                    var now = new Date().getTime();
-                    var distance = time - now;
-                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                    let now = new Date().getTime();
+                    let distance = time - now;
+                    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
                     if(document.getElementById("time") == null) {
                         clearInterval(x);
                     }
                     else {
-                        document.getElementById("time").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+                        document.getElementById("current_mission").querySelectorAll("button")[0].style.display = "";
+                        document.getElementById("time").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
                     }
                     if(distance < 0 && mission != 0){
                         clearInterval(x);
@@ -32,13 +32,16 @@
                         btn.appendChild(t);
                         btn.addEventListener("click", updateMission);
                         document.getElementById('current_mission').appendChild(btn);
-                        document.getElementById("current_mission").querySelectorAll("button")[0].display = "none";
+                        document.getElementById("current_mission").querySelectorAll("button")[0].style.display = "none";
                         document.getElementById("time").innerHTML = "Finished";
                     }
                     else if (distance < 0) {
                         clearInterval(x);
                         document.getElementById("time").innerHTML = "None";
-                        document.getElementById("current_mission").querySelectorAll("button")[0].display = "none";
+                        document.getElementById("current_mission").querySelectorAll("button")[0].style.display = "none";
+                        if(document.getElementById("current_mission").querySelectorAll("button")[1] !== undefined) {
+                            document.getElementById("current_mission").removeChild(document.getElementById("current_mission").querySelectorAll("button")[1]);
+                        }
                     }
                 }, 1000);
             }
@@ -50,7 +53,6 @@
         clone.deleteCell(5);
         clone.setAttribute("id", mission_id);
         var ele = document.getElementById("mission_enabled");
-        console.log(document.getElementById("mission_table").firstChild);
         /* Check wether mission table is visible, if so replace node instead of appending to prevent two rows with missions
            Plus if the mission table there is no need to make ajax request to get warrior data */
         if(document.getElementById("mission_table").firstChild && ele.style.visibility == "visible") {
@@ -66,23 +68,22 @@
                     document.getElementById("mission_enabled").innerHTML += response[1];
                     var divs = document.querySelectorAll(".warriors");
                     divs.forEach(function(element) {
-                        element.addEventListener('click', function() {
-                            check();
-                        });
-                        element.querySelectorAll("button")[0].addEventListener('click', flipWarriorCard);
+                        addWarriorEvents(element);
+                        
                     });
                 }
             });
         }
     }
     function exit() {
-        var ele = document.getElementById("mission_enabled");
+        let ele = document.getElementById("mission_enabled");
         ele.style.visibility = "hidden";
-        ele.innerHTML = div;
         document.getElementById("mission_table").innerHTML = "";
         // Loop through children and find warriors_container and remove it
-        for(var i = 0; i < ele.children.length; i++) {
-            if(ele.children[i].id == "warriors_container") {
+        for(let i = 0; i < ele.children.length; i++) {
+            console.log(ele.children[i].id);
+            if(ele.children[i].id === "warriors_container") {
+                console.log('removed');
                 ele.removeChild(ele.children[i]);
                 break;
             }
@@ -91,15 +92,13 @@
     function doMission() {
         // warriorSelect.js
         var warrior_check = warriorsCheck();
-        console.log(warrior_check);
         // Send array with warriors id and mission id to model
         if(warrior_check.length == 0) {
             gameLog("Please select warriors");
             return false;
         }
-        var mission_id = document.getElementById("mission_table").querySelectorAll("tr")[0].id;
-        console.log(document.getElementById("mission_table").querySelectorAll("tr")[0]);
-        var data = "model=SetArmymission" + "&method=setMission" + "&mission_id=" + mission_id + "&warrior_check=" + warrior_check;
+        let mission_id = document.getElementById("mission_table").querySelectorAll("tr")[0].id;
+        let data = "model=SetArmymission" + "&method=setMission" + "&mission_id=" + mission_id + "&warrior_check=" + warrior_check;
         ajaxP(data, function(response) {
             console.log(response);
             if(response[0] != false) {
@@ -132,8 +131,7 @@
                 updateCountdownTab();
                 updateInventory();
                 gameLog(responseText[1]);
-                document.getElementById("current_mission").querySelectorAll("button")[1].display = "none";
-                document.getElementById("time").innerHTML = "None";
+                getCountdown();
                 // Search string for next level message
                 newLevel.searchString(response[1]);
             }
