dd3d6be5554ac258e7d415e59be077b6743d97c1
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-07-10 16:46:40 +0200

    Layout and style change

--- views/archeryshop.php
+++ views/archeryshop.php
@@ -11,38 +11,73 @@
                         </tr>
                         <?php
                         
-                        foreach($this->data as $key):?>
+                        foreach($this->data as $key => $element):?>
                             <tr>
                             <td><figure>
-                                <img class="item_img" src="<?php echo constant('ROUTE_IMG') . $key['item']. '.png';?>" />
+                                <img class="item_img" src="<?php echo constant('ROUTE_IMG') . $element['item']. '.png';?>" />
                                 <figcaption><?php echo ucwords($key['item']);?></figcaption>
                             </figure></td>
-                            <td>
+                            <td <?php if($key !== array_key_last($this->data)): ?>
+                                class="archeryShop_required"
+                                <?php endif;?>>
                                 <?php
-                                    switch($key['item']) {
-                                        case 'oak bow':
-                                            echo '3 x Oak log';
+                                    $required;
+                                    switch($element['item']) {
+                                        case 'oak bow': ?>
+                                            <p class="archeryShop_required_amount"><?php echo '3 x'?></p>
+                                            <figure>
+                                                <img src="<?php echo constant('ROUTE_IMG') . 'oak log.png'?>" />
+                                                <figcaption> Oak Log</figcaption>
+                                            </figure>
+                                            <?php
                                             break;
-                                        case 'birch bow':
-                                            echo '3 x Birch log';
+                                        case 'birch bow': ?>
+                                            <p class="archeryShop_required_amount"><?php echo '3 x'?></p>
+                                            <figure>
+                                                <img src="<?php echo constant('ROUTE_IMG') . 'birch log.png'?>" />
+                                                <figcaption> Birch Log</figcaption>
+                                            </figure>
+                                            <?php
                                             break;
-                                        case 'yew bow':
-                                            echo '3 x Yew log';
+                                        case 'yew bow': ?>
+                                            <p class="archeryShop_required_amount"><?php echo '3 x'?></p>
+                                            <figure>
+                                                <img src="<?php echo constant('ROUTE_IMG') . 'yew log.png'?>" />
+                                                <figcaption> Yew Log</figcaption>
+                                            </figure>
+                                            <?php
                                             break;
-                                        case 'arrow shaft':
-                                            echo '1 x Oak log';
+                                        case 'arrow shaft':?>
+                                            <p class="archeryShop_required_amount"><?php echo '1 x'?><p>
+                                            <figure>
+                                                <img src="<?php echo constant('ROUTE_IMG') . 'oak log.png'?>" />
+                                                <figcaption> Oak Log</figcaption>
+                                            </figure>
+                                            <?php
                                             break;
-                                        case 'unfinished arrows':
-                                            echo '1 x Arrow shaft';
-                                            echo '</br>';
-                                            echo '1 x Feather';
+                                        case 'unfinished arrows': ?>
+                                            <div class="archeryShop_required">
+                                                <p class="archeryShop_required_amount"><?php echo '1 x'?></p>
+                                                <figure>
+                                                    <img src="<?php echo constant('ROUTE_IMG') . 'arrow shaft.png'?>" />
+                                                    <figcaption> Arrow shaft</figcaption>
+                                                </figure>
+                                            </div>
+                                            <div class="archeryShop_required">
+                                                <p class="archeryShop_required_amount"><?php echo '1 x'?></p>
+                                                <figure>
+                                                    <img src="<?php echo constant('ROUTE_IMG') . 'feather.png'?>" />
+                                                    <figcaption> Feather </figcaption>
+                                                </figure>
+                                            </div>
+                                            <?php
                                             break;
                                         default:
                                             break;
                                     }
                                 ?>
                             </td>
-                            <td><?php echo $key['cost'];?><img class="gold" src="<?php echo constant('ROUTE_IMG') . 'gold.png';?>" /></td>
+                            <td><?php echo $element['cost'];?><img class="gold" src="<?php echo constant('ROUTE_IMG') . 'gold.png';?>" /></td>
                             <td><input type="number" min="0" />
                             <button> Make</button></td>
                         </tr>
