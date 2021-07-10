354e7f436017427a05a7139d131d4e6f12ce5115
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-07-10 15:28:50 +0200

    Changed JS event listener

--- public/js/checkSite.js
+++ public/js/checkSite.js
@@ -1,4 +1,4 @@
-     window.addEventListener("load", function() {
+     window.addEventListener("DOMContentLoaded", function() {
         // Make section cover whole width of page by setting the gridColumn to 1 / 3 from 2 / 3. See layout.css
         if(document.getElementsByTagName("aside").length == 0) {
             document.getElementsByTagName("body")[0].id = "wrapperOneColumn";
