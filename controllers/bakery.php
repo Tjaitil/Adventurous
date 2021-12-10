ea49fa092dc201bdf8f44a427ab4c2706f922c9a
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-12-10 17:03:47 +0100

    Updated rendering
    Controller will now fetch data from model

--- controllers/bakery.php
+++ controllers/bakery.php
@@ -1,12 +1,14 @@
 <?php
     class bakery extends controller {
-    
+        public $data = array();
         function __construct() {
             parent::__construct();
         }
         
         public function index() {
-            $this->render('bakery', 'Bakery', false, true, true);
+            $this->loadModel('Bakery', true);
+            $this->data = $this->model->getData();
+            $this->render('bakery', 'Bakery', $this->data, true, true);
         }
     }
 ?>
