bfae28903d0696e7b8f2d2d28e54212e30703771
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-12-14 23:14:30 +0100

    Updated to use the new response functionality

--- models/SetTraining_model.php
+++ models/SetTraining_model.php
@@ -9,15 +9,11 @@
             $this->session = $session;
             $this->commonModels(true, true);
         }
-        public function getTrainingTypeData($trainingType, $warrior_id) {
-           
-            unset($stmt);
-            return $data;
-        }
         public function setTraining($POST) {
             // $POST variable holds the post data
             // This function is called from an AJAX request
             // Function to set training
+
             $warriors = $POST['warrior'];
             $type = $POST['type'];
             
@@ -33,18 +29,17 @@
             $stmt = $this->db->conn->prepare($sql);
             $stmt->execute($query_array);
             if($stmt->rowCount() !== $amount) {
-                $this->gameMessage("ERROR: One or more of your warriors are on mission or training", true);
+                $this->response->addTo("errorGameMessage" , "One or more of the selected warriors are on mission or training");
                 return false;
             }
             
+            $param_training_type = $type;
             $sql = "SELECT time, experience FROM training_type_data WHERE training_type=:training_type";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":training_type", $param_training_type, PDO::PARAM_STR);
-            $param_training_type = $type;
             $stmt->execute();
             $row2 = $stmt->fetch(PDO::FETCH_ASSOC);
             
-            $data = array();
             $addTime = $row2['time'];
             $date = date("Y-m-d H:i:s");
             $new_date = new DateTime($date);
@@ -52,28 +47,30 @@
             
             try {
                 $this->db->conn->beginTransaction();
-                
-
+                $param_training_type = "";
+                $param_training_countdown = "";
+                $param_warrior_id = "";
+                $param_username = $this->username;
                 $sql2 = "UPDATE warriors SET training_type=:training_type, training_countdown=:training_countdown, fetch_report=1
                          WHERE warrior_id=:warrior_id AND username=:username";
                 $stmt2 = $this->db->conn->prepare($sql2);
                 $stmt2->bindParam(":training_type", $param_training_type, PDO::PARAM_STR);
                 $stmt2->bindParam(":training_countdown", $param_training_countdown, PDO::PARAM_STR);
-                $stmt2->bindParam(":warrior_id", $param_warrior_id, PDO::PARAM_STR);
+                $stmt2->bindParam(":warrior_id", $param_warrior_id, PDO::PARAM_INT);
                 $stmt2->bindParam(":username", $param_username, PDO::PARAM_STR);
                 foreach($warrior_ids as $key) {
                     $param_training_type = $type;
                     $param_training_countdown = date_format($new_date, "Y-m-d H:i:s");
                     $param_warrior_id = $key;
-                    $param_username = $this->username;
                     $stmt2->execute();
                 }
                 
                 // Only gain xp when warrior level is below 30 or if profiency is warrior
                 if($this->session['warrior']['level'] < 30 || $this->session['profiency'] == 'warrior') { 
-                    $this->UpdateGamedata->updateXP('warrior', $row2['experience'] * count($warriors));
+                    $this->response->addTo("levelUP", 
+                        $this->UpdateGamedata->updateXP('warrior', $row2['experience'] * count($warriors))
+                    );
                 }
-                
                 $this->db->conn->commit();
                 }
             catch (Exception $e) {
@@ -81,12 +78,7 @@
                 return false;
             }
             $this->db->closeConn();
-            /* Echo order, split by "|"
-             * [0] -> possible level up message;
-             * [1] -> gameMessage
-             */
-            echo "|";
-            $this->gameMessage("Warrior {$type} training started", true);
+            $this->response->addTo("gameMessage", "Warrior {$type} training started");
+            $this->response->send();
         }
     }
-?>
