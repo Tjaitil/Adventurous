d178625dbf4d7c082afa79aa6e48672b51f039ce
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-12-14 23:08:42 +0100

    Updated to use the new response functionality

--- models/Smithy_model.php
+++ models/Smithy_model.php
@@ -34,20 +34,20 @@
             $required = (strpos($item, 'bar')) ? $mineral . ' ore' : $mineral . ' bar';
             $minerals = array("iron", "steel", "gargonite", "adron", "yeqdon", "frajrite");
             if(in_array($mineral, $minerals) == false) {
-                $this->gameMessage("ERROR: You are not allowed to smith from that mineral", true);
+                $this->response->addTo("errorGameMessage" , "You are not allowed to smith from that mineral");
                 return false;
             }
             // Check if player has the required ore and gold in inventory
+            $param_item = $required;
+            $param_username = $this->username;
             $sql = "SELECT item, amount FROM inventory WHERE (item=:item OR item='gold') AND username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":item", $param_item, PDO::PARAM_STR);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_item = $required;
-            $param_username = $this->username;
             $stmt->execute();
             $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
             if($stmt->rowCount() == 1) {
-                $this->gameMessage("ERROR: You missing one or more items in your inventory", true);
+                $this->response->addTo("errorGameMessage" , "You missing one or more items in your inventory");
                 return false;
             }
             if($row[0]['item'] === 'gold') {
@@ -66,36 +66,35 @@
             $stmt->execute();
             $row2 = $stmt->fetch(PDO::FETCH_ASSOC);
             if(!$stmt->rowCount() > 0) {
-                $this->gameMessage("ERROR: That item does not exist!", true);
+                $this->response->addTo("errorGameMessage" , "That item does not exist!");
                 return false;
             }
             if($row2['level'] > $this->session['miner']['level']) {
-                $this->gameMessage("ERROR: Your level is too low", true);
+                $this->response->addTo("errorGameMessage" , "Your level is too low");
                 return false;
             }
             if(strpos($item, 'arrows') !== false) {
                 $item_data = get_item($this->session, 'unfinished arrow');
                 if(!$item_data['amount'] < 5 * $amount) {
-                    $this->gameMessage("ERROR: You don't have enough unfinished arrows in your inventory", true);
+                    $this->response->addTo("errorGameMessage" , "You don't have enough unfinished arrows in your inventory");
                     return false;    
                 }
             }
             
             $minerals_needed = $row2['mineral_required'] * $amount;
             if($row2['mineral_required'] * $amount > $row['item']['amount']) {
-                $this->gameMessage("ERROR! You dont have enough ores", true);
+                $this->response->addTo("errorGameMessage" , "You dont have enough ores");
                 return false;
             }
             $cost = $row2['cost'] * $amount;
-            // If profiency is farmer pay 10 % less
+            // If profiency is farmer pay 20 % less
             if($this->session['profiency'] === 'miner') {
-                $cost*= 0.90;
+                $cost*= 0.80;
             }
             if($row['gold']['amount'] < $cost) {
-                $this->gameMessage("ERROR! You don't have enough gold", true);
+                $this->response->addTo("errorGameMessage" , "You don't have enough gold");
                 return false;
             }
-            
             try {
                 $this->db->conn->beginTransaction();
                 if(strpos($item, 'arrows') !== false) {
