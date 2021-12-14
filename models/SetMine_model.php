b8b82d4fb8347be973f5dc6ee4b73cfa62d2e6c5
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2021-12-14 23:09:16 +0100

    Updated to use the new response functionality

--- models/SetMine_model.php
+++ models/SetMine_model.php
@@ -15,52 +15,52 @@
             // Function to set mining
             $mineral = $POST['mineral'];
             $workforce = $POST['workforce'];
-            
-            /*if($this->session['hunger'] < 10) {
-                $this->gameMessage("ERROR: Your hunger is too high, please eat!", true);
-                return false;
-            }*/
+            if(!$this->checkHunger()) return false;
+
+            $param_location = $this->session['location'];
+            $param_username = $this->username;
             $sql = "SELECT m.mining_type, m.permits, mw.avail_workforce, mw.efficiency_level
                     FROM miner AS m INNER JOIN miner_workforce AS mw ON mw.username = m.username
                     WHERE location=:location AND m.username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":location", $param_location, PDO::PARAM_STR);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_location = $this->session['location'];
-            $param_username = $this->username;
             $stmt->execute();
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             if($row['mining_type'] != 'none') {
-                $this->gameMessage("ERROR: There is already mining going on!", true);
+                $this->response->addTo("errorGameMessage" , "There is already mining going on!");
                 return false;
             }
             if($row['avail_workforce'] < $workforce) {
-                $this->gameMessage("ERROR: You don't have that many workforce available", true);
+                $this->response->addTo("errorGameMessage" , "You don't have that many workforce available");
                 return false;
             }
+
+            $param_mineral_type = $mineral;
+            $param_location = $this->session['location'];
             $sql = "SELECT miner_level, experience, time, permit_cost FROM minerals_data
                     WHERE mineral_type=:mineral_type AND location=:location";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":mineral_type", $param_mineral_type, PDO::PARAM_STR);
             $stmt->bindParam(":location", $param_location, PDO::PARAM_STR);
-            $param_mineral_type = $mineral;
-            $param_location = $this->session['location'];
             $stmt->execute();
             $row2 = $stmt->fetch(PDO::FETCH_ASSOC);
             if($row2['miner_level'] > $this->session['miner']['level']) {
-                $this->gameMessage("ERROR: You are too low level to mine this mineral", true);
+                $this->response->addTo("errorGameMessage" , "You are too low level to mine this mineral");
+
                 return false;
             }
             if(!$stmt->rowCount() > 0) {
-                $this->gameMessage("ERROR: You are in the wrong city to mine this mineral", true);
+                $this->response->addTo("errorGameMessage" , "You are in the wrong city to mine this mineral");
+
                 return false;
             }
             if($row2['permit_cost'] > $row['permits']) {
-                $this->gameMessage("ERROR: You don't have enough permits!", true);
+                $this->response->addTo("errorGameMessage" , "You don't have enough permits!");
                 return false;
             }
             
-            $addTime = $row2['time'] * ($row['efficiency_level'] / 100);
+            $addTime = $row2['time'] - ($row2['time'] * ($row['efficiency_level'] / 100));
             $date = date("Y-m-d H:i:s");
             $newDate = new DateTime($date);
             $newDate->modify("+{$addTime} seconds");
@@ -68,6 +68,11 @@
             $experience = ($row2['experience'] * 0.20);
             try {
                 $this->db->conn->beginTransaction();
+                $param_mining_type = $mineral;
+                $param_mining_countdown = date_format($newDate, "Y-m-d H:i:s");
+                $param_permits = $row['permits'] - $row2['permit_cost'];
+                $param_location = $this->session['location'];
+                $param_username = $this->username;
                 $sql = "UPDATE miner SET mining_type=:mining_type, mining_countdown=:mining_countdown,
                         permits=:permits, fetch_minerals=1 WHERE location=:location AND username=:username";
                 $stmt = $this->db->conn->prepare($sql);
@@ -76,28 +81,23 @@
                 $stmt->bindParam(":permits", $param_permits, PDO::PARAM_STR);
                 $stmt->bindParam(":location", $param_location, PDO::PARAM_STR); 
                 $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-                $param_mining_type = $mineral;
-                $param_mining_countdown = date_format($newDate, "Y-m-d H:i:s");
-                $param_permits = $row['permits'] - $row2['permit_cost'];
-                $param_location = $this->session['location'];
-                $param_username = $this->username;
                 $stmt->execute();
                 
-                $sql2 = "UPDATE miner_workforce SET avail_workforce=:avail_workforce,
-                {$this->session['location']}_workforce=:miner_workforce WHERE username=:username";
+                $param_avail_workforce = $row['avail_workforce'] - $workforce;
+                $param_miner_workforce = $workforce;
+                $param_username = $this->username;
+                $sql2 = "UPDATE miner_workforce 
+                         SET avail_workforce=:avail_workforce, {$this->session['location']}_workforce=:miner_workforce 
+                         WHERE username=:username";
                 $stmt2 = $this->db->conn->prepare($sql2);
                 $stmt2->bindParam(":avail_workforce", $param_avail_workforce, PDO::PARAM_STR);
                 $stmt2->bindParam(":miner_workforce", $param_miner_workforce, PDO::PARAM_STR);
                 $stmt2->bindParam(":username", $param_username, PDO::PARAM_STR);
-                $param_avail_workforce = $row['avail_workforce'] - $workforce;
-                $param_miner_workforce = $workforce;
-                $param_username = $this->username;
                 $stmt2->execute();
                 
                 // Only gain xp when miner level is below 30 or if profiency is miner
                 if($this->session['miner']['level'] < 30 || $this->session['profiency'] == 'miner') { 
-                    $this->UpdateGamedata->updateXP('miner', $experience);
-                    $xpUpdate = true;
+                    $this->response->addTo("levelUP", $this->UpdateGamedata->updateXP('miner', $experience));
                 }
                 
                 $this->db->conn->commit();
@@ -107,22 +107,11 @@
                 return false;
             }
             $this->db->closeConn();
-            /* Echo order, split by "|"
-             * [1] -> possible level up message;
-             * [2] -> gameMessage
-             * [3] -> $echo_data with updated game data
-             */
-            echo "|";
-            $echo_data = array();
-            if(isset($xpUpdate)) {
-                $echo_data['xp_gained'] = $experience;
-                $this->gameMessage("Mining started, {$experience} miner xp gained", true);
-            }
-            echo "|";
-            $echo_data['avail_workforce'] = $param_avail_workforce;
-            $echo_data['permits'] = $param_permits;
-            
-            echo json_encode($echo_data);
+            $this->response->addTo("data" , $experience, array("index" => "xpGained"));
+            $this->response->addTo("data" , $param_avail_workforce, array("index" => "availWorkforce"));
+            $this->response->addTo("data" , $param_permits, array("index" => "permits"));
+            $this->response->addTo("gameMessage" , "Mining started, {$experience} miner xp gained");
+            $this->response->send();
         }
     }
 ?>
