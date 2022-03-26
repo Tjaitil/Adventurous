c4538f9d475b392b015740327b969e73ac179c45
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-03-26 02:08:55 +0100

    Removed message functionality

--- views/templates/messages_tpl.php
+++
@@ -1,9 +0,0 @@
-<?php foreach($data as $key): ?>
-        <tr class="message_row">
-            <td><input type="checkbox" /></td>
-            <td><a onclick="showMessage(<?php echo $key['id'];?>, this)"><?php echo $key['title']; ?></a></td>
-            <td><?php echo ucfirst($key['receiver']); ?></td>
-            <td><?php echo $key['date']; ?></td>
-            <td><img src="<?php echo $key['message_read'] . '_read';?>.jpg"/></td>
-        </tr>
-<?php endforeach;?>
