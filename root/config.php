5107e3d8148ea1908392335bdaf9c03df28c4eef
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-03-29 23:35:48 +0200

    define timezone

--- root/config.php
+++ root/config.php
@@ -3,4 +3,6 @@
     define('HTTP_SERVER', 'http://adventurous.com');
     define('URL', 'http://adventurous.com');
     define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+    define('TIMEZONE', 'Europe/Oslo');
+    date_default_timezone_set(TIMEZONE);
 ?>
