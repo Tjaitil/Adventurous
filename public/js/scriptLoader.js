ca3cfe940c16914b293cbbcd13a4a332045b7e7b
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-04-17 23:37:53 +0200

    Rewrote scriptLoader, will no return a promise

--- public/js/scriptLoader.js
+++ public/js/scriptLoader.js
@@ -1,31 +1,32 @@
 const scriptLoader = {
     scripts: [],
-    loadScript(scriptArray) {
-        if(scriptArray.length === 0) return false;
-        scriptArray.forEach(element => {
-            let tag = document.createElement("script");
-            tag.src = './public/js/clientScripts/' + element + ".js";
-            tag.type = "text/javascript";
-            document.getElementsByTagName("section")[0].appendChild(tag);
-            this.scripts.push[{name: element, loading: true}];
-            tag.onload = () => {
-                this.scripts.map((script) => {
-                    if(script.name === "name") {
-                        return {...script, loading: false};
-                    }
-                })
-            };
-            tag.onerror = () =>  {alert("Script couldn't load")};
+    loadScript(scriptArray, scriptType) {
+        return new Promise((resolve, reject) => {
+            let folder; 
+            if(scriptArray.length === 0) return false;
+            switch (scriptType) {
+            case 'client':
+                folder = "../public/js/clientScripts/";
+                break;
+                case 'utility':
+                    folder = "../public/js/utilities/";
+                    break;
+                default:
+                    break;
+            }
+            let err = 0;
+            scriptArray.forEach(element => {
+                let tag = document.createElement("script");
+                tag.src = folder + element + ".js";
+                tag.type = "text/javascript";
+                document.getElementsByTagName("head")[0].appendChild(tag);
+                tag.onerror = () => err++;
+            });
+            if(err === 0) {
+                resolve("Done")
+            } else {
+                reject("Error loading scripts");
+            }
         });
-        this.checkLoadedScripts();
     },
-    checkLoadedScripts() {
-        let number = this.scripts.filter((script) => {
-            return (script.loading === true)
-        });
-        if(number.length == 0) {
-            console.log('finished loading');
-            this.scripts = [];
-        }
-    }
 };
