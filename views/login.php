c02eea8f7ecf080be7e7a42431e79c50305dc748
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-04-18 19:20:03 +0200

    New login layout and styling

--- views/login.php
+++ views/login.php
@@ -1,39 +1,51 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title><?php echo $title;?></title>
-        <link rel="stylesheet" type="text/css" href="public/css/<?php echo $name ?>.css" />
-        <link rel='shortcut icon' type='image/x-icon' href='<?php echo constant('ROUTE_IMG') . 'favicon.ico';?>' />
-    </head>
-    <body>
+
+<head>
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="public/css/<?php echo $name ?>.css" />
+    <link rel='shortcut icon' type='image/x-icon' href='<?php echo constant('ROUTE_IMG') . 'favicon.ico'; ?>' />
+</head>
+
+<body>
+    <section id="slot1" class="login-slide">
         <div id="background_image_container">
-            <img src="<?php echo constant('ROUTE_IMG') . "7.5m.png";?>" id="background_image"/>
+            <img src="<?php echo constant('ROUTE_IMG') . "7.5m.png"; ?>" id="background_image" />
         </div>
+        
+        <div class="front-title z-front">
+            <img src="<?php echo constant('ROUTE_IMG') . 'adventurous_logo.png'; ?>" />
+            <h1>dventurous</h1>
+        </div>
+    </section>
+    <section id="slot2" class="login-slide">
         <div id="login">
-            <img src="<?php echo constant('ROUTE_IMG') . 'adventurous_logo.png';?>" />
-            <?php if(isset($_SESSION['outdatedSessionID'])):?>
-                 <p style="color:red"> Your session is timed out or is logged in on another device. Login to continue to play</p>
+            <img src="<?php echo constant('ROUTE_IMG') . 'adventurous_logo.png'; ?>" />
+            <?php if (isset($_SESSION['outdatedSessionID'])) : ?>
+                <p style="color:red"> Your session is timed out or is logged in on another device. Login to continue to play</p>
             <?php unset($_SESSION['outdatedSessionID']);
-                  endif;?>
+            endif; ?>
             <form id="login_form" name="login" method="post" action="/login">
-                <h3>Login to continue</h3>
+                <h1>Login to continue</h1>
                 </br>
-                <label for="username">Username:</label>
-                <input id="username" type="text" name="username" minlength="4"/>
-                <span class="login_error"><?php echo $this->error['userErr'];?></span></br>
-                <label for="password">Password:</label>
-                <input id="password" type="password" name="password" minlength="4"/>
-                <span class="login_error"><?php echo $this->error['passErr'];?></span></br></br>
+                <label for="username">Username</label>
+                <input id="username" type="text" name="username" minlength="4" />
+                <span class="login_error"><?php echo $this->error['userErr']; ?></span></br>
+                <label for="password">Password</label>
+                <input id="password" type="password" name="password" minlength="4" />
+                <span class="login_error"><?php echo $this->error['passErr']; ?></span></br></br>
                 <button type="submit">Login</button>
             </form>
-            <span class="login_error"><?php echo $this->error['loginfail'];?></span>
+            <span class="login_error"><?php echo $this->error['loginfail']; ?></span>
             <p>Registration is currently closed, contact kjetil@baksaas.no for access</p>
         </div>
-        <script src="<?php echo constant('ROUTE_JS') . 'login.js';?>"></script>
-        <!--<div>
+    </section>
+    <script src="<?php echo constant('ROUTE_JS') . 'login.js'; ?>"></script>
+    <!--<div>
             <video autoplay>
                 <source src="../public/img/adventurous video test.mov" type="video/mp4">
             </video>
         </div>-->
-    </body>
+</body>
+
 </html>
