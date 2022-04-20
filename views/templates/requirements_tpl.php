2eecb350aae5ec81c6dd5ed49817672a90b61cf9
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-04-20 20:59:28 +0200

    New diplomacy icon, adjust requirements template

--- views/templates/requirements_tpl.php
+++ views/templates/requirements_tpl.php
@@ -1,10 +1,17 @@
 <?php foreach($data as $key):?>
     <tr>
         <td><img src="<?php echo constant('ROUTE_IMG') . $key['role'] . ' icon.png';?>"></td>
-        <td><?php echo ucwords($key['required']);?>
-            <img class="item_img" src="<?php echo
-            (strpos($key['required'], 'warrior'))? constant('ROUTE_IMG') . str_replace('warrior', 'icon',$key['required']) . '.png'
-                                                    : constant('ROUTE_IMG') . $key['required'] . '.png';?>"/></td>
+        <td><?php echo ($key['role'] === 'warrior') ? 'Warrior' : ucwords($key['required']);?>
+            <?php 
+                if($key['role'] === "trader") {
+                    $src = "diplomacy icon";
+                } else {
+                    $src = $key['required'];
+                }
+
+                if($key['role'] !== "warrior"): ?>
+                    <img class="item_img" src="<?php echo constant('ROUTE_IMG') . $src .'.png';?>"/></td>
+                <?php endif;?>
         <td <?php if($key['provided'] === $key['amount']) echo 'class="able-color"';?>>
             <?php echo $key['provided'], '/', $key['amount'];?>
         </td>
