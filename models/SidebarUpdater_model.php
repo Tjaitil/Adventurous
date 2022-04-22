12bd7a9a435ebfd8d16806522bdc517416b7c337
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-04-22 20:51:14 +0200

    Adjusted countdown calculations and return in response object

--- models/SidebarUpdater_model.php
+++ models/SidebarUpdater_model.php
@@ -9,39 +9,61 @@
             $this->session = $session;
         }
         public function getDiplomacy($js = false) {
+            $param_username = $this->username;
             $sql = "SELECT hirtam, pvitul, khanz, ter, fansalplains FROM diplomacy WHERE username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_username = $this->username;
             $stmt->execute();
             if($js === false) {
                 $this->data['diplomacy'] = $stmt->fetch(PDO::FETCH_ASSOC);
             }
             else {
-                echo json_encode($stmt->fetch(PDO::FETCH_NUM));
+                $this->response->addTo("data", $stmt->fetch(PDO::FETCH_NUM), array("index" => "diplomacy"));
             }
         }
         public function getCountdownData($js = false) {
-            $sql = "SELECT crop_countdown, location, plot1_harvest FROM farmer WHERE username=:username";
+            $param_username = $this->username;
+            $sql = "SELECT crop_countdown, crop_type, location, plot1_harvest FROM farmer WHERE username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_username = $this->username;
             $stmt->execute();
-            $this->data['farmer_countdowns'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            
-            $sql = "SELECT mining_countdown, location, fetch_minerals FROM miner WHERE username=:username";
+            $this->data['countdowns']['farmer'] = $this->data['farmer_countdowns'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $sql = "SELECT workforce_total, towhar_workforce, krasnur_workforce FROM farmer_workforce WHERE username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_username = $this->username;
             $stmt->execute();
-            $this->data['miner_countdowns'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $this->data['farmer_workforce'] = $stmt->fetch(PDO::FETCH_ASSOC);
             
+            $sql = "SELECT mining_countdown, mining_type, location, fetch_minerals FROM miner WHERE username=:username";
+            $stmt = $this->db->conn->prepare($sql);
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->execute();
+            $this->data['countdowns']['miner'] = $this->data['miner_countdowns'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+            $sql = "SELECT workforce_total, golbak_workforce, snerpiir_workforce FROM miner_workforce WHERE username=:username";
+            $stmt = $this->db->conn->prepare($sql);
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->execute();
+            $this->data['miner_workforce'] = $stmt->fetch(PDO::FETCH_ASSOC);
+
             $sql = "SELECT assignment_id FROM trader WHERE username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_username = $this->username;
             $stmt->execute();
             $this->data['trader_countdown'] = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($this->data['trader_countdown']['assignment_id'] !== 0) {
+                $sql = "SELECT t.assignment_id, t.cart_amount, t.delivered, ta.assignment_amount, ta.cargo, ta.assignment_type,
+                            ta.destination, ta.base, (SELECT capasity FROM travelbureau_carts WHERE wheel= cart) as capasity
+                    FROM trader AS t INNER JOIN trader_assignments AS ta ON ta.assignment_id = t.assignment_id
+                    WHERE t.username=:username"; 
+                    $stmt = $this->db->conn->prepare($sql);
+                    $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+                    $param_username = $this->username;
+                    $stmt->execute();
+                    $this->data['trader_countdown'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
             
             $sql = "SELECT mission, mission_countdown FROM warrior WHERE username=:username";
             $stmt = $this->db->conn->prepare($sql);
@@ -63,34 +85,47 @@
                 $this->getDiplomacy();
             }
             $date_now = date_timestamp_get(new DateTime(date("Y-m-d H:i:s")));
+            $i = 0;
             foreach($this->data['farmer_countdowns'] as $key) {
+                $location = $key['location'];
+                $this->data['countdowns']['farmer'][$location]['crop_type'] = $key['crop_type'];
                 $countdown = date_timestamp_get(new DateTime($key['crop_countdown']));
                 if($countdown < $date_now && $key['plot1_harvest'] == 1) {
-                    $this->data['countdowns']['farmer'][] = "finished";
+                    $this->data['countdowns']['farmer'][$location]['status'] = "finished";
                 }
                 else if($countdown > $date_now && $key['plot1_harvest'] == 1) {
                     $time_left = $countdown - $date_now;
                     $m = round($time_left / 60);
-                    $this->data['countdowns']['farmer'][] = "done in " . $m . 'm';
+                    $this->data['countdowns']['farmer'][$location]['status'] = "done in " . $m . 'm';
                 }
                 else {
-                    $this->data['countdowns']['farmer'][] = "nothing happening";
+                    $this->data['countdowns']['farmer'][$location]['status'] = "nothing happening";
                 }
+                $this->data['countdowns']['farmer'][$location]['workforce'] = $this->data['farmer_workforce'][$location . '_workforce'];
+                $i++;
             }
+            $this->data['countdowns']['farmer']['workforce'] = $this->data['farmer_workforce'];
+            $i = 0;
             foreach($this->data['miner_countdowns'] as $key) {
+                $location = $key['location'];
+                $this->data['countdowns']['miner'][$location]['mining_type'] = $key['mining_type'];
                 $countdown = date_timestamp_get(new DateTime($key['mining_countdown']));
                 if($countdown < $date_now && $key['fetch_minerals'] == 1) {
-                    $this->data['countdowns']['miner'][] = "Finished";
+                    $this->data['countdowns']['miner'][$location]['status'] = "Finished";
                 }
                 else if($countdown > $date_now && $key['fetch_minerals'] == 1) {
                     $time_left = $countdown - $date_now;
                     $m = round($time_left / 60);
-                    $this->data['countdowns']['miner'][] = "done in " . $m . 'm';
+                    $this->data['countdowns']['miner'][$location]['status'] = "done in " . $m . 'm';
                 }
                 else {
-                    $this->data['countdowns']['miner'][] = "nothing happening";
+                    $this->data['countdowns']['miner'][$location]['status'] = "nothing happening";
                 }
+                $this->data['countdowns']['miner'][$location]['workforce'] = $this->data['miner_workforce'][$location . '_workforce'];
+                $i++;
             }
+            $this->data['countdowns']['miner']['workforce'] = $this->data['miner_workforce'];
+
             $this->data['countdowns']['warrior']['training'] = 0;
             $this->data['countdowns']['warrior']['finished'] = 0;
             $this->data['countdowns']['warrior']['mission'] = 0;
@@ -129,14 +164,15 @@
             else {
                 $this->data['countdowns']['warrior']['armymission'] = sprintf($string, 'none');
             }
-            $this->data['countdowns']['trader'] = ($this->data['trader_countdown']['assignment_id'] == 0) ?
-                "none" : "assignment underway";
+            $this->data['countdowns']['trader'] = $this->data['trader_countdown'];
             $this->db->closeConn();
             if($js == false) {
                 return $this->data;
             }
             else {
+                ob_start();
                 get_template('countdown', $this->data['countdowns'], true);
+                $this->response->addTo("html", ob_get_clean());
             }
         }
     }
