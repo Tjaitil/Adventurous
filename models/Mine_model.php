af9ed43923a6e3eceaf72c0fd83eb7fd0f017b2f
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-04-22 20:57:11 +0200

    Use response object

--- models/Mine_model.php
+++ models/Mine_model.php
@@ -11,16 +11,18 @@
         public function getData($js = false) {
             $data = array();
             
+            $param_location = $this->session['location'];
+            $param_username = $this->username;
             $sql = "SELECT permits, mining_type, mining_countdown, fetch_minerals FROM miner
                     WHERE location=:location AND username=:username";          
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":location", $param_location, PDO::PARAM_STR);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_location = $this->session['location'];
-            $param_username = $this->username;
             $stmt->execute();
             $data['minerData'] = $stmt->fetch(PDO::FETCH_ASSOC);
             
+            $param_minerlevel = $this->session['miner']['level'];
+            $param_location = $this->session['location'];
             if($js === true) {
                 $sql2 = "SELECT mineral_type, miner_level, experience, time, permit_cost, location 
                 FROM minerals_data ORDER BY miner_level ASC";
@@ -29,10 +31,6 @@
                 $sql2 = "SELECT mineral_type, permit_cost FROM minerals_data ORDER BY miner_level ASC";
             }
             $stmt2 = $this->db->conn->prepare($sql2);
-            $stmt2->bindParam(":minerlevel", $param_minerlevel, PDO::PARAM_STR);
-            $stmt2->bindParam(":location", $param_location, PDO::PARAM_STR);
-            $param_minerlevel = $this->session['miner']['level'];
-            $param_location = $this->session['location'];
             $stmt2->execute();
             $data['mineral_types'] = $stmt2->fetchAll(PDO::FETCH_ASSOC);
             
@@ -43,7 +41,8 @@
             $stmt3->execute();
             $data['workforce_data'] = $stmt3->fetch(PDO::FETCH_ASSOC);
             if($js === true) {
-                echo json_encode($data);
+                $this->response->addTo("data", $data['mineral_types'], array("index" => "mineral_types"));
+                $this->response->addTo("data", $data['workforce_data'], array("index" => "workforce_data"));
             }
             else {
                 return $data;
@@ -52,22 +51,26 @@
         
         // Function to echo date for ajax request
         public function checkCountdown() {
+            $param_location = $this->session['location'];
+            $param_username = $this->username;
             $sql = "SELECT mining_countdown, fetch_minerals, mining_type FROM miner WHERE location=:location AND username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":location", $param_location, PDO::PARAM_STR);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_location = $this->session['location'];
-            $param_username = $this->username;
             $stmt->execute();
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             $date = date_timestamp_get(new DateTime($row['mining_countdown']));
             $this->db->closeConn();
-            js_echo(array($date, $row['fetch_minerals'], $row['mining_type']));
+            $this->response->addTo("data", $date, array("index" => "data"));
+            $this->response->addTo("data", $row['fetch_minerals'], array("index" => "fetch_minerals"));
+            $this->response->addTo("data", $row['mining_type'], array("index" => "mining_type"));
         }
         public function cancelMining() {
             if(in_array($this->session['location'], array('golbak', 'snerpiir')) != true) {
                     return false;   
             }
+            $param_location = $this->session['location'];
+            $param_username = $this->username;
             $workforce = $this->session['location'] . '_workforce';
             $sql = "SELECT m.mining_countdown, m.fetch_minerals, mw.avail_workforce, mw.$workforce FROM miner as m
                     INNER JOIN miner_workforce as mw ON mw.username = m.username
@@ -75,8 +78,6 @@
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":location", $param_location, PDO::PARAM_STR);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_location = $this->session['location'];
-            $param_username = $this->username;
             $stmt->execute();
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             
@@ -84,11 +85,11 @@
             $date_timestamp = date_timestamp_get(new DateTime($date));
             $countdown_timestamp = date_timestamp_get(new DateTime($row['mining_countdown']));
             if($date_timestamp > $countdown_timestamp  && $row['fetch_minerals'] == 0) {
-                $this->gameMessage("ERROR: You currently have no mining going on", true);
+                $this->response->addTo("errorGameMessage", "You currently have no mining going on");
                 return false;
             }
             else if($date_timestamp > $countdown_timestamp && $row['fetch_minerals'] == 1) {
-                $this->gameMessage("ERROR: Why would you cancel completed mining?", true);
+                $this->response->addTo("errorGameMessage", "Why would you cancel completed mining");
                 return false;
             }   
             $time = strtotime($date);
@@ -96,6 +97,10 @@
             $date = date("Y-m-d H:i:s", $time);
             try {
                 $this->db->conn->beginTransaction();
+                $param_mining_countdown = $date;
+                $param_avail_workforce = $row['avail_workforce'] + $row[$workforce];
+                $param_location = $this->session['location'];
+                $param_username = $this->username;
                 $sql = "UPDATE miner as m INNER JOIN miner_workforce as mw ON mw.username = m.username
                         SET m.mining_type='none', m.fetch_minerals=0, m.mining_countdown=:mining_countdown,
                         mw.avail_workforce=:avail_workforce, mw.$workforce=0
@@ -105,24 +110,16 @@
                 $stmt->bindParam(":avail_workforce", $param_avail_workforce, PDO::PARAM_INT);
                 $stmt->bindParam(":location", $param_location, PDO::PARAM_STR);
                 $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-                $param_mining_countdown = $date;
-                $param_avail_workforce = $row['avail_workforce'] + $row[$workforce];
-                $param_location = $this->session['location'];
-                $param_username = $this->username;
                 $stmt->execute();
                     
                 $this->db->conn->commit();
             }
             catch(Exception $e) {
-                $this->errorHandler->catchAJAX($this->db, $e);
+                $this->response->addTo("errorGameMessage", $this->errorHandler->catchAJAX($this->db, $e));
                 return false;
             }
-            // $echo_data holds game data to update data in the client in form of JSON for easier accessibility
-            $echo_data = array();
-            $this->gameMessage("You have canceled your mining", true);
-            echo "|";
-            $echo_data['avail_workforce'] = $param_avail_workforce;
-            echo json_encode($echo_data);
+            $this->response->addTo("gameMessage", "You have canceled your mining");
+            $this->response->addTo("data", $param_avail_workforce, array("index" => "availWorkforce"));            
         }
     }
 ?>
