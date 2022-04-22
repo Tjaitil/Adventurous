cd2bfc60314e441b4c34975d24740e796cdfdf73
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-04-22 22:27:01 +0200

    Adjusted data fetching

--- models/Workforcelodge_model.php
+++ models/Workforcelodge_model.php
@@ -9,14 +9,13 @@
             $this->session = $session;
         }
         public function getData() {
-            $profiences = array('farmer', 'miner');
             $data = array();
             
+            $param_username = $this->username;
             $sql = "SELECT workforce_total, towhar_workforce, krasnur_workforce, avail_workforce, efficiency_level
                     FROM farmer_workforce WHERE username=:username";
             $stmt = $this->db->conn->prepare($sql);
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $param_username = $this->username;
             $stmt->execute();
             $data['farmer_workers'] = $stmt->fetch(PDO::FETCH_ASSOC);
             
@@ -33,8 +32,15 @@
             $sql3 = "SELECT level, max_farm_workers, max_mine_workers FROM level_data WHERE level IN ($in)";
             $stmt3 = $this->db->conn->prepare($sql3);
             $stmt3->execute(array($this->session['farmer']['level'], $this->session['miner']['level']));
-            $data['workforce_cap'] = $stmt3->fetchAll(PDO::FETCH_ASSOC);
-        
+            $row3 = $stmt3->fetchAll(PDO::FETCH_ASSOC);
+            
+            $data['workforce_cap']['farmer']  = $row3[0]['max_farm_workers'];
+            // If rowCount is higher than one the farmer and miner level is different and max_mine_workers value is stored in key 1
+            if($stmt->rowCount() > 1) {
+                $data['workforce_cap']['miner']  = $row3[1]['max_mine_workers'];
+            } else {
+                $data['workforce_cap']['miner']  = $row3[0]['max_mine_workers'];
+            }
             $this->db->closeConn();
             return $data;
         }
