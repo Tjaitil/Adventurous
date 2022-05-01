1d15a91139b48024451d2ac460dbbee1a7caef39
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-01 21:42:43 +0200

    Removed hardcoded switch

--- handlers/handler_p.php
+++ handlers/handler_p.php
@@ -7,7 +7,6 @@
     $methodname = $_POST['method'];
     if($modelname === "cookieMaker") {
         $model = $handler->includeModel('CanvasGameID', $_SESSION['gamedata']);
-        $model->checkGameID($_POST);
     }
     else {
         $model = $handler->includeModel($_POST['model'], $_SESSION['gamedata']);
@@ -17,47 +16,13 @@
     }
     $method = $handler->checkMethod($model, $_POST['method']);
     if($method === true) {
-        switch($modelname) {
-            case 'Travel':
-                $city = $_SESSION['gamedata']['location'];
-                $travel_countdown = $_SESSION['gamedata']['arrive_time'];
-                $model->$methodname($city, $_POST['destination'], $travel_countdown);
-                break;
-            case "Travelbureau":
-                $model->$methodname($_POST['shop'], $_POST['item']);
-                break;
-            case "RecruitWorker":
-                //method recruitWorker()
-                if(isset($_POST['level'])) {
-                    $model->$methodname($_POST['type'], $_POST['level']);
-                }
-                else {
-                    $model->$methodname($_POST['type']);   
-                }
-                
-                break;
-            case "SetAdventure":
-                //method provide()
-                if(!isset($_POST['warrior_check'])) {
-                    $model->$methodname(strtolower($_POST['item']), $_POST['quantity'], $warrior_check = false);
-                }
-                else {
-                    $model->$methodname($item = false, $quantity = false, $_POST['warrior_check']);
-                }
-                break;
-            case "Setassignment":
-                //method newAssignment()
-                if(isset($_POST['favor'])) {
-                    $model->$methodname($_POST['assignment_id'], $favor = true);
-                }
-                else {
-                    $model->$methodname($_POST['assignment_id']);
-                }
-                break;
-            default:
-                $model->$methodname($_POST);
-                break;
-        }
+        // Response is returned from model
+        $model->$methodname($_POST);
+        echo $model->response->send();
+    }
+    else if($modelname === "cookieMaker") {
+        $model->checkGameID($_POST);
+        echo $model->response->send();
     }
     else {
         // errorHandler;
