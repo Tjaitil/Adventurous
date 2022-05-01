8009e1449740b06a5448bcd891f19740eef55297
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-01 21:30:41 +0200

    Change H tag and removed old commented code

--- views/stockpile.php
+++ views/stockpile.php
@@ -1,25 +1,11 @@
             stockpile.css|stockpile.js|
-            <h3 class="page_title"> Stockpile </h3>
+            <h1 class="page_title">Stockpile</h1>
                 <p class="help">
                     Click on items to withdraw or insert items into the stockpile.
                 </p>
             <div id="stockpile" class="div_content mb-1">
                 <?php get_template('stockpile', $this->data, true);?>
             </div>
-            <!--<div id="inventory">
-                <div id="hidden">
-                        <div class="inventory_buttons">
-                            <button onclick="insert(this, 1);"> 1 </button>
-                            <button onclick="insert(this, 3);"> 3 </button>
-                            <button onclick="insert(this, 5);"> 5 </button>
-                            <button onclick="withdraw(this, 'all');" id="all"> All </button>
-                        </div>
-                        <figure><img src="#" height="50px" witdh="50px" />
-                            <figcaption></figcaption>
-                        </figure>
-                                    
-                    </div>
-            </div>-->
             <div id="stck_menu">
                 <ul>
                     <li></li>
