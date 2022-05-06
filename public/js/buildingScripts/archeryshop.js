1c4682908c0c33b2f54e912e5132ae314e382116
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-06 17:16:40 +0200

    Convert into module

--- public/js/buildingScripts/archeryshop.js
+++ public/js/buildingScripts/archeryshop.js
@@ -1,23 +1,26 @@
-    if(document.getElementById("news_content").children[3] != null) {
-        var buttons = document.getElementById("fletch").querySelectorAll("button");
-        console.log(buttons);
-        buttons.forEach(function(element) {
-            // Add event for each element
-            element.addEventListener('click', fletch);
-        });
-    }
-    function fletch() {
+const archeryShopModule = {
+    init() {
+        [...document.getElementsByClassName("archery-shop-fletch-button")].forEach(element => 
+            element.addEventListener('click', this.fletch)
+        );
+    },
+    fletch(event) {
         if(checkInventoryStatus()) {
             gameLogger.addMessage(commonMessages.inventoryFull, true);
             return false;
         }
-        var amount = event.target.parentElement.children[0].value;
-        var item = event.target.closest("tr").querySelectorAll("figcaption")[0].innerHTML.toLowerCase();
+        let amount = event.target.parentElement.children[0].value;
+        let item = event.target.closest("tr").querySelectorAll("figcaption")[0].innerHTML.toLowerCase();
         event.target.parentElement.children[0].value = "";
-        var data = "model=ArcheryShop" + "&method=fletch" + "&item=" + item  + "&amount=" + amount;
+        let data = "model=ArcheryShop" + "&method=fletch" + "&item=" + item  + "&amount=" + amount;
         ajaxP(data, function(response) {
             if(response[0] !== false) {
                 updateInventory('ArhceryShop');
             }       
         });
-    }
+    },
+    onClose() {
+
+    },
+};
+export default archeryShopModule;
