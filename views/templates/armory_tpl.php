f5f2b54dbc7b53973d78e9762586bd6289bd5ee2
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-08 14:25:19 +0200

    Adjusted layout and styling

--- views/templates/armory_tpl.php
+++ views/templates/armory_tpl.php
@@ -1,41 +1,25 @@
 <?php
-foreach($data as $key):
-    if(!isset($key['check'])):?>
-        <div class="armory_view darkTextColor">
-    <?php endif;?>
-        <img class="type_icon" src="<?php echo constant('ROUTE_IMG') . $key['type'] . ' icon.png';?>" />
-        <p> Warrior #<?php echo $key['warrior_id'];?></p>
-        <p> Attack: <?php echo $key['attack'] + 10;?></p>
-        <p> Defence: <?php echo $key['defence'] + 15;?></p>
-        <img onclick="removeArmor(this);"
-             title="<?php echo $key['helm'];?>"
-             class="helm" src="<?php echo constant("ROUTE_IMG") . $key['helm'] . '.png'?>"/>
-        <img onclick="removeArmor(this);"
-             title="<?php echo $key['ammunition'];?>"
-             class="ammunition" src="<?php echo constant("ROUTE_IMG") . $key['ammunition'] . '.png'?>"/>
-             <div class="item" id="armory_view_div">
-                <figure>
-                    
-                </figure>
-                <span id="armory_view_span" class="item_amount">
-                <?php echo ($key['ammunition_amount'] > 0) ? $key['ammunition_amount'] : "";?></span>
-             </div>
-        <img onclick="removeArmor(this);"        
-             title="<?php echo $key['right_hand'];?>"
-             class="right_hand" src="<?php echo constant("ROUTE_IMG") . $key['right_hand'] . '.png'?>" />
-        <img onclick="removeArmor(this);"
-             title="<?php echo $key['body'];?>"
-             class="body" src="<?php echo constant("ROUTE_IMG") . $key['body'] . '.png'?>" />
-        <img onclick="removeArmor(this);"
-             title="<?php echo $key['left_hand'];?>"
-             class="left_hand" src="<?php echo constant("ROUTE_IMG") . $key['left_hand'] . '.png'?>" />
-        <img onclick="removeArmor(this);"
-             title="<?php echo $key['legs'];?>"
-             class="legs" src="<?php echo constant("ROUTE_IMG") . $key['legs'] . '.png'?>" />
-        <img onclick="removeArmor(this);"
-             title="<?php echo $key['boots'];?>"
-             class="boots" src="<?php echo constant("ROUTE_IMG") . $key['boots'] . '.png'?>" />
-    <?php if(!isset($key['check'])):?>
-        </div>
-    <?php endif;?>
-<?php endforeach;?>
+foreach ($data as $key) : ?>
+     <div class="armory_view darkTextColor">
+          <div class="armory_view_info">
+               <img class="type_icon" src="<?php echo constant('ROUTE_IMG') . $key['type'] . ' icon.png'; ?>" />
+               <div class="armory_view_info_text_wrapper">
+                    <p> Warrior #<span class="armory_view_warrior_id"><?php echo $key['warrior_id']; ?></span></p>
+                    <p> Attack: <?php echo $key['attack'] + 10; ?></p>
+                    <p> Defence: <?php echo $key['defence'] + 15; ?></p>
+               </div>
+          </div>
+          <div class="armory_view_part_grid">
+               <img class="armory_view_part helm" title="<?php echo $key['helm']; ?>" class="helm" src="<?php echo constant("ROUTE_IMG") . $key['helm'] . '.png' ?>" />
+               <div class="armory_view_div">
+                    <span class="ammunition_amount"><?php echo ($key['ammunition_amount'] > 0) ? $key['ammunition_amount'] : ""; ?></span>
+                    <img class="armory_view_part ammunition" title="<?php echo $key['ammunition']; ?>" src="<?php echo constant("ROUTE_IMG") . $key['ammunition'] . '.png' ?>">
+               </div>
+               <img class="armory_view_part right_hand" title="<?php echo $key['right_hand']; ?>" src="<?php echo constant("ROUTE_IMG") . $key['right_hand'] . '.png' ?>" />
+               <img class="armory_view_part body" title="<?php echo $key['body']; ?>" src="<?php echo constant("ROUTE_IMG") . $key['body'] . '.png' ?>" />
+               <img class="armory_view_part left_hand" title="<?php echo $key['left_hand']; ?>" src="<?php echo constant("ROUTE_IMG") . $key['left_hand'] . '.png' ?>" />
+               <img class="armory_view_part legs" title="<?php echo $key['legs']; ?>" src="<?php echo constant("ROUTE_IMG") . $key['legs'] . '.png' ?>" />
+               <img class="armory_view_part boots" title="<?php echo $key['boots']; ?>" src="<?php echo constant("ROUTE_IMG") . $key['boots'] . '.png' ?>" />
+          </div>
+     </div>
+<?php endforeach; ?>
