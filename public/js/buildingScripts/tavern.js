eea4d6295d48ad704e77c7b5951d2e2b8c2520b3
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-09 22:52:19 +0200

    Convert to module

--- public/js/buildingScripts/tavern.js
+++ public/js/buildingScripts/tavern.js
@@ -1,47 +1,33 @@
-    // Check if news_content_main_content -> children[2] has gotten content from game.js -> game.fetchBuilding()
-    if(document.getElementById("news_content").children[2] != null) {
-        document.getElementById("eat").querySelectorAll("button")[0].addEventListener("click", eat);
+const tavernModule = {
+    init() {
+        document.getElementById("tavern-eat-button").addEventListener("click", () => this.eat());
         selectItemEvent.addSelectEvent();
-    }
-    
-    function recruitWorker(type, level = false) {
-        var element = event.target.parentNode;
+        [... document.getElementsByClassName("tavern-worker-recrute")].forEach(element =>
+            element.addEventListener("click", event => this.recruitWorker(event))    
+        );
+        let hungerBar = document.getElementById("hunger_progressBar");
+        let tavernHungerBar = hungerBar.cloneNode(true);
+        let eatDiv = document.getElementById("eat");
+        eatDiv.insertBefore(tavernHungerBar, eatDiv.children[1]);
+    },
+    recruitWorker(event) {
+        let element = event.target.closest(".tavern-worker");
+        let type = element.querySelectorAll(".tavern-worker-type")[0].innerText.trim();
+        if(!type) return false;
+        let level = (element.querySelectorAll(".tavern-worker-level")[0]) ? 
+            element.querySelectorAll("tavern-worker-level")[0].innerText.trim() : false;
         let data = "model=RecruitWorker" + "&method=recruitWorker" + "&type=" + type + "&level=" + level;
-        ajaxP(data, function(response) {
+        ajaxP(data, response => {
            if(response[0] != false) {
-                element.parentNode.removeChild(element);
+                let container = event.target.closest(".tavern-worker");
+                document.getElementById("tavern-workers-grid-container").removeChild(container);
            }
         });
-    }    
-    // function talk(person, part) {
-    //     document.getElementById("curtain").style.display = "block";
-    //     document.getElementById("conversation").style.display = "block";
-    //     var data = "model=Talk" + "&method=talk" + "&person=" + person + "&part=" + part;
-    //     ajaxG(data, function(response) {
-    //         if(response[0] != false) {
-    //             var responseText = response[1].split("|");
-    //             if(responseText[1].search("<") != -1) {
-    //                 document.getElementById("conv_button").style.visibility = "hidden";
-    //             }
-                
-    //             document.getElementById(responseText[0]).style.visibility = "hidden";
-    //             document.getElementById("conv").innerHTML = responseText[1];  
-    //         }
-    //     });        
-    // }   
-    function close() {
-        console.log("close");
-        document.getElementById("curtain").style.display = "none";
-        document.getElementById("conversation").style.display = "none";
-    }
-    function getHealingAmount(item) {
-        console.log(event.target);
-        if(item.length == 0) {
-            return false;
-        }
+    },
+    getHealingAmount(item) {
+        if(item.length == 0) return false;
         let data = "model=Tavern" + "&method=getHealingAmount" + "&item=" + item; 
         ajaxG(data, function(response) {
-            console.log(response);
             if(response[0] != false) {
                 let responseText = response[1];
                 if(parseInt(responseText) === 0) {
@@ -50,18 +36,18 @@
                 else {
                     document.getElementById("item_healing_amount").innerText = "Healing per item " + responseText.heal;
                 }
-                
+                newsContentSidebar.adjustMainContentHeight();
             }
         });
-    }
-    function eat() {
+    },
+    eat() {
         let item = document.getElementById("selected").querySelectorAll("figure")[0].children[1].innerHTML.toLowerCase();
         if(item.length == 0) {
             gameLogger.addMessage("ERROR: Select a item to eat!");
             gameLogger.logMessages();
             return false;
         }
-        let amount = document.getElementById("eat").querySelectorAll("input")[0].value;
+        let amount = document.getElementById("healing-item-amount").value;
         if(amount == 0 || amount == null) {
             gameLogger.addMessage("ERROR: Select a amount");
             gameLogger.logMessages();
@@ -70,10 +56,19 @@
         let data = "model=Hunger" + "&method=eat" + "&item=" + item + "&amount=" + amount;
         ajaxP(data, function(response) {
             if(response[0] != false) {
-                getHunger();
+                let responseText = response[1];
+                let hungerBarContainers = document.querySelectorAll("#hunger_progressBar");
+                hungerBarContainers.forEach(element => 
+                    progressBar.calculateProgress(element, responseText.newHunger, false, true)
+                );
                 updateInventory();
                 document.getElementById("selected").innerHTML = "";
-                document.getElementById("eat").querySelectorAll("input")[0].value = "";
+                document.getElementById("healing-item-amount").value = "";
             }
         });
+    },
+    onClose() {
+
     }
+}
+export default tavernModule;
