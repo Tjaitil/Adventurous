ebb3b7726bcd5de43d7500cf4c4b77ec47619686
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-18 23:34:33 +0200

    Rewrite file into module

--- public/js/buildingScripts/trader.js
+++ public/js/buildingScripts/trader.js
@@ -1,5 +1,36 @@
-    function newAssignment(id) {
-        let data = "model=Trader" + "&method=newAssignment" + "&assignment_id=" + id;
+const traderModule = {
+    selected: null,
+    init() {
+        document.getElementById("start_trader_assignment").addEventListener("click", () => this.newAssignment());
+        [...document.getElementsByClassName("trader_assignment")].forEach(element =>
+            element.addEventListener("click", event => traderModule.selectTrade(event))
+        );
+        // Check if active trader assignment
+        if(document.getElementById("traderAssignment_progressBar")) {
+            // Calculate progress
+            progressBar.calculateProgress(document.getElementById("traderAssignment_progressBar"), false, false, true);    
+            document.getElementById("traderAssignment-pick-up").addEventListener("click", () => this.pickUp());
+            document.getElementById("traderAssignment-deliver").addEventListener("click", () => this.deliver());
+        }
+    },
+    selectTrade(event) {
+        let target = event.currentTarget;
+        [...document.getElementsByClassName("trader_assignment")].forEach(element => {
+            if(element === target) {
+                element.classList.add("selected_trader_assignment");
+                if(element.querySelectorAll(".trader_assignment_id")[0]) {
+                    this.selected = element.querySelectorAll(".trader_assignment_id")[0].innerHTML.trim();
+                }
+            } else {
+                element.classList.remove("selected_trader_assignment");
+            }
+        });
+    }, 
+    newAssignment() {
+        if(this.selected === null || !this.selected) {
+            gameLogger.addMessage("Unvalid assignment", true);
+        }
+        let data = "model=Trader" + "&method=newAssignment" + "&assignment_id=" + this.selected;
         ajaxP(data, function(response) {
             if(response[0] !== false) {
                 let responseText = response[1];
@@ -9,8 +40,8 @@
                 progressBar.calculateProgress(document.getElementById("traderAssignment_progressBar"), false, false, true);
             }
         });
-    }
-    function pickUp() {
+    },
+    pickUp() {
         let data = "model=Trader" + "&method=pickUp";
         ajaxP(data, function(response) {
             console.log(response);
@@ -20,11 +51,10 @@
                 document.getElementById("traderAssignment_cart_amount").innerHTML = responseText.cartAmount;
             }
         });
-    }
-    function deliver() {
+    },
+    deliver() {
         let data = "model=Trader" + "&method=deliver";
         ajaxP(data, function(response) {
-            console.log(response);
             if(response[0] != false) {
                 let responseText = response[1];
                 updateCountdownTab();
@@ -39,7 +69,9 @@
                     // Calculate progressBar
                     progressBar.calculateProgress(document.getElementById("traderAssignment_progressBar"), 
                                                     responseText.delivered, false, true);
-                    }
                 }
+            }
         });
     }
+};
+export default traderModule;
