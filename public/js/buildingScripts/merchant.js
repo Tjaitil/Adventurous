208051fc331271415a3d223a21004ebdd85e819e
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-18 23:34:17 +0200

    Rewrite file into module

--- public/js/buildingScripts/merchant.js
+++ public/js/buildingScripts/merchant.js
@@ -1,102 +1,94 @@
-    var updateStockCountdown = function(pause = false) {
+    
+import traderModule from './trader.js';
+import countdown from '../utilities/countdown.js';
+
+const merchantModule = {
+    init() {
+        this.updateStockCountdown(true);
+        this.getMerchantCountdown();
+        selectItemEvent.addSelectEvent(this.selectTrade);
+        if(traderModule.init) traderModule.init();
+        document.getElementById("trade_button").addEventListener("click", () => this.tradeItem());
+    },
+    updateStockCountdown(pause = false) {
         time = 0;
         if(pause == true) {
-            resetStockTimer();
+            this.resetStockTimer();
         }
         else if(pause == 'end') {
             clearTimeout(time);
         }
-        function updateStock() {
-            var data = "model=Merchant" + "&method=getOffers";
-            ajaxJS(data, function(response) {
-                if(response[0] != false) {
-                    if(document.getElementById("trades") != null) {
-                        document.getElementById("merchant-offer-list").innerHTML = response[1].html;
-                        addMerchantEvents();
-                        resetStockTimer();
-                    }
-                    
+    },
+    updateStock() {
+        let data = "model=Merchant" + "&method=getOffers";
+        ajaxJS(data, response => {
+            if(response[0] != false) {
+                if(document.getElementById("trades") != null) {
+                    document.getElementById("merchant-offer-list").innerHTML = response[1].html;
+                    this.addMerchantEvents();
+                    this.resetStockTimer();
                 }
-            });
-        }
-        function resetStockTimer() {
-            clearTimeout(time);
-            time = setTimeout(updateStock, 15000);
-            // 1000 milliseconds = 1 second
-        }  
-    };
-    if(document.getElementById("news_content").children[2] != null) {
-        addMerchantEvents();
-        updateStockCountdown(true);
-        getMerchantCountdown();
-        if(typeof(document.getElementById("traderAssignment_progressBar")) != undefined) {
-            // Calculate progress
-            progressBar.calculateProgress(document.getElementById("traderAssignment_progressBar"), false, false, true);    
-        }
-        document.getElementById("trade_button").addEventListener("click", tradeItem);
-    }
-    function getMerchantCountdown() {
+                
+            }
+        });
+    },
+    resetStockTimer() {
+        clearTimeout(time);
+        time = setTimeout(() => this.updateStock(), 15000);
+    }, 
+    getMerchantCountdown() {
         let data = "&model=Merchant" + "&method=getMerchantCountdown";
         ajaxG(data, function(response) {
-            console.log(response);
             if(response[0] != false) {
                 let responseText = response[1];
-                let time = (parseInt(responseText.date) + 14400) * 1000;
+                let endTime = (parseInt(responseText.date) + 14400) * 1000;
                 let x = setInterval (function() {
-                    let now = new Date().getTime();
-                    let distance = time - now;
-                    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                    let { remainder, hours, minutes, seconds } = countdown.calculate(endTime);
                     if(document.getElementById("time") == null) {
+                        clearInterval(x);
+                    } 
+                    else if(remainder < 1) {
+                        document.getElementById("time").innerHTML = "0";
+                        this.updateStock();
                         clearInterval(x);
                     }
                     else {
                         document.getElementById("time").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";   
                     }
-                    if(distance < 1) {
-                        clearInterval(x);
-                        document.getElementById("time").innerHTML = "0";
-                    }
                 }, 1000);
             }
         });
-    }
-    
-    function addMerchantEvents() {
-        var trades = document.getElementById("trades").querySelectorAll(".merchant-offer");
+    },
+    addMerchantEvents() {
+        let trades = document.getElementById("trades").querySelectorAll(".merchant-offer");
         if(trades.length > 0) {
-            trades.forEach(function(element) {
-                // Add eventListener to each node
-                element.addEventListener('click', selectTrade);
-            });
+            trades.forEach(element => 
+                element.addEventListener('click', event => this.selectTrade(event))
+            );
             return false;
         }
-        selectItemEvent.addSelectEvent();
-        var button = document.getElementById("do_trade").querySelectorAll("button")[0];
-        button.addEventListener("click", tradeItem);
-    }
-    function selectTrade() {
+    },
+    selectTrade(event) {
         // If trades div is hidden return false, because then the tab visible is trader assignment
         if(document.getElementById("trades").visibility == "hidden" || document.getElementById("trades") == null) {
             return false;
         }
         document.getElementById("do_trade").querySelectorAll("button")[0].disabled = false;
-        let item = event.target.closest(".merchant-offer").querySelectorAll("figcaption")[0].innerHTML;
-        if(item === "Gold") {
-            gameLogger.addMessage("You cannot sell gold!");
-            gameLogger.logMessages();
-            return false;
-        }
         let elementDiv;
         let price;
+        let item;
         if(!event.target.closest(".merchant-offer")) {
+            item = event.target.closest(".inventory_item").querySelectorAll("figcaption")[0].innerHTML.trim();
+            if(item === "Gold") {
+                gameLogger.addMessage("You cannot sell gold!", true);
+                return false;
+            }
             // Item is in inventory
             elementDiv = event.target.closest(".inventory_item");
             // Check if player is in fagna
             if(document.title.indexOf("Fagna") == -1) {
                 // Check if the merchant is interested in that item
-                let items = document.getElementById("trades_container").querySelectorAll(".tooltip");
+                let items = document.getElementById("trades_container").querySelectorAll(".tooltip");ftrade
                 let match = false;
                 for(var i = 0; i < items.length; i++) {
                     if(item === items[i].innerHTML) {
@@ -106,8 +98,7 @@
                     }
                 }
                 if(match === false) {
-                    gameLogger.addMessage("This merchant is not interested in that item");
-                    gameLogger.logMessages();
+                    gameLogger.addMessage("This merchant is not interested in that item", true);
                     return false;
                 }
             }
@@ -115,12 +106,12 @@
         else {
             elementDiv = event.target.closest(".merchant-offer");
             price = elementDiv.querySelectorAll(".item_buy_price")[0].innerHTML.trim();
+            let amount = elementDiv.querySelectorAll(".merchant-offer-amount")[0].innerHTML;
+            document.getElementById("do_trade").querySelectorAll("#amount")[0].max = amount;
         }
-        let amount = elementDiv.querySelectorAll(".merchant-offer-amount")[0].innerHTML;
         let figure = elementDiv.querySelectorAll("figure")[0].cloneNode(true);
         document.getElementById("selected_trade").innerHTML = "";
         document.getElementById("selected_trade").appendChild(figure);
-        document.getElementById("do_trade").querySelectorAll("#amount")[0].max = amount;
         document.getElementById("do_trade").querySelectorAll("p")[0].innerHTML = item;
         document.getElementById("trade_price").querySelectorAll("span")[0].innerText = 0;
         // If location is fagna, fetch price
@@ -129,7 +120,7 @@
         }
         else {
             document.getElementById("trade_price").querySelectorAll("span")[0].innerHTML = price;
-        }
+        } 
         let mode;
         if(elementDiv.parentNode.id !== "inventory") {
             mode = "Buy";
@@ -148,42 +139,46 @@
             });
             
         }
-    }
-    function tradeItem() {
+    },
+    tradeItem() {
         if(checkInventoryStatus()) {
             gameLogger.addMessage(commonMessages.inventoryFull, true);
             return false;
         }
         if(document.getElementById("selected_trade").children[0] == undefined) {
-            gameLogger.addMessage("ERROR: Select a trade!");
-            gameLogger.logMessages();
+            gameLogger.addMessage("ERROR: Select a trade!", true);
             return false;
         }
         let item = document.getElementById("selected_trade").querySelectorAll("figcaption")[0].innerHTML;
         let amount = document.getElementById("amount").value;
         let mode = document.getElementById("do_trade").querySelectorAll("button")[0].innerText.toLowerCase();
         if(mode !== "buy" && mode !== "sell") {
-            gameLogger.addMessage("Trade mode doesn't exists");
-            gameLogger.logMessages();
+            gameLogger.addMessage("Trade mode doesn't exists", true);
             return false;
         }
         if(amount.length == 0) {
-            gameLogger.addMessage("ERROR: Select your amount");
-            gameLogger.logMessages();
+            gameLogger.addMessage("ERROR: Select your amount", true);
             return false;
         }
-        var data = "model=Merchant" + "&method=tradeItem" + "&item=" + item + "&amount=" + amount + "&mode=" + mode;
-        ajaxP(data, function(response) {
+        let data = "model=Merchant" + "&method=tradeItem" + "&item=" + item + "&amount=" + amount + "&mode=" + mode;
+        ajaxP(data, response => {
             if(response[0] != false) {
                 let responseText = response[1];
                 updateDiplomacyTab();
                 updateInventory();
-                updateStock();
-                updateStockCountdown(true);
-                document.getElementById("trades_container").innerHTML = responseText.html;
+                this.addMerchantEvents();
+                this.updateStockCountdown(true);
+                document.getElementById("merchant-offer-list").innerHTML = responseText.html;
                 document.getElementById("selected_trade").innerHTML = "";
-                document.getElementById("do_trade").querySelectorAll("p")[1].innerHTML = "";
+                document.getElementById("trade_price").querySelectorAll("span")[0].innerHTML = "";
                 document.getElementById("amount").value = "0";
             }
         });
+    },
+    onClose() {
+
     }
+};
+export {
+    merchantModule as default,
+};
