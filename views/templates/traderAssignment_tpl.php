51b4c47ca5e02be6bd370b422189bf11e135ce98
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-29 14:51:43 +0200

    Adjusted layout same as in aside

--- views/templates/traderAssignment_tpl.php
+++ views/templates/traderAssignment_tpl.php
@@ -1,16 +1,28 @@
 <?php
     if($data['assignment_id'] != 0): ?>
+    
         <div class="traderAssignment_fullColumn">
                 <figure class="item">
                     <img src="<?php echo constant('ROUTE_IMG') . strtolower($data['cargo']) . '.png';?>" />
                         <figcaption><?php echo ucwords($data['cargo']);?></figcaption>
                 </figure>
         </div>
-        <div class="detatil">
+        <div class="traderAssignment_fullColumn">
             <span>
                 Route:&nbsp;
                 <?php echo ucwords($data['base']) . ' -> ' . ucwords($data['destination']);?>
             </span>
+        </div>
+        <div id="traderAssignment_progressBar" class="progressBarContainer traderAssignment_fullColumn mx-auto">
+            <div class="progressBarOverlayShadow">
+            </div>
+            <div class="progressBarOverlay">
+            </div>
+            <div class="progressBar">
+                <span class="progressBar_currentValue"><?php echo $data['delivered'];?></span>
+                &nbsp/&nbsp
+                <span class="progressBar_maxValue"><?php echo $data['assignment_amount'];?></span>
+            </div>
         </div>
         <div>
             <span>
@@ -18,25 +30,11 @@
                 <?php echo $data['assignment_type'];?>
             </span>
         </div>
-        <div class="mb-1">
-            <span>Progress &nbsp;</span>
-            <div id="traderAssignment_progressBar" class="progressBarContainer">
-                <div class="progressBarOverlayShadow">
-                </div>
-                <div class="progressBarOverlay">
-                </div>
-                <div class="progressBar">
-                    <span class="progressBar_currentValue"><?php echo $data['delivered'];?></span>
-                    &nbsp/&nbsp
-                    <span class="progressBar_maxValue"><?php echo $data['assignment_amount'];?></span>
-                </div>
-            </div>
-        </div>
-        <div class="mt-1">
+        <div>
             Cart Capasity: <span id="traderAssignment_cart_amount"><?php echo $data['cart_amount'];?></span>  /
             <span id="traderAssignment_cart_"><?php echo $data['capasity'];?></span>
         </div>
-        <div class="traderAssignment_fullColumn">
+        <div class="traderAssignment_fullColumn mb-2">
                 <button id="traderAssignment-pick-up">Pick up items</button>
                 <button id="traderAssignment-deliver">Deliver</button>
         </div>
