57e8a53b550e5c7a341b2eb7b9034dcdf1c6000d
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-05-29 23:16:12 +0200

    Rewrite into module

--- public/js/buildingScripts/crops.js
+++ public/js/buildingScripts/crops.js
@@ -1,144 +1,156 @@
-    scriptLoader.loadScript(['jsonForm'], 'utility');
-    if(document.getElementById("news_content").children[2] != null) {
-        getCountdown();
-        var img = document.getElementById("select").querySelectorAll("img");
-        img.forEach(function(element) {
-              // ... code code code for this one element
-                element.addEventListener('click', showSelect);
-            });
-        console.log(document.getElementById("data_container").querySelectorAll("button"));
-        document.getElementById("data_container").querySelectorAll("button")[0].addEventListener("click", grow);
-        document.getElementById("seed_generator").children[3].addEventListener("click", seedGenerator);
-        document.getElementById("cancel_action").addEventListener("click", destroyCrops);
+import selectContainer from '../selectContainer.js';
+import countdown from '../utilities/countdown.js';
+
+scriptLoader.loadScript(['jsonForm'], 'utility');
+    
+const cropsModule = {
+    intervalID: null,
+    init() {
+        this.getCountdown();
+        [...document.getElementsByClassName("crop_type")].forEach(element => 
+            element.addEventListener('click', (event) => selectContainer.showSelect(event))
+        );
+        document.getElementById("data_container").querySelectorAll("button")[0].addEventListener("click", 
+            () => this.grow());
+        document.getElementById("seed_generator").children[3].addEventListener("click", 
+            () => this.seedGenerator());
+        document.getElementById("cancel_action").addEventListener("click", 
+            () => this.destroyCrops());
+        document.getElementById("harvest_action").addEventListener("click", 
+            () => this.updateCrop());
         // selectitem.js
         selectItemEvent.addSelectEvent();
-        fetchData();
-    }
-    var intervals = [];
-    function getCountdown() {
+        selectContainer.fetchData();
+    },
+    getCountdown() {
         document.getElementById("growing").innerHTML = "No crops growing";
-        var data = "model=Crops" + "&method=checkCountdown";
-        ajaxJS(data, function(response) {
+        let data = "model=Crops" + "&method=checkCountdown";
+        ajaxJS(data, (response) => {
             if(response[0] != false) {
-                console.log(response[1]);
-                var data = response[1].split("|");
-                console.log(data);
-                var time = data[0] * 1000;
-                var harvest = parseInt(data[1]);
-                console.log(harvest);
-                var x = setInterval(function() {
-                    intervals.push(x);
-                    var now = new Date().getTime();
-                    var distance = time - now;
-                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                let responseText = response[1];
+                let endTime = responseText.date * 1000;
+                let harvest = parseInt(responseText.harvest);
+                this.intervalID = setInterval(() => {
+                    let { remainder, hours, minutes, seconds } = countdown.calculate(endTime);
+                    
+                    document.getElementById("harvest_action").style.display = "none";
                     if(document.getElementById("time") == null) {
                         clearInterval(x);
                     }
                     else {
-                        document.getElementById("time").innerText = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-                        document.getElementById("cancel_action").style.visibility = "";
+                        document.getElementById("time").innerText = hours + "h " + minutes + "m " + seconds + "s ";
+                        document.getElementById("cancel_action").style.display = "inline-block";
                     }
                     // Check if countdown is finished and harvest is true
-                    if(distance < 0 && harvest === 1) {
-                        clearInterval(x);
-                        var btn = document.createElement("BUTTON");
-                        var t = document.createTextNode("Harvest");
-                        btn.appendChild(t);
-                        btn.addEventListener("click", updateCrop);
-                        document.getElementById("cancel_action").style.visibility = "hidden";
+                    if(remainder < 0 && harvest === 1) {
+                        clearInterval(this.intervalID);
+                        document.getElementById("harvest_action").style.display = "inline-block";
+                        document.getElementById("cancel_action").style.display = "none";
                         document.getElementById("time").innerText = "";
-                        document.getElementById('time').appendChild(btn);
                         document.getElementById("growing").innerText = "Finished";
                     }
-                    else if(distance < 0) {
-                        clearInterval(x);
+                    else if(remainder < 0) {
+                        clearInterval(this.intervalID);
                         document.getElementById("growing").innerText = "No crops growing";
                         document.getElementById("time").innerText = "";
+                        document.getElementById("cancel_action").style.display = "none";
+                        document.getElementById("harvest_action").style.display = "none";
                     }
                 }, 1000);
+                newsContentSidebar.adjustMainContentHeight();
             }
         });
-    }
-    function grow() {
-        var form = document.getElementById("data_form");
+    },
+    grow() {
+        let form = document.getElementById("data_form");
         if(!form.reportValidity()) {
             console.log("error");
         }
-        else {
-            let JSON_data = JSONForm(form);
-            let data = "model=SetCrops" + "&method=setCrops" + "&JSON_data=" + JSON_data;
-            ajaxP(data, function(response) {
-                if(response[0] !== false) {
-                    let responseText = response[1];
-                    updateHunger(responseText.newHunger);
-                    getCountdown();
-                    updateCountdownTab();
-                    document.getElementById("data_container_avail_workforce").innerText = '(' + responseText.availWorkforce + ')';
-                }
-            });
+        let workforce = document.getElementById("workforce_amount").value;
+        let crop = document.getElementById("selected_crop_type").value;
+
+        if(workforce === 0 || workforce.length === 0) {
+            gameLogger.addMessage("You need to select the amount of workers", true);
+        } else if(crop.length === 0) {
+            gameLogger.addMessage("You need to select the crop you are trying to grow", true);
         }
-    }
-    function updateCrop() {
+        let JSON_data = JSON.stringify({
+            workforce,
+            crop
+        });
+        let data = "model=SetCrops" + "&method=setCrops" + "&JSON_data=" + JSON_data;
+        ajaxP(data, response => {
+            if(response[0] !== false) {
+                let responseText = response[1];
+                updateHunger(responseText.newHunger);
+                this.getCountdown();
+                updateInventory();
+                updateCountdownTab();
+                document.getElementById("data_container_avail_workforce").innerText = '(' + responseText.availWorkforce + ')';
+            }
+        });
+    },
+    updateCrop() {
         if(checkInventoryStatus()) {
             gameLogger.addMessage(commonMessages.inventoryFull, true);
             return false;
         }
         let data = "model=UpdateCrops" + "&method=updateCrops";
-        ajaxP(data, function (response) {
+        ajaxP(data, response => {
             if(response[0] !== false) {
                 let responseText = response[1];
-                getCountdown();
+                this.getCountdown();
                 updateCountdownTab();
+                updateInventory();
                 document.getElementById("data_container_avail_workforce").innerText = '(' + responseText.availWorkforce + ')';
 
             }
         });
-    }
-    function destroyCrops() {
-        if(confirm("You will lose seeds used to plant crops, proceed?")) {
-            return false;
-        }
-        var data = "model=Crops" + "&method=destroyCrops";
-        ajaxP(data, function(response) {
+    },
+    destroyCrops() {
+        let data = "model=Crops" + "&method=destroyCrops";
+        ajaxP(data, response => {
             if(response[0] !== false) {
                 let responseText = response[1];
-                window.clearInterval(intervals.pop());
-                getCountdown();
+                clearInterval(this.intervalID);
+                this.getCountdown();
                 updateCountdownTab();
+                updateInventory();
                 document.getElementById("data_container_avail_workforce").innerText = '(' + responseText.availWorkforce + ')';
             }
         });
-    }
-    function img() {
-        var img = document.getElementById("type_img");
-        var select = document.getElementById("form_select");
-        var name = select.children[select.selectedIndex].value;
+    },
+    img() {
+        let img = document.getElementById("type_img");
+        let select = document.getElementById("form_select");
+        let name = select.children[select.selectedIndex].value;
         if(name.length < 1) {
             return;
         }
         img.style = "display:block";
         img.src = "public/images/" + name;
-    }
-    function seedGenerator() {
-        var itemData = selectedCheck();
-        console.log(itemData);
+    },
+    seedGenerator() {
+        let itemData = selectedCheck();
         if(itemData === false) {
             return false;
         }
-        var items = ["potato", "tomato", "corn", "carrots", "cabbages", "wheat", "sugar", "spices", "apples", "oranges", "watermelon"];
-        if(items.indexOf(itemData[0]) == -1) {
+        let items = ["potato", "tomato", "corn", "carrots", "cabbages", "wheat", "sugar", "spices", "apples", "oranges", "watermelon"];
+        if(!items.includes(itemData[0])) {
             gameLogger.addMessage("ERROR: Pick a valid item");
             gameLogger.logMessages();
             return false;
         }
-        var data = "model=Crops" + "&method=getSeeds" + "&type=" + itemData[0] + "&amount=" + itemData[1];
+        let data = "model=Crops" + "&method=getSeeds" + "&type=" + itemData[0] + "&amount=" + itemData[1];
         ajaxP(data, function(response) {
             if(response[0] !== false) {
-                updateInventory("crops");
+                updateInventory();
                 document.getElementById("selected_amount").value = "";
             }
         });
+    },
+    onClose() {
+
     }
+};
+export default cropsModule;
