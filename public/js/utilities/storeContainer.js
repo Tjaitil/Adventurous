de90ee3ef651c0a69438e471063fe388f0efeb4a
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-06-08 21:50:51 +0200

    Adjust selector names. Add new function to return selectedTrade and check for tooltip

--- public/js/utilities/storeContainer.js
+++ public/js/utilities/storeContainer.js
@@ -1,27 +1,52 @@
 const storeContainer = {
     addSelectedItemButtonEvent(func, text = false) {
-        document.getElementById("container-item-selected-event-button").addEventListener("click", () => func());
+        document.getElementById("store-container-item-event-button").addEventListener("click", () => func());
         // Custom text for button
-        if(text) document.getElementById("container-item-selected-event-button").innerHTML = text;
+        if(text) document.getElementById("store-container-item-event-button").innerHTML = text;
     },
     addSelectTrade() {
-        [...document.getElementsByClassName("container-item")].forEach(element => 
+        [...document.getElementsByClassName("store-container-item")].forEach(element => 
             element.addEventListener("click", event => this.selectTrade(event)));
     },
     selectTrade(event) {
-        document.getElementById("do_trade").querySelectorAll("button")[0].disabled = false;
-        let elementDiv = event.currentTarget.closest(".container-item");
-        let price = elementDiv.querySelectorAll(".container-item-price")[0].innerHTML.trim();
+        document.getElementById("store-container-do-trade").querySelectorAll("button")[0].disabled = false;
+        let elementDiv = event.currentTarget.closest(".store-container-item");
+        let price = elementDiv.querySelectorAll(".store-container-item-price")[0].innerHTML.trim();
         let item = elementDiv.querySelectorAll("figcaption")[0].innerHTML.trim();
         let figure = elementDiv.querySelectorAll("figure")[0].cloneNode(true);
-        document.getElementById("selected_trade").innerHTML = "";
-        document.getElementById("selected_trade").appendChild(figure);
-        document.getElementById("do_trade").querySelectorAll("p")[0].innerHTML = item;
-        document.getElementById("trade_price").querySelectorAll("span")[0].innerText = 0;
-        document.getElementById("trade_price").querySelectorAll("span")[0].innerHTML = price;
+        document.getElementById("store-container-selected-trade").innerHTML = "";
+        document.getElementById("store-container-selected-trade").appendChild(figure);
+        document.getElementById("store-container-do-trade").querySelectorAll("p")[0].innerHTML = item;
+        document.getElementById("store-contaniner-trade-price").querySelectorAll("span")[0].innerText = 0;
+        document.getElementById("store-contaniner-trade-price").querySelectorAll("span")[0].innerHTML = price;
+    },
+    getSelectedTrade() {
+        if(checkInventoryStatus()) {
+            gameLogger.addMessage(commonMessages.inventoryFull, true);
+            return false;
+        }
+
+        let item = document.getElementById("store-container-selected-trade")
+                   .querySelectorAll("figcaption")[0]
+                   .innerHTML
+                   .toLowerCase()
+                   .trim();
+        let amount = document.getElementById("store-container-selected-trade-amount").value;
+
+        if(amount == 0) {
+            gameLogger.addMessage("Please enter a valid quantity", true);
+            return false;
+        } else if(!item) {
+            gameLogger.addMessage("Please select an item", true);
+        }
+
+        return {
+            item,
+            amount
+        }
     },
     clearRequirementContainer() {
-        document.getElementById("container-item-requirements").innerHTML = "";
+        document.getElementById("store-container-item-requirements").innerHTML = "";
     },
     addRequirement(name, amount, imgSrc) {
         // Add requirements to storeContainer
@@ -42,7 +67,12 @@ const storeContainer = {
         // ADd itemtitle events
         div.addEventListener("mouseenter", () => itemTitle.show());
         div.addEventListener("mouseleave", () => itemTitle.hide());
-        document.getElementById("container-item-requirements").append(div);
+        document.getElementById("store-container-item-requirements").append(div);
+    },
+    checkItemTooltip() {
+        if(document.getElementById("news_content_main_content").querySelectorAll("item_tooltip").length > 0) {
+            itemTitle.resetItemTooltip();
+        }
     }
 }
 export default storeContainer;
--- views/templates/storeContainer_tpl.php
+++ views/templates/storeContainer_tpl.php
@@ -1,7 +1,7 @@
-<div id="container-item-wrapper" class="div_content div_content_dark">
-    <div id="container-item-list" class=".pb-05">
+<div id="store-container-item-wrapper" class="div_content div_content_dark">
+    <div id="store-container-item-list" class=".pb-05">
         <?php foreach($data['container_items'] as $key): ?>
-            <div class="container-item">
+            <div class="store-container-item">
                 <figure>
                     <img src="<?php echo constant('ROUTE_IMG') . $key['item'] . '.png'; ?>" />
                     <figcaption class="tooltip"><?php echo ucwords($key['item']); ?></figcaption>
@@ -9,7 +9,7 @@
                 <span class="item_amount"></span>
                 <p>
                     <span>
-                        <span class="container-item-price"><?php echo $key['price']?></span>
+                        <span class="store-container-item-price"><?php echo $key['price']?></span>
                         <span class="<?php echo (isset($key['discount']) && $key['discount'] >  0) ? "able-color" : ""?>">
                             <?php echo (isset($key['discount']) && $key['discount'] >  0) ?  "- " . $key['discount'] : ""?>
                         </span>
@@ -19,21 +19,21 @@
             </div>
         <?php endforeach; ?>
     </div>
-    <div id="container-item-selected" class="div_content_dark">
-        <div id="do_trade" class="div_content_dark">
-            <div id="selected_trade">
+    <div id="store-container-item-selected" class="div_content_dark">
+        <div id="store-container-do-trade" class="div_content_dark">
+            <div id="store-container-selected-trade">
 
             </div>
             <p></p>
-            <p id="trade_price"><span></span>
+            <p id="store-contaniner-trade-price"><span></span>
                 <img class="gold" src="<?php echo constant("ROUTE_IMG") . 'gold.png'; ?>" />
             </p>
             <?php if(is_array($options) && $options['site'] === 'bakery'): ?>
-                <div id="container-item-requirements"></div>
+                <div id="store-container-item-requirements"></div>
             <?php endif;?>
             <label for="amount"> Amount </label></br>
-            <input class="mb-1" type="number" id="amount" name="amount" min="0" /></br>
-            <button class="mb-1" id="container-item-selected-event-button"> Trade </button>
+            <input class="mb-1" type="number" id="store-container-selected-trade-amount" name="amount" min="1" /></br>
+            <button class="mb-1" id="store-container-item-event-button"> Trade </button>
         </div>
     </div>
 </div>
