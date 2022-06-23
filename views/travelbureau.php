c4729030542be3918f0c23debcf21d5086155b65
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-06-23 21:48:24 +0200

    Adjusted to createItem function

--- views/travelbureau.php
+++ views/travelbureau.php
@@ -1,4 +1,8 @@
             travelbureau.css|travelbureau.js|
+            <?php 
+                // Get required templates
+                get_template('createItem', null, true); 
+            ?>
             <h1 class="page_title"><?php echo $title; ?></h1>
             <div id="cart_shop">
                 <p> Your current cart: <?php echo $this->data['cart'] . ' cart'; ?></p>
@@ -13,21 +17,12 @@
                                 <p><?php echo $key['capasity']; ?> capasity</p>
                                 <h4>Required</h4>
                                 <div class="d-flex justify-center">
-                                    <?php get_template(
-                                        'createItem',
-                                        array(
-                                            ["item" => $key['wheel'] . ' bar', "amount" => $key['mineral_amount']]
-                                        ),
-                                        true
+                                <?php 
+                                    $data = array(
+                                        ["item" => $key['wheel'] . ' bar', "amount" => $key['mineral_amount']],
+                                        ["item" => $key['wood'] . ' logs', "amount" => $key['wood_amount']]
                                     );
-                                    ?>
-                                    <?php get_template(
-                                        'createItem',
-                                        array(
-                                            ["item" => $key['wood'] . ' logs', "amount" => $key['wood_amount']]
-                                        ),
-                                        true
-                                    );
+                                    createItem($data);
                                     ?>
                                 </div>
                                 <p><?php echo ucfirst($key['price']);?><img class="gold" src="<?php echo constant('ROUTE_IMG') . 'gold.png'; ?>" /></p>
