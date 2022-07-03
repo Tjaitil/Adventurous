c5d920c1dc1ee297ef650fb3b9ac64c02e790f91
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-07-03 13:40:15 +0200

    Adjusted template to new function

--- views/bakery.php
+++ views/bakery.php
@@ -5,5 +5,9 @@
         <p>Here you can make food to decrease your hunger. Players with farmer profiency pay 75 % less
             </br> For more information head to <a href="gameguide/bakery" target="_blank">gameguide/bakery</a></p>
     </div>
-    <?php get_template("storeContainer", array("container_items" => $this->data), true, false, array("site" => "bakery"));?>
+    <?php 
+        get_template("storeContainer", null, true, false);
+        createStoreContainer(array("container_items" => $this->data), 
+                            ['item_requirements' => true, 'item_information' => true]);
+    ?>
 
