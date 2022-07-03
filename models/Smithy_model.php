967bddc97991a23dac64e5cc33c282403a9bd9dd
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-07-03 14:03:00 +0200

    Turn smithy into module. Rewritten frontend and backend

--- models/Smithy_model.php
+++ models/Smithy_model.php
@@ -1,125 +1,134 @@
 <?php
-    class Smithy_model extends model {
-        public $username;
-        public $session;
-        
-        function __construct ($session) {
-            parent::__construct();
-            $this->username = $session['username'];
-            $this->session = $session;
-            $this->commonModels(true, false);
-        }
-        public function getData() {
-            $data = array();
-            $sql = "SELECT item, mineral_required, cost FROM armory_items_data ORDER BY mineral_required";
-            $stmt = $this->db->conn->prepare($sql);
-            $stmt->execute();
-            $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $minerals = array("iron", "steel", "clay", "gargonite", "adron", "yeqdon", "frajrite");
-            foreach($minerals as $key) {
-                $mineral = $key;
-                $data[$key] = array_filter($row, function($key) use ($mineral) {
-                    return stripos($key['item'], $mineral) !== false;
-                });
-            }
+class Smithy_model extends model {
+    public $username;
+    public $session;
+
+    function __construct($session) {
+        parent::__construct();
+        $this->username = $session['username'];
+        $this->session = $session;
+        $this->commonModels(true, false);
+    }
+    public function getData($js = false) {
+        $data = array();
+        $sql = "SELECT a.item_id, a.item, a.price, b.item_required, b.required_amount, 
+                    (SELECT miner_level FROM minerals_data WHERE mineral_type=a.mineral) as level 
+                FROM smithy_items AS a 
+                INNER JOIN smithy_items_required AS b ON a.item_id=b.item_id 
+                ORDER BY level ASC";
+        $stmt = $this->db->conn->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $data = array();
+
+
+        $data = $this->createItemStoreData($rows, "iron");
+        // $data['iron'] = $this->createItemStoreData($rows, "iron");
+        // $data['steel'] = $this->createItemStoreData($rows, "steel");
+        // $data['gargonite'] = $this->createItemStoreData($rows, "gargonite");
+        // $data['adron'] = $this->createItemStoreData($rows, "adron");
+        // $data['yeqdon'] = $this->createItemStoreData($rows, "yeqdon");
+
+        if ($js === false) {
             return $data;
-        }
-        public function smith($POST) {
-            // $POST variable holds the post data
-            // This function is called from an AJAX request from smithy.js
-            // Function to smith items from minerals
-            $item = strtolower($POST['item']);
-            $mineral = strtolower($POST['mineral']);
-            $amount = $POST['amount'];
-            $required = (strpos($item, 'bar')) ? $mineral . ' ore' : $mineral . ' bar';
-            $minerals = array("iron", "steel", "gargonite", "adron", "yeqdon", "frajrite");
-            if(in_array($mineral, $minerals) == false) {
-                $this->response->addTo("errorGameMessage" , "You are not allowed to smith from that mineral");
-                return false;
-            }
-            // Check if player has the required ore and gold in inventory
-            $param_item = $required;
-            $param_username = $this->username;
-            $sql = "SELECT item, amount FROM inventory WHERE (item=:item OR item='gold') AND username=:username";
-            $stmt = $this->db->conn->prepare($sql);
-            $stmt->bindParam(":item", $param_item, PDO::PARAM_STR);
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $stmt->execute();
-            $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            if($stmt->rowCount() == 1) {
-                $this->response->addTo("errorGameMessage" , "You missing one or more items in your inventory");
-                return false;
-            }
-            if($row[0]['item'] === 'gold') {
-                $row['gold'] = $row[0];
-                $row['item'] = $row[1];
-            }
-            else {
-                $row['gold'] = $row[1];
-                $row['item'] = $row[0];
-            }
-            
-            $sql = "SELECT mineral_required, level, cost FROM armory_items_data WHERE item=:item";
-            $stmt = $this->db->conn->prepare($sql);
-            $stmt->bindParam(":item", $param_item, PDO::PARAM_STR);
-            $param_item = $item;
-            $stmt->execute();
-            $row2 = $stmt->fetch(PDO::FETCH_ASSOC);
-            if(!$stmt->rowCount() > 0) {
-                $this->response->addTo("errorGameMessage" , "That item does not exist!");
-                return false;
-            }
-            if($row2['level'] > $this->session['miner']['level']) {
-                $this->response->addTo("errorGameMessage" , "Your level is too low");
-                return false;
-            }
-            if(strpos($item, 'arrows') !== false) {
-                $item_data = get_item($this->session, 'unfinished arrow');
-                if(!$item_data['amount'] < 5 * $amount) {
-                    $this->response->addTo("errorGameMessage" , "You don't have enough unfinished arrows in your inventory");
-                    return false;    
-                }
-            }
-            
-            $minerals_needed = $row2['mineral_required'] * $amount;
-            if($row2['mineral_required'] * $amount > $row['item']['amount']) {
-                $this->response->addTo("errorGameMessage" , "You dont have enough ores");
-                return false;
-            }
-            $cost = $row2['cost'] * $amount;
-            // If profiency is farmer pay 20 % less
-            if($this->session['profiency'] === 'miner') {
-                $cost*= 0.80;
-            }
-            if($row['gold']['amount'] < $cost) {
-                $this->response->addTo("errorGameMessage" , "You don't have enough gold");
-                return false;
-            }
-            try {
-                $this->db->conn->beginTransaction();
-                if(strpos($item, 'arrows') !== false) {
-                    $this->UpdateGamedata->updateInventory($item, $amount * 5);
-                    $this->UpdateGamedata->updateInventory('unfinished arrow', -$minerals_needed * 5, true);
-                }
-                else {
-                    // Update inventory if item is not type of arrows
-                    $this->UpdateGamedata->updateInventory($item, $amount);
-                }
-                
-                if($this->session['profiency'] !== 'miner') {
-                    // Update inventory
-                    $this->UpdateGamedata->updateInventory('gold', -$cost);   
-                }
-                // Update inventory
-                $this->UpdateGamedata->updateInventory($required, -$minerals_needed, true);
-                
-                $this->db->conn->commit();
-            }
-            catch(Exception $e) {
-                $this->response->addTo("errorGameMessage", $this->errorHandler->catchAJAX($this->db, $e));
-                return false;
-            }
-            $this->db->closeConn();
+        } else {
+            $this->response->addTo("data", $data, array("index" => "data"));
         }
     }
-?>
+    public function createItemStoreData($rows)
+    {
+        $data = array();
+
+        // $rows = array_filter($rows, function($element) use ($mineral) {
+        //     return strpos($element['item'], $mineral) !== false;
+        // });
+
+        $setAmountItems = array("arrow");
+        foreach ($rows as $key => $value) {
+            $index = array_search($value['item_id'], array_column($data, 'item_id'));
+            if ($index !== false) {
+                $data[$index]['required'][] =
+                    array("required_amount" => $value['required_amount'], "required_item" => $value['item_required']);
+            } else {
+                $value['required'][] = array("required_amount" => $value['required_amount'], "required_item" => $value['item_required']);
+                (strpos($value['item'], $setAmountItems[0]) !== false) ? $value['setAmount'] = 5 : "";
+                array_push($data, $value);
+            }
+        }
+        return $data;
+    }
+    public function smith($POST) {
+        // $POST variable holds the post data
+        // Smith items from mineral bar and ores
+        $item = strtolower($POST['item']);
+        $amount = $POST['amount'];
+        
+
+        $param_item = $item;
+        $sql = "SELECT a.item_id, a.item, a.price, b.item_required, b.required_amount, 
+                    (SELECT miner_level FROM minerals_data WHERE mineral_type=a.mineral) as level 
+                FROM smithy_items AS a 
+                INNER JOIN smithy_items_required AS b ON a.item_id=b.item_id 
+                WHERE a.item=:item
+                ORDER BY level ASC";
+        $stmt = $this->db->conn->prepare($sql);
+        $stmt->bindParam(":item", $param_item, PDO::PARAM_STR);
+        $stmt->execute();
+        if (!$stmt->rowCount() > 0) {
+            $this->response->addTo("errorGameMessage", "You cannot craft this item!");
+            return false;
+        }
+        $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $have_required_items = true;
+        $check_item = 'none';
+        foreach ($row as $key) {
+            $check_item = get_item($this->session['inventory'], $key['item_required']);
+            if (is_null($check_item) || $check_item['amount'] < $key['required_amount']) {
+                $have_required_items = false;
+                $check_item = $key['item_required'];
+                break;
+            }
+        }
+        if ($have_required_items != true) {
+            $this->response->addTo("errorGameMessage", "You don't have enough of $check_item");
+        }
+
+        $cost = $row[0]['price'] * $amount;
+        // If profiency is farmer pay 20 % less
+        if ($this->session['profiency'] === 'miner') {
+            $cost *= 0.80;
+        }
+
+        if ($this->session['gold'] < $cost) {
+            $this->response->addTo("errorGameMessage", "You don't have enough gold");
+            return false;
+        }
+
+        // Check if level requirement is set or not
+        if ($row[0]['level'] > $this->session['miner']['level']) {
+            $this->response->addTo("errorGameMessage", "Your level is too low");
+            return false;
+        }
+
+        try {
+            $this->db->conn->beginTransaction();
+            if (strpos($item, "arrows") !== false || strpos($item, "knives") !== false) {
+                $this->UpdateGamedata->updateInventory($item, $amount * 5);
+            } else {
+                // Subtract gold from inventory
+                $this->UpdateGamedata->updateInventory($item, $amount);
+                foreach ($row as $key) {
+                    $this->UpdateGamedata->updateInventory($key['item_required'], -$key['required_amount'] * $amount);
+                }
+            }
+            $this->UpdateGamedata->updateInventory("gold", -$cost);
+
+            $this->db->conn->commit();
+        } catch (Exception $e) {
+            $this->response->addTo("errorGameMessage", $this->errorHandler->catchAJAX($this->db, $e));
+            return false;
+        }
+        $this->db->closeConn();
+    }
+}
--- public/js/buildingScripts/smithy.js
+++ public/js/buildingScripts/smithy.js
@@ -1,15 +1,45 @@
+import storeContainer from '../utilities/storeContainer.js';
+
 const smithyModule = {
     init() {
-        document.getElementById("smith").querySelectorAll("button").forEach(element => 
-            element.addEventListener('click', this.smith)
-        );
-
-        [...document.getElementsByClassName("minerals")].forEach(element => {
-            element.addEventListener('click', () => {
-                if(!element.getAttribute("title")) return;
-                this.showMineral(element.getAttribute("title"));
-            });
+        this.getData();
+        storeContainer.addSelectTrade();
+        storeContainer.addRequirementEvent(event => this.getRequiredItem(event));
+        storeContainer.addSelectedItemButtonEvent(this.smith, 'Smith');
+    },
+    data: null,
+    getData() {
+        ajaxJS("model=Smithy" + "&method=getData", response => {
+            if(response[0] != false) {
+                this.data = response[1].data;
+            }
         });
+    },
+    getRequiredItem(event) {
+        let elementDiv = event.currentTarget.closest(".store-container-item");
+        let item = elementDiv.querySelectorAll("figcaption")[0].innerHTML.toLowerCase().trim();
+        let itemData = this.data.find(element => element.item === item);
+        if(itemData && itemData.required.length > 0) {
+            storeContainer.clearRequirementContainer();
+            itemData.required.forEach(element => 
+                storeContainer.addRequirement(element.required_item, element.required_amount, element.required_item)
+            );
+            storeContainer.checkSetAmount(itemData);
+        }
+        document.getElementById("store-container-item-information").innerHTML = "Required mineral level " + itemData.level;
+
+    },
+    smith(event) {
+        let { item, amount } = storeContainer.getSelectedTrade() || {};
+        if(!item) return;
+
+        let data = "model=Smithy" + "&method=smith" + "&item=" + item + "&amount=" + amount;
+        ajaxP(data, response => {
+            if(response[0] != false) {
+                updateInventory();
+            }
+        });
+
     },
     showMineral(mineral) {
         if(!mineral) return;
@@ -26,34 +56,8 @@ const smithyModule = {
         newsContentSidebar.activeButton = "smith";
         newsContentSidebar.adjustMainContentHeight();
     },
-    smith(event) {
-        if(checkInventoryStatus()) {
-            gameLogger.addMessage(commonMessages.inventoryFull, true);
-            return false;
-        }
-        let amount = event.target.parentElement.children[0].value;
-        let item = event.target.closest("tr").querySelectorAll("figcaption")[0].innerHTML.toLowerCase();
-        let minerals = document.getElementsByClassName("minerals");
-        for(let i = 0; i < minerals.length; i++) {
-            if(minerals[i].style.borderStyle == "solid") {
-                this.mineral = minerals[i].getAttribute("title");
-                break;
-            }
-        } 
-        if(this.mineral.length < 1){
-            gameLogger.addMessage("Please select a mineral", true);
-            return false;
-        }
-        event.target.parentElement.children[0].value = "";
-        let data = "model=Smithy" + "&method=smith" + "&item=" + item  + "&amount=" + amount + "&mineral=" + this.mineral;
-        ajaxP(data, function(response) {
-            if(response[0] !== false) {
-                updateInventory();
-            }       
-        });
-    },
     onClose() {
-        return;
+        storeContainer.checkItemTooltip();
     },
 }
 export default smithyModule;
