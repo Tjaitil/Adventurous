cc142feed063c24ce7a7f0232cf459db3a67273b
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-07-03 14:30:30 +0200

    Decode messages and loop through them

--- handlers/handler_log.php
+++ handlers/handler_log.php
@@ -2,6 +2,10 @@
     require('../libs/handler.php');
     $handler = new handler(false);
     $handler->sessionCheck();
-    $log = $_GET['log'];
-    $_SESSION['log'][] = $log;
+    $log = json_decode($_GET['log']);
+
+    for($i = 0; $i < count($log); $i++) {
+        if(!strlen($log[$i]) > 0) continue;
+        $_SESSION['log'][] = $log[$i];
+    }
 ?>
