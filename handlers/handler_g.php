a1ae1a948457147334f366b5a85ffbd9203d781b
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-07-03 14:35:33 +0200

    Use response object and remove modelname switch

--- handlers/handler_g.php
+++ handlers/handler_g.php
@@ -8,56 +8,8 @@
     $model = $handler->includeModel($_GET['model'], $_SESSION['gamedata']);
     $method = $handler->checkMethod($model, $_GET['method']);
     if($method === true) {
-        if(count($_GET) < 3) {
-            $model->$methodname(); //If there is less than 3 parameters, the only parameter is model and method
-        }
-        else  {
-            switch($modelname) {
-                case "Adventures":
-                    if($methodname === "getAdventure") {
-                        $model->$methodname($_GET['id']);    
-                    }
-                    else if($methodname === "checkUser") {
-                        $model->$methodname($_GET['username']);
-                    }
-                    break;
-                case "Market":
-                    if(isset($_GET['part'])) {
-                        // Method getData()
-                        $model->$methodname($_GET['part']);
-                    }
-                    else {
-                        // Method searchOffers()
-                        $model->$methodname($_GET['item']);
-                    }
-                    
-                    break;
-                case "Messages":
-                    switch($methodname) {
-                        case 'showMessage':
-                            $model->$methodname($_GET['message_id']);
-                            break;
-                        case 'userCheck':
-                            // $_GET['input'] is user input
-                            $model->$methodname($_GET['input'], true);
-                            break;
-                        case 'getmMessages':
-                            $model->$methodname($_GET['table'], $_GET['type'], $_GET['date']);
-                            break;
-                    }
-                    break;
-                case "Item":
-                    $model->$methodname($_GET['query']);
-                    break;
-                case 'Talk':
-                    //method talk()
-                    $model->$methodname($_GET['person'], $_GET['part']);
-                    break;
-                default:
-                    $model->$methodname($_GET);
-                    break;
-            }
-        }
+        $model->$methodname($_GET);
+        echo $model->response->send();
     }
     else {
         echo "Method does not exists!";
