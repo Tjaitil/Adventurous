9cbae6b8ac29f691d80a6d4faf4d494c70667733
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-04-03 23:29:13 +0200

    Log errors to custom file

--- root/config.php
+++ root/config.php
@@ -1,8 +1,9 @@
 <?php
-    define('APP_NAME', 'Adventurous');
-    define('HTTP_SERVER', 'http://adventurous.com');
-    define('URL', 'http://adventurous.com');
-    define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
-    define('TIMEZONE', 'Europe/Oslo');
-    date_default_timezone_set(TIMEZONE);
-?>
+define('APP_NAME', 'Adventurous');
+define('HTTP_SERVER', 'http://adventurous.com');
+define('URL', 'http://adventurous.com');
+define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('TIMEZONE', 'Europe/Oslo');
+date_default_timezone_set(TIMEZONE);
+ini_set("log_errors", 1);
+ini_set("error_log", $_SERVER['DOCUMENT_ROOT'] . "adventurous.log");
