574a67f6e62a72235c484f4d8ef9ac53f0b6b1e5
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-05-29 20:25:46 +0200

    Add namespace and small tweaks

--- libs/session.php
+++ libs/session.php
@@ -1,40 +1,48 @@
 <?php
-    class session {
-        public $status = 'neutral';
-        
-        function __construct () {
-            session_start();
-        }
-        public function setSession($username, $loggedin) {
-            $_SESSION['username'] = $username;
-            $_SESSION['loggedin'] = true;
-            $_SESSION['session_id'] = $this->generateID();
-            $_SESSION['profiency'] = true;
-            $this->status = true;
-        }
-        public function validateLogin() {
-            if(!isset($_SESSION['username']) || empty($_SESSION['username'])) {
-                $this->status = false;
-            }
-        }
-        private function generateID() {
-            $id = null;
-            for($i = 0; $i < 6; $i++) {
-                $id .= rand(1,9);
-            }
-            return $id;
-        }
-        public function destroy() {
-            if(isset($_SESSION['outdatedSessionID'])) {
-                $_SESSION = array();
-                $_SESSION['outdatedSessionID'] = true;
-            }
-            else {
-                session_unset();
-                session_destroy();
-            }
-            header("Location: /login");
-            exit;
+
+namespace App\libs;
+
+class session
+{
+    public $status = 'neutral';
+
+    function __construct()
+    {
+        session_start();
+    }
+    public function setSession($username, $loggedin)
+    {
+        $_SESSION['username'] = $username;
+        $_SESSION['loggedin'] = true;
+        $_SESSION['session_id'] = $this->generateID();
+        $_SESSION['profiency'] = true;
+        $_SESSION['log'][] = "Welcome to Adventurous, " . $username . "!";
+        $this->status = true;
+    }
+    public function validateLogin()
+    {
+        if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
+            $this->status = false;
         }
     }
-?>
+    private function generateID()
+    {
+        $id = null;
+        for ($i = 0; $i < 6; $i++) {
+            $id .= rand(1, 9);
+        }
+        return $id;
+    }
+    public function destroy()
+    {
+        if (isset($_SESSION['outdatedSessionID'])) {
+            $_SESSION = array();
+            $_SESSION['outdatedSessionID'] = true;
+        } else {
+            session_unset();
+            session_destroy();
+        }
+        header("Location: /login");
+        exit;
+    }
+}
