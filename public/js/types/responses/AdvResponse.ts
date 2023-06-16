d354506c65fa8db6b05a156bcea86c0a32602ee5
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-06-17 00:14:43 +0200

    Fixed issue with response type

--- public/js/types/responses/AdvResponse.ts
+++ public/js/types/responses/AdvResponse.ts
@@ -1,13 +1,4 @@
-export interface advAPIResponse<T extends object> {
-    gameMessage?: string[];
-    errorGameMessage?: string[];
-    data: T;
-    levelUp?: {
-        skill: "farmer" | "miner" | "trader" | "warrior",
-        new_level: number;
-    },
-    html?: string[];
-}import { LevelUpSkill } from './../LevelUpSkill';
+import { LevelUpSkill } from './../LevelUpSkill';
 export interface advAPIResponse {
     gameMessage?: string[];
     errorGameMessage?: string[];
