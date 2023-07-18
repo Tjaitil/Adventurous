54e2f5ef213636eff4e7def4ffb7cdcceab6e081
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-07-18 16:43:37 +0200

    Migrated some custom css to tailwind

--- views/page.php
+++ views/page.php
@@ -6,25 +6,27 @@
     <link rel="stylesheet" type="text/css" href="<?php echo '../' . constant('ROUTE_CSS') . $name ?>.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo '../' . constant('ROUTE_CSS') ?>conversation.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo '../' . constant("ROUTE_CSS"); ?>news.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo '../' . constant("ROUTE_COMPILED"); ?>css/output.css" />
     <?php require(constant('ROUTE_VIEW') . 'head.php'); ?>
 </head>
 
-<body>
-    <header>
+<body class="grid grid-cols-6">
+    <header class="col-span-6 row-start-1">
         <?php require(constant('ROUTE_VIEW') . 'header.php'); ?>
     </header>
-    <section>
+    <section class="col-span-5 col-start-2 row-start-2">
         <?php if (array_search($name, array('advclient', 'gameguide')) === false) : ?>
-            <script src="<?php echo constant('ROUTE_JS') . $name . '.js'; ?>"></script>
+            <script src=" <?php echo constant('ROUTE_JS') . $name . '.js'; ?>">
+            </script>
         <?php endif; ?>
         <?php require(constant('ROUTE_VIEW') . $name . '.php'); ?>
     </section>
     <?php if (array_search($name, array("gameguide", "profile", "error", "main", "highscores", "news", "messages")) === false) : ?>
-        <aside>
+        <aside class="col-span-1 col-start-1 row-start-2">
             <?php require(constant('ROUTE_VIEW') . '/aside.php'); ?>
         </aside>
     <?php endif; ?>
-    <footer>
+    <footer class="col-span-5 col-start-2 row-start-3">
         Delevoped by Kjetil Baksaas
     </footer>
 </body>
