8221c68f669485200951125269dbb3c01251676b
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-07-27 21:51:31 +0200

    Add more functionality to component

--- views/components/item.blade.php
+++ views/components/item.blade.php
@@ -1,6 +1,24 @@
-<figure @class(['item', 'no-tooltip' => !$show_tooltip])>
-    <img class="mx-auto" src="{{ constant('ROUTE_IMG') . strtolower($name) . '.png' }}" />
-    <figcaption @class(['tooltip' => $show_tooltip])>
-        {{ ucwords($name) }}
-    </figcaption>
-</figure>
+@php
+    /**
+     * @param string $name
+     * @param int $amount
+     * @param boolean $show_tooltip
+     * @param boolean $show_amount
+     * @param string $id
+     */
+@endphp
+<div @class(['item', 'no-tooltip' => $show_tooltip === false]) @if (isset($id))
+    id='{{ $id }}'
+    @endif
+    >
+    <figure>
+        <img class="mx-auto" src="{{ constant('ROUTE_IMG') . strtolower($name) . '.png' }}" />
+        <figcaption @class(['tooltip' => $show_tooltip === true])>
+            {{ ucwords($name) }}
+        </figcaption>
+        <span @class([
+            'item_amount',
+            'hidden' => isset($show_amount) && $show_amount === false,
+        ])>{{ $amount }}</span>
+    </figure>
+</div>
