c293c2e8dd3efa8221cd84fa872d4cea07788be1
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-08-28 19:22:16 +0200

    Update model docblocks

--- models/Inventory.php
+++ models/Inventory.php
@@ -4,14 +4,17 @@ namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $username
+ * @property string $item
+ * @property int $amount
+ * @property int $id
+ * @mixin \Eloquent
+ */
 class Inventory extends Model
 {
     public $timestamps = false;
     public $table = 'inventory';
 
-    public $fillable = [
-        'item',
-        'amount',
-        'username'
-    ];
+    public $guarded = [];
 }
--- models/Stockpile.php
+++ models/Stockpile.php
@@ -4,15 +4,18 @@ namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $username
+ * @property string $item
+ * @property int $amount
+ * @property int $id
+ * @mixin \Eloquent
+ */
 class Stockpile extends Model
 {
     public $timestamps = false;
 
     public $table = 'stockpile';
 
-    public $fillable = [
-        'username',
-        'item',
-        'amount',
-    ];
+    public $guarded = [];
 }
