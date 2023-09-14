216e97e70d7b9f456b67322066282e560511a257
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-09-15 00:05:45 +0200

    Some style changes

--- views/components/baseInput.blade.php
+++ views/components/baseInput.blade.php
@@ -8,9 +8,13 @@
      */
 @endphp
 <div>
-    <label for="{{ $name }}">{{ $label }}</label>
-    <input type="{{ $type }}" name="{{ $name }}"
-        id="{{ $id }}" value="{{ $value }}"
-        {{ $attributes }} />
-    {{ $slot }}
+    <label for="{{ $name }}"
+        class="block text-start">{{ $label }}</label>
+    <input
+        {{ $attributes->merge(['class' => 'custom-input m-auto block w-full rounded-lg border-2 border-stone-500 bg-primary-300 px-2 py-1']) }}
+        type="{{ $type }}" name="{{ $name }}"
+        id="{{ $id }}" value="{{ $value }}" />
+    <div>
+        {{ $slot }}
+    </div>
 </div>
