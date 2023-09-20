95b2c276027a89827299416ce9421cd40cfc7610
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-09-20 19:10:07 +0200

    phpdocs

--- models/MerchantOffer.php
+++ models/MerchantOffer.php
@@ -4,7 +4,21 @@ namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @property int id
+ * @property string location
+ * @property string item
+ * @property int store_value
+ * @property int sell_value
+ * @property int amount
+ * @property Carbon date_inserted
+ * @package App\models
+ */
 class MerchantOffer extends Model
 {
     public $timestamps = false;
+
+    protected $guarded = [];
+    protected $dates = ['date_inserted'];
 }
