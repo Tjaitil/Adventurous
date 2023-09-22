7ad59c8887f3e0e29b7b637c6abad39980c3cbfa
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-09-22 18:36:04 +0200

    Bug fix in phpdocs

--- models/MerchantOffer.php
+++ models/MerchantOffer.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 
 /**
- * @property int id
- * @property string location
- * @property string item
- * @property int store_value
- * @property int sell_value
- * @property int amount
- * @property Carbon date_inserted
- * @package App\models
+ * @property int $id
+ * @property string $location
+ * @property string $item
+ * @property int $store_value
+ * @property int $store_buy_price
+ * @property int $amount
+ * @property Carbon $date_inserted
+ * @mixin \Eloquent
  */
 class MerchantOffer extends Model
 {
