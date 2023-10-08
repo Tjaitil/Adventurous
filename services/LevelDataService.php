28feeeeccaa5bbd09015bff576a3f73edcc99619
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-10-08 17:52:52 +0200

    Service class for levelData functionality

---
+++ services/LevelDataService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\services;
+
+use App\models\LevelData;
+
+class LevelDataService
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * 
+     * @param int $level 
+     * @return int
+     */
+    public function getMaxMiners(int $level)
+    {
+        $LevelData = LevelData::where('level', $level)->first();
+        if (!$LevelData instanceof LevelData) {
+            return 1;
+        }
+        return $LevelData->max_mine_workers;
+    }
+
+    /**
+     * 
+     * @param int $level 
+     * @return int
+     */
+    public function getMaxFarmers(int $level)
+    {
+        $LevelData = LevelData::where('level', $level)->first();
+        if (!$LevelData instanceof LevelData) {
+            return 1;
+        }
+        return $LevelData->max_farm_workers;
+    }
+
+    /**
+     * 
+     * @param int $level 
+     * @return int
+     */
+    public function getMaxTraders(int $level)
+    {
+        $LevelData = LevelData::where('level', $level)->first();
+        if (!$LevelData instanceof LevelData) {
+            return 1;
+        }
+        return $LevelData->max_trade_workers;
+    }
+
+    /**
+     * 
+     * @param int $level 
+     * @return \Illuminate\Database\Eloquent\Builder|\App\models\LevelData 
+     */
+    public function getMaxEfficiencyLevel(int $level)
+    {
+        $LevelData = LevelData::where('level', 0);
+        if (!$LevelData instanceof LevelData) {
+            return 1;
+        }
+
+        return $LevelData->max_efficiency_level;
+    }
+}
