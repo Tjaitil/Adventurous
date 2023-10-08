5d065d3ec9dafa26971db5100d31a20c00899f53
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-10-08 17:23:22 +0200

    New model for efficiency upgrade

---
+++ models/EfficiencyUpgrade.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $level
+ * @property int $price
+ * 
+ * @mixin \Eloquent
+ */
+class EfficiencyUpgrade extends Model
+{
+}
