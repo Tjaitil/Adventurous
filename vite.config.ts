fe7ff255761cb5230192da0f3280e7b0a97d7690
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-12-14 22:57:47 +0100

    Remove advclient from input

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
             input: ['resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/backgroundScroller.ts',
-                'resources/js/advclient.ts',
                 'resources/js/clientScripts/getXp.ts',
                 'resources/js/clientScripts/sidebar.ts'],
             refresh: true,
