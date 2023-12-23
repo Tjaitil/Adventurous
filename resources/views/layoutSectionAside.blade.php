75607eb2662020abc3dd6928744c877ec82f5925
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-12-23 23:47:17 +0100

    Load sidebar script

--- resources/views/layoutSectionAside.blade.php
+++ resources/views/layoutSectionAside.blade.php
@@ -5,6 +5,7 @@
 </section>
 <aside class="col-span-1 col-start-1 row-start-2 relative">
     @yield('asideContent')
+    @vite('resources/js/clientScripts/sidebar.ts')
 </aside>
 <footer class="col-span-5 col-start-2 row-start-3">
     Delevoped by Kjetil Baksaas
