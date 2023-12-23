aae1f409df63f23393587062ea914548d37cce29
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-12-23 21:42:02 +0100

    Decrease z-index to a more reasonable value

--- resources/views/partials/gameScreen.blade.php
+++ resources/views/partials/gameScreen.blade.php
@@ -1,6 +1,6 @@
 <div id="game-screen">
     <div id="canvas-border"
-        class="absolute z-20 w-[700px] h-[400px] rounded border-4 border-primary-700 after:pointer-events-none after:absolute after:left-[-1px] after:top-[-1px] after:h-[calc(100%+2px)] after:w-[calc(100%+2px)] after:rounded after:border-4 after:border-solid after:border-gray-950 after:content-['']"
+        class="absolute z-[7] w-[700px] h-[400px] rounded border-4 border-primary-700 after:pointer-events-none after:absolute after:left-[-1px] after:top-[-1px] after:h-[calc(100%+2px)] after:w-[calc(100%+2px)] after:rounded after:border-4 after:border-solid after:border-gray-950 after:content-['']"
     ></div>
     <x-client.canvas id="game_canvas" />
     <x-client.canvas id="game_canvas2" />
