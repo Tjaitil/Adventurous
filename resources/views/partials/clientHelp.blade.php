fa72621d86644e77fc988ee8c80effc9e35618b3
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2023-12-23 23:42:00 +0100

    New partials

---
+++ resources/views/partials/clientHelp.blade.php
@@ -0,0 +1,16 @@
+<div id="client_help_content">
+    <div id="help_content_introduction" class="mb-1">
+        Here is some short information to help you out. More extensive info can be found
+        on /gameguide.
+    </div>
+    <div class="help_content_section">
+        <h4 class="help_content_section">Hunger</h4>
+        <p class="help_content_section_p">
+            Every time you grow crops, start mining, start a new assignment or a new
+            army mission the hunger
+            will increase. To decrease hunger e.g increase hunger bar you have to eat
+            food. Visit tavern found
+            in every city to eat.
+        </p>
+    </div>
+</div>
