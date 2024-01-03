5b82c9930bcc29ea2f17f28541a2b88aa4c618e9
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-01-03 22:33:30 +0100

    Add docblock

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,32 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     protected $fillable = ['username', 'password'];
@@ -12,6 +38,6 @@ class User extends Authenticatable
 
     public function user_data()
     {
-        return $this->hasOne(User_data::class, 'username', 'username');
+        return $this->hasOne(UserData::class, 'username', 'username');
     }
 }
