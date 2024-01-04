524160c723f370b60bdd0b6500fea781397f82b7
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-01-04 22:55:52 +0100

    Remove unused functionality

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,17 +1,5 @@
-<?php
-
-use App\libs\TemplateFetcher;
-
-?>
-<div id="log_pseudo_parent">
-
-</div>
 <input type="checkbox" name="" id="draw_checkbox">
 <div id="log_2"></div>
-<?php
-
-// if (count($_SESSION['gamedata']['level_up']) > 0) :
-?>
 <!-- News is the black "curtain" which the content is displayed upon
      both used for alert and news -->
 <div id="news">
