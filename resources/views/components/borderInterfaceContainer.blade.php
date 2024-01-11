9a5df0db77f604eeb048d6b95d7ed1547492682d
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-01-11 23:26:27 +0100

    Change styling

--- resources/views/components/borderInterfaceContainer.blade.php
+++ resources/views/components/borderInterfaceContainer.blade.php
@@ -1,4 +1,4 @@
 <div
-    {{ $attributes->merge(['class' => "relative p-2 my-4 bg-primary-800 border-primary-700 text-white border-4 rounded border-default shadow-lg after:border-gray-950/60 after:pointer-events-none after:absolute after:top-0 after:left-0 after:content-[''] after:w-full after:h-full after:border-solid after:border-4 after:rounded"]) }}>
+    {{ $attributes->merge(['class' => "relative p-2 my-4 bg-primary-800 border-primary-700 text-white border-8 border-ridge rounded border-default shadow-lg after:border-gray-950/60 after:pointer-events-none after:absolute after:top-0 after:left-0 after:content-[''] after:w-full after:h-full after:border-solid after:border-4 after:rounded"]) }}>
     {!! $slot !!}
 </div>
