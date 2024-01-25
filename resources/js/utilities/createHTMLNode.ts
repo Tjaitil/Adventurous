226b129a37db7a6c9acac4e0da5a78597c560391
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-01-25 20:35:53 +0100

    Fix bug where element would return empty

--- resources/js/utilities/createHTMLNode.ts
+++ resources/js/utilities/createHTMLNode.ts
@@ -1,7 +1,7 @@
 const createHTMLNode = (htmlString: string): Node => {
     const element = document.createElement("div");
 
-    if(htmlString === null || htmlString === undefined || htmlString.length > 0) {
+    if(htmlString === null || htmlString === undefined || htmlString.length === 0) {
         return element;
     }
     // Create an dummy element
