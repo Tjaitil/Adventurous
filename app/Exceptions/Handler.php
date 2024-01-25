c787bc1a75f68587b91c437c35b9bef954c7906a
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-01-25 18:00:27 +0100

    Add a generic jsonException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -26,5 +26,9 @@ class Handler extends ExceptionHandler
         $this->renderable(function (InventoryFullException $e, Request $request) {
             $e->render($request);
         });
+
+        $this->renderable(function (JsonException $e, Request $request) {
+            $e->render($request);
+        });
     }
 }
---
+++ app/Exceptions/JsonException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Http\Responses\AdvResponse;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Log;
+
+class JsonException extends Exception
+{
+    public function __construct(
+        protected $message,
+        protected $code = 500,
+    ) {
+        parent::__construct($message, $code);
+        Log::critical($message, ["username" => Auth::user()?->username]);
+    }
+
+    public function render(Request $request): JsonResponse
+    {
+        return (new AdvResponse([], 500))->addErrorMessage('Unexpected issue happened')->toResponse($request);
+    }
+}
