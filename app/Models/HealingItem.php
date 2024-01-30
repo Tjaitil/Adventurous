4e9d3bd6e593c4688ea465ef9dd067aab4fa0e71
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-01-30 22:25:16 +0100

    Add doc blocks

--- app/Models/HealingItem.php
+++ app/Models/HealingItem.php
@@ -7,13 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
+ * App\Models\HealingItem
+ *
  * @property int $item_id
- * @property int $item
+ * @property string $item
  * @property int $price
  * @property int $heal
  * @property int $bakery_item
- * @property Collection<HealingItemRequired> $requiredItems
- * @property Collection<HealingItem> $bakery
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Inventory> $inventory
+ * @property-read int|null $inventory_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\HealingItemRequired> $requiredItems
+ * @property-read int|null $required_items_count
+ *
+ * @method static Builder|HealingItem newModelQuery()
+ * @method static Builder|HealingItem newQuery()
+ * @method static Builder|HealingItem query()
+ * @method static Builder|HealingItem whereBakeryItem($value)
+ * @method static Builder|HealingItem whereHeal($value)
+ * @method static Builder|HealingItem whereItem($value)
+ * @method static Builder|HealingItem whereItemId($value)
+ * @method static Builder|HealingItem wherePrice($value)
+ *
  * @mixin \Eloquent
  */
 class HealingItem extends Model
--- app/Models/HealingItemRequired.php
+++ app/Models/HealingItemRequired.php
@@ -3,20 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
+ * App\Models\HealingItemRequired
+ *
  * @property int $item_id
  * @property string $required_item
- * @property string $amount
- * @property HealingItem $healing_item
+ * @property int $amount
+ * @property int $id
+ * @property-read \App\Models\HealingItem|null $healingItem
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|HealingItemRequired newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|HealingItemRequired newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|HealingItemRequired query()
+ * @method static \Illuminate\Database\Eloquent\Builder|HealingItemRequired whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HealingItemRequired whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HealingItemRequired whereItemId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HealingItemRequired whereRequiredItem($value)
+ *
  * @mixin \Eloquent
  */
 class HealingItemRequired extends Model
 {
     public $timestamps = false;
+
     public $table = 'healing_items_required';
 
-    public function healingItem()
+    public function healingItem(): BelongsTo
     {
         return $this->belongsTo(HealingItem::class);
     }
