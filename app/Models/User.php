05d1c9f5e517aac53c72c60c725721400ac2fd77
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-02-03 19:00:58 +0100

    Add relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
@@ -15,7 +16,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  * @property string|null $remember_token
  * @property string|null $created_at
  * @property string|null $updated_at
- *
+ * @property-read \App\Models\UserLevels|null $userLevels
+ * @property-read \App\Models\UserData|null $user_data
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
@@ -23,11 +25,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- *
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUsername($value)
  * @mixin \Eloquent
  */
 class User extends Authenticatable
@@ -36,8 +37,13 @@ class User extends Authenticatable
 
     public $timestamps = false;
 
-    public function user_data()
+    public function user_data(): HasOne
     {
         return $this->hasOne(UserData::class, 'username', 'username');
+    }
+
+    public function userLevels(): HasOne
+    {
+        return $this->hasOne(UserLevels::class);
     }
 }
