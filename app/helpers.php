8b0565adcc019b601301f87a73826e877e4f9e39
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-02-07 23:40:02 +0100

    Add helper file with helper function for advResponse

---
+++ app/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Responses\AdvResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+if (! function_exists('advResponse')) {
+    /**
+     * @param  array<string, mixed>  $data
+     * @param  int  $status
+     * @param  array<string, mixed>  $headers
+     * @return \App\Http\Responses\AdvResponse
+     */
+    function advResponse($data, $status = Response::HTTP_OK, $headers = [])
+    {
+        return new AdvResponse($data, $status, $headers);
+    }
+}
