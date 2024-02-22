928b4b96990f3f5c8f7359bca74af857c112ad0c
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-02-22 18:20:24 +0100

    New docblocks

--- app/Models/Crop.php
+++ app/Models/Crop.php
@@ -4,6 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Crop
+ *
+ * @property string $crop_type
+ * @property int $farmer_level
+ * @property int $time
+ * @property int $experience
+ * @property int $seed_required
+ * @property string $seed_item
+ * @property int $min_crop_count
+ * @property int $max_crop_count
+ * @property string $location
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereCropType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereExperience($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereFarmerLevel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereMaxCropCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereMinCropCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereSeedItem($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereSeedRequired($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Crop whereTime($value)
+ * @mixin \Eloquent
+ */
 class Crop extends Model
 {
     public $timestamps = false;
--- app/Models/Farmer.php
+++ app/Models/Farmer.php
@@ -2,18 +2,32 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
- * Class Farmer
- * @property int $id
+ * App\Models\Farmer
+ *
  * @property string $username
- * @property Carbon $crop_finishes_at
+ * @property int $fields
+ * @property string|null $crop_type
  * @property int $crop_quant
+ * @property \Carbon\CarbonInterface|null $crop_finishes_at
  * @property string $location
- * @property FarmerWorkforce $workforce
+ * @property int $id
+ * @property int $user_id
+ * @property-read \App\Models\FarmerWorkforce|null $workforce
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereCropFinishesAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereCropQuant($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereCropType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereFields($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Farmer whereUsername($value)
  * @mixin \Eloquent
  */
 class Farmer extends Model
@@ -24,11 +38,12 @@ class Farmer extends Model
 
     protected $guarded = [];
 
-    protected $dates = ['crop_finishes_at'];
-
+    protected $casts = [
+        'crop_finishes_at' => 'datetime',
+    ];
 
     public function workforce(): HasOne
     {
-        return $this->hasOne(FarmerWorkforce::class, 'username', 'username');
+        return $this->hasOne(FarmerWorkforce::class, 'user_id', 'user_id');
     }
 }
