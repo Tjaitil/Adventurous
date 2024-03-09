ca48775eba179ed897364016c2b568cc5a26f1af
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-03-09 23:59:49 +0100

    Add pinia store and default component

--- resources/js/ui/main.ts
+++ resources/js/ui/main.ts
@@ -1,7 +1,16 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import AppVue from './components/App.vue';
 
+import SkillInfoList from './components/skills/SkillInfoList.vue';
+
+const components = {
+    SkillInfoList,
+};
+
 document.querySelectorAll('.vue-app').forEach(element => {
-    const app = createApp({ AppVue, components: {} });
-    app.mount(element);
+    const app = createApp({ AppVue, components });
+    const pinia = createPinia();
+
+    app.use(pinia).mount(element);
 });
---
+++ resources/js/ui/stores/SkillsStore.ts
@@ -0,0 +1,36 @@
+import { UserLevelsResource } from '@/types/UserLevelsResource';
+import { defineStore } from 'pinia';
+
+interface State {
+    handleXpGainedEvent: boolean;
+    UserLevelsResource?: UserLevelsResource;
+}
+
+export const useSkillsStore = defineStore('skillsStore', {
+    state: (): State => ({
+        handleXpGainedEvent: false,
+    }),
+    actions: {
+        setHandleXpGainedEvent(status: boolean) {
+            this.handleXpGainedEvent = status;
+        },
+        setUserLevelsResource(resource: UserLevelsResource) {
+            this.UserLevelsResource = resource;
+        },
+        hasRequiredMinerLevel(levelRequired: number) {
+            return this.UserLevelsResource?.miner_level > levelRequired;
+        },
+        hasRequiredFarmerLevel(levelRequired: number) {
+            return this.UserLevelsResource?.farmer_level > levelRequired;
+        },
+        hasRequiredAdventurerRespect(levelRequired: number) {
+            return this.UserLevelsResource?.adventurer_respect > levelRequired;
+        },
+        hasRequiredTraderLevel(levelRequired: number) {
+            return this.UserLevelsResource?.trader_level > levelRequired;
+        },
+        hasRequiredWarriorLevel(levelRequired: number) {
+            return this.UserLevelsResource?.warrior_level > levelRequired;
+        },
+    },
+});
