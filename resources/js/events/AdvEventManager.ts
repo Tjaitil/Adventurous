7901ae6d9465790fb5a045d0559d764e4ed39263
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-03-09 23:56:03 +0100

    Add an event manager system

---
+++ resources/js/events/AdvEventManager.ts
@@ -0,0 +1,5 @@
+import { createGameEventManager } from '@/lib/GameEventManager';
+
+type AdvEvents = {};
+
+export const AdvEventManager = createGameEventManager<AdvEvents>({});
---
+++ resources/js/lib/GameEventManager.ts
@@ -0,0 +1,31 @@
+export const createGameEventManager = <T extends GameEventsMapping>(
+    events: T,
+): GameEventManager<T> => {
+    return {
+        events: events,
+        handle(event, data: object = {}) {
+            if (this.events[event]) {
+                this.events[event](data);
+            }
+        },
+        notify(event) {
+            if (Array.isArray(event)) {
+                events.forEach(event => {
+                    this.handle(event);
+                });
+            } else {
+                this.handle(event);
+            }
+        },
+    };
+};
+
+interface GameEventManager<T> {
+    events: T;
+    handle(event: keyof T, data?: object): void;
+    notify(event: keyof T | Array<keyof T>): void;
+}
+
+type GameEventsMapping = {
+    [key: string]: CallableFunction;
+};
