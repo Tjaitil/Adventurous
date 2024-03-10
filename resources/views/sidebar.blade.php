420c9fdd07e63202184263669a256a0d386f42fc
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-03-10 12:44:47 +0100

    Use vue components instead of blade

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,6 +1,6 @@
-<div id="sidebar"
-    class="h-full pt-2 bg-primary-800 px-2 text-white transition-all duration-200">
-    <button id="sidebar_button_toggle" class="sidebar_button rounded-md text-black bg-orange-50 border-2 shadow p-2
+<div id="sidebar" class="h-full pt-2 bg-primary-800 px-2 text-white transition-all duration-200">
+    <button id="sidebar_button_toggle"
+        class="sidebar_button rounded-md text-black bg-orange-50 border-2 shadow p-2
         font-bold text-xs float-right invisible outline-none">
         {{ '<<' }} </button>
     <p>{{ ucfirst($username) }}</p>
@@ -11,29 +11,23 @@
             <x-tab id="log-tab" aria-controls="sidebar-log-tabpanel" class="sidebar-tab">
                 Log
             </x-tab>
-            <x-tab id="adventure-tab" aria-controls="sidebar-adventure-tabpanel"
-                class="sidebar-tab">
+            <x-tab id="adventure-tab" aria-controls="sidebar-adventure-tabpanel" class="sidebar-tab">
                 Adventure
             </x-tab>
-            <x-tab id="countdown-tab" aria-controls="sidebar-countdowns-tabpanel"
-                class="sidebar-tab">
+            <x-tab id="countdown-tab" aria-controls="sidebar-countdowns-tabpanel" class="sidebar-tab">
                 Countdowns
             </x-tab>
-            <x-tab id="diplomacy-tab" aria-controls="sidebar-diplomacy-tabpanel"
-                class="sidebar-tab">
+            <x-tab id="diplomacy-tab" aria-controls="sidebar-diplomacy-tabpanel" class="sidebar-tab">
                 Diplomacy
             </x-tab>
             <x-tab id="skills-tab" aria-controls="sidebar-skills-tabpanel" class="sidebar-tab">
                 Skills
             </x-tab>
             <x-tab id="help-tab" aria-controls="help-settings-tabPanel" class="sidebar-tab">
-                <img class="mx-auto w-12 h-12" id="HUD_help_button"
-                    src="{{ asset('images/help icon.png') }}" />
+                <img class="mx-auto w-12 h-12" id="HUD_help_button" src="{{ asset('images/help icon.png') }}" />
             </x-tab>
-            <x-tab id="client-settings-tab" aria-controls="sidebar-settings-tabPanel"
-                class="sidebar-tab">
-                <img class="mx-auto w-12 h-12" id="setting_button"
-                    src="{{ asset('images/settings icon.png') }}" />
+            <x-tab id="client-settings-tab" aria-controls="sidebar-settings-tabPanel" class="sidebar-tab">
+                <img class="mx-auto w-12 h-12" id="setting_button" src="{{ asset('images/settings icon.png') }}" />
             </x-tab>
         </x-tabList>
         <div id="sidebar-tabpanels" class="flex-grow overflow-y-scroll h-full max-h-[600px]">
@@ -52,7 +46,9 @@
                 ?>
             </x-tabpanel>
             <x-tabpanel id="sidebar-skills-tabpanel" aria-labelled-by="skills-tab">
-                <x-skillLevels.skillLevelsContainer :levels="$Levels" />
+                <div class="vue-app">
+                    <skill-info-list :init-levels="{{ json_encode($Levels) }}" />
+                </div>
             </x-tabpanel>
             <x-tabPanel id="help-settings-tabPanel" aria-labelled-by="help-settings-tab">
                 @include('partials.clientHelp')
