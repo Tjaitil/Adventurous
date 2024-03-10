88115885638d5886d9d0724815e07b655ad7f93d
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-03-10 12:40:37 +0100

    Check for events and lint

--- resources/js/AdvApi.ts
+++ resources/js/AdvApi.ts
@@ -1,51 +1,74 @@
 import { advAPIResponse } from './types/Responses/AdvResponse';
-import { BaseAxios } from "./ajax";
+import { BaseAxios } from './ajax';
 import { GameLogger } from './utilities/GameLogger';
+import { AdvEventManager } from './events/AdvEventManager';
 
 export class AdvApi extends BaseAxios {
-
     protected static interceptorsConfigured = false;
 
     private static init() {
-        BaseAxios.getInstance().interceptors.response.use(response => {
+        BaseAxios.getInstance().interceptors.response.use(
+            response => {
+                if (response.status !== 200) {
+                    GameLogger.addMessage('An Error Occured', true);
+                    return Promise.reject(response);
+                } else if (Array.isArray(response.data.gameMessage) === true) {
+                    GameLogger.addMessages(response.data.gameMessage, true);
+                    GameLogger.logMessages();
+                    return Promise.resolve(response);
+                } else if (
+                    typeof response.data.errorGameMessage !== 'undefined'
+                ) {
+                    GameLogger.addMessage(response.data.errorGameMessage, true);
+                    return Promise.reject(response.data.gameMessage);
+                }
 
-            if(response.status !== 200) {
-                GameLogger.addMessage("An Error Occured", true);
-                return Promise.reject(response);
-            }
+                if (Array.isArray(response.data.gameEvents)) {
+                    AdvEventManager.notify(response.data.gameEvents);
+                }
 
-            else if (Array.isArray(response.data.gameMessage) === true) {
-                GameLogger.addMessages(response.data.gameMessage, true);
-                GameLogger.logMessages()
-                return Promise.resolve(response);
-            } else if (typeof response.data.errorGameMessage !== "undefined") {
-                GameLogger.addMessage(response.data.errorGameMessage, true);
-                return Promise.reject(response.data.gameMessage);
-            }   
+                if (
+                    Array.isArray(response.data.levelUp) &&
+                    response.data.levelUp.length > 0
+                ) {
+                    GameLogger.addMessage('Level Up!', false);
+                    response.data.levelUp.forEach(skill => {
+                        GameLogger.addMessage(
+                            `You have gained a level in ${skill.name}!`,
+                            false,
+                        );
+                    });
+                }
 
-            return response;
-        }, error => {
-            if (Array.isArray(error.response.data.gameMessage) === true) {
-                GameLogger.addMessages(error.response.data.gameMessage, true);
-                GameLogger.logMessages()
-            } 
+                return response;
+            },
+            error => {
+                if (Array.isArray(error.response.data.gameMessage) === true) {
+                    GameLogger.addMessages(
+                        error.response.data.gameMessage,
+                        true,
+                    );
+                    GameLogger.logMessages();
+                }
 
-            return Promise.reject(error);
-        })
+                return Promise.reject(error);
+            },
+        );
         this.interceptorsConfigured = true;
     }
 
     public static async get<T = advAPIResponse>(url: string): Promise<T> {
-        if(this.interceptorsConfigured === false) this.init();
+        if (this.interceptorsConfigured === false) this.init();
 
         return BaseAxios.get<T>(url);
     }
 
-    public static async post<T = advAPIResponse>(url: string, data: Object): Promise<T> {
-        if(this.interceptorsConfigured === false) this.init();
+    public static async post<T = advAPIResponse>(
+        url: string,
+        data: object,
+    ): Promise<T> {
+        if (this.interceptorsConfigured === false) this.init();
 
         return BaseAxios.post<T>(url, data);
     }
 }
-
-
