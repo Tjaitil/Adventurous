fe54539909a5d92e7b64b5fb432dba0c1905a274
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-03-10 11:09:30 +0100

    Linting and remove unused properties

--- resources/js/advclient.ts
+++ resources/js/advclient.ts
@@ -1,54 +1,55 @@
 import { SetWorldRequest } from './types/requests/WorldLoaderRequests';
-import { ajaxP } from "./ajax";
-import { loadingCanvas } from "./clientScripts/canvasText";
-import { ClientOverlayInterface } from "./clientScripts/clientOverlayInterface";
-import { collisionCheck } from "./clientScripts/collision";
-import { controls } from "./clientScripts/controls";
-import { conversation } from "./clientScripts/conversation";
-import { eventHandler } from "./clientScripts/gameEventHandler";
-import { GamePieces } from "./clientScripts/gamePieces";
-import { HUD } from "./clientScripts/HUD";
-import { itemPrices } from "./clientScripts/inventory";
-import { Map } from "./clientScripts/map";
-import { pauseManager } from "./clientScripts/pause";
-import { tutorial } from "./clientScripts/tutorial";
-import viewport from "./clientScripts/viewport";
+import { ajaxP } from './ajax';
+import { loadingCanvas } from './clientScripts/canvasText';
+import { ClientOverlayInterface } from './clientScripts/clientOverlayInterface';
+import { collisionCheck } from './clientScripts/collision';
+import { controls } from './clientScripts/controls';
+import { conversation } from './clientScripts/conversation';
+import { eventHandler } from './clientScripts/gameEventHandler';
+import { GamePieces } from './clientScripts/gamePieces';
+import { HUD } from './clientScripts/HUD';
+import { itemPrices } from './clientScripts/inventory';
+import { Map } from './clientScripts/map';
+import { pauseManager } from './clientScripts/pause';
+import { tutorial } from './clientScripts/tutorial';
+import viewport from './clientScripts/viewport';
 import { CustomFetchApi } from './CustomFetchApi';
-import { GameProperties, loadWorldParamters } from "./types/Advclient";
-import { getRandomInteger } from "./utilities/getRandomInteger";
-import { jsUcWords } from "./utilities/uppercase";
+import { GameProperties, loadWorldParamters } from './types/Advclient';
+import { jsUcWords } from './utilities/uppercase';
 import { setUpTabList } from './utilities/tabs';
 import { AssetPaths } from './clientScripts/ImagePath';
 import { GetWorldResponse } from './types/Responses/WorldLoaderResponse';
 
 const CookieTicket = {
-    checkCookieTicket(cookieNoob = "getOut") {
-        var today = new Date();
-        var cookieTicket;
+    checkCookieTicket(cookieNoob = 'getOut') {
+        const today = new Date();
+        let cookieTicket;
         if (CookieTicket.sweetCookie === null) {
-            cookieTicket = today.getMonth() + today.getDate() + "|";
-            for (var i = 0; i < 10; i++) {
+            cookieTicket = today.getMonth() + today.getDate() + '|';
+            for (let i = 0; i < 10; i++) {
                 cookieTicket += Math.floor(Math.random() * (10 - 1) + 1);
             }
             CookieTicket.sweetCookie = cookieTicket;
         } else {
             cookieTicket = CookieTicket.sweetCookie;
         }
-        let data =
-            "model=cookieMaker" +
-            "&method=yummyCookies" +
-            "&cookieTicket=" +
+        const data =
+            'model=cookieMaker' +
+            '&method=yummyCookies' +
+            '&cookieTicket=' +
             cookieTicket +
-            "&cookieNoob=" +
+            '&cookieNoob=' +
             cookieNoob;
         ajaxP(data, function (response) {
             console.log(response);
-            let responseText = response[1].status;
-            if (responseText.status === "false") {
+            const responseText = response[1].status;
+            if (responseText.status === 'false') {
                 // Session check return false, go to logout
-                alert("A newer session is ongoing. You are being logged out from this session");
+                alert(
+                    'A newer session is ongoing. You are being logged out from this session',
+                );
                 setTimeout(() => {
-                    location.href = "/logout";
+                    location.href = '/logout';
                 }, 5000);
             } else {
                 return;
@@ -56,15 +57,19 @@ const CookieTicket = {
         });
     },
     disposeGarbage(event) {
-        let ego = event.target.innerText;
-        if (ego === "") {
+        const ego = event.target.innerText;
+        if (ego === '') {
             this.checkCookieTicket();
         } else {
             this.burntCookie();
         }
     },
     burntCookie() {
-        let data = "model=cookieMaker" + "&method=delicCookies" + "&cookieTicket=" + CookieTicket.sweetCookie;
+        const data =
+            'model=cookieMaker' +
+            '&method=delicCookies' +
+            '&cookieTicket=' +
+            CookieTicket.sweetCookie;
         ajaxP(data, function (response) {
             console.log(response);
             if (response[1] == false) {
@@ -77,19 +82,6 @@ const CookieTicket = {
     },
     sweetCookie: null,
 };
-var click = 0;
-const FPS_TRACKER = {};
-function doubleClickDetect() {
-    click++;
-    if (click == 2) {
-        click = 0;
-        return true;
-    } else {
-        setTimeout(function () {
-            click = 0;
-        }, 300);
-    }
-}
 
 export class Game {
     public static properties: GameProperties = {
@@ -99,16 +91,16 @@ export class Game {
         timestamp: 0,
         xbase: 320,
         ybase: 200,
-        currentMap: "0.0",
-        gameState: "loading",
+        currentMap: '0.0',
+        gameState: 'loading',
         // Scale is a variable which compensates for the canvas being zoomed in so that objects drawn on canvas will follow the background.
         // 1 is normal then the picture will be painted in 1024 width and height.
-        device: "pc",
-        building: "none",
+        device: 'pc',
+        building: 'none',
         inBuilding: false,
-        checkingPerson: "none",
+        checkingPerson: 'none',
         delta: 0,
-    }
+    };
 
     public static getProperty(val: keyof GameProperties) {
         return this.properties[val];
@@ -128,7 +120,16 @@ export class Game {
 
     public static setGameState(state: string) {
         // TODO: Fix enum here
-        if (["playing", "conversation", "loading", "help", "map", "pause"].indexOf(state) === -1) {
+        if (
+            [
+                'playing',
+                'conversation',
+                'loading',
+                'help',
+                'map',
+                'pause',
+            ].indexOf(state) === -1
+        ) {
             return false;
         }
         this.properties.gameState = state;
@@ -137,59 +138,64 @@ export class Game {
     public static async getWorld() {
         this.pauseWorld();
 
-        await CustomFetchApi.get<GetWorldResponse>('/worldloader').then((response) => {
-            this.loadWorld(response);
-        });
+        await CustomFetchApi.get<GetWorldResponse>('/worldloader').then(
+            response => {
+                this.loadWorld(response);
+            },
+        );
     }
 
     public static async setWorld(parameters: loadWorldParamters = {}) {
         this.pauseWorld();
 
-        let data: SetWorldRequest = {
+        const data: SetWorldRequest = {
             is_new_map_string: false,
             new_map: parameters.newMap,
-            new_destination: parameters.newDestination
+            new_destination: parameters.newDestination,
         };
-
 
         if (parameters.newDestination !== undefined) {
             data.is_new_map_string = true;
         }
 
-        await CustomFetchApi.post<GetWorldResponse>('/worldloader/change', data).then(async (response) => {
+        await CustomFetchApi.post<GetWorldResponse>(
+            '/worldloader/change',
+            data,
+        ).then(async response => {
             await this.loadWorld(response, parameters);
         });
-
     }
 
     private static pauseWorld() {
         window.cancelAnimationFrame(Game.properties.requestId);
-        Game.setGameState("loading");
+        Game.setGameState('loading');
 
         GamePieces.player.travel = false;
-        if (loadingCanvas.opacity !== 1) loadingCanvas.loadingAnimationTracker.start("close");
+        if (loadingCanvas.opacity !== 1)
+            loadingCanvas.loadingAnimationTracker.start('close');
 
         GamePieces.reset();
     }
 
-    public static async loadWorld(response: GetWorldResponse, parameters: loadWorldParamters = {}) {
-
-        let data = response.data;
+    public static async loadWorld(
+        response: GetWorldResponse,
+        parameters: loadWorldParamters = {},
+    ) {
+        const data = response.data;
         Game.properties.currentMap = data.current_map;
         if (data.changed_location) {
-            document.title = jsUcWords(data.changed_location.replace("-", " "));
+            document.title = jsUcWords(data.changed_location.replace('-', ' '));
         }
         // for (let x = 0; x < data.events.length; x++) {
         //     eventHandler.events.push(data.events[x]);
         // }
         function findStartPoint(object) {
-            return object.type === "start_point";
+            return object.type === 'start_point';
         }
         function removeStartPoint(object) {
-            return object.type !== "start_point";
+            return object.type !== 'start_point';
         }
-        let startPoints = data.map_data.objects.filter(findStartPoint);
-        let startPoint = getRandomInteger(0, startPoints.length);
+        const startPoints = data.map_data.objects.filter(findStartPoint);
         GamePieces.objects = GamePieces.objects.filter(removeStartPoint);
 
         if (parameters.newxBase) {
@@ -202,25 +208,37 @@ export class Game {
         if (parameters.newyBase) {
             // Legge til ybase i JSON map filene
             Game.properties.ybase = parameters.newyBase;
-        }
-        else if (startPoints.length > 0) {
+        } else if (startPoints.length > 0) {
             Game.properties.ybase = startPoints[0].y;
         }
 
-        let worldMapSrc = AssetPaths.getImagePath(Game.properties.currentMap + ".png");
-        viewport.adjustViewport(Game.properties.xbase, Game.properties.ybase, worldMapSrc);
+        const worldMapSrc = AssetPaths.getImagePath(
+            Game.properties.currentMap + '.png',
+        );
+        viewport.adjustViewport(
+            Game.properties.xbase,
+            Game.properties.ybase,
+            worldMapSrc,
+        );
 
-        GamePieces.loadAssets(Game.properties.xbase, Game.properties.ybase, data.map_data);
+        GamePieces.loadAssets(
+            Game.properties.xbase,
+            Game.properties.ybase,
+            data.map_data,
+        );
         viewport.checkViewportGamePieces(true);
         // viewport.init();
 
         Map.load(Game.properties.currentMap);
 
         setTimeout(() => {
-            loadingCanvas.loadingAnimationTracker.start("open");
+            loadingCanvas.loadingAnimationTracker.start('open');
             Game.startGame();
-            if (parameters.method !== "changeMap" && Game.properties.currentMap == "9.9") {
-                if (Game.properties.currentMap == "9.9") {
+            if (
+                parameters.method !== 'changeMap' &&
+                Game.properties.currentMap == '9.9'
+            ) {
+                if (Game.properties.currentMap == '9.9') {
                     tutorial.startTutorial();
                 }
             }
@@ -234,20 +252,32 @@ export class Game {
         let newyBase = GamePieces.player.ypos;
         let match = false;
 
-        if (GamePieces.player.diameterDown > 3170 && GamePieces.player.direction.indexOf("down") !== -1) {
+        if (
+            GamePieces.player.diameterDown > 3170 &&
+            GamePieces.player.direction.indexOf('down') !== -1
+        ) {
             newY += 1;
             newyBase = 1;
             match = true;
-        } else if (GamePieces.player.diameterUp < 50 && GamePieces.player.direction.indexOf("up") !== -1) {
+        } else if (
+            GamePieces.player.diameterUp < 50 &&
+            GamePieces.player.direction.indexOf('up') !== -1
+        ) {
             newY -= 1;
             newyBase = 3158;
             match = true;
         }
-        if (GamePieces.player.xpos > 3170 && GamePieces.player.direction.indexOf("right") !== -1) {
+        if (
+            GamePieces.player.xpos > 3170 &&
+            GamePieces.player.direction.indexOf('right') !== -1
+        ) {
             newX += 1;
             newxBase = 1;
             match = true;
-        } else if (GamePieces.player.diameterLeft < 50 && GamePieces.player.direction.indexOf("left") !== -1) {
+        } else if (
+            GamePieces.player.diameterLeft < 50 &&
+            GamePieces.player.direction.indexOf('left') !== -1
+        ) {
             newX -= 1;
             newxBase = 3158;
             match = true;
@@ -261,7 +291,7 @@ export class Game {
             this.setWorld({
                 newxBase: newxBase,
                 newyBase: newyBase,
-                method: "changeMap",
+                method: 'changeMap',
                 newMap: { newX, newY },
             });
         }
@@ -274,20 +304,27 @@ export class Game {
         ClientOverlayInterface.setup();
 
         viewport.setup({
-            background: document.getElementById("game_canvas") as HTMLCanvasElement,
-            player: document.getElementById("game_canvas2") as HTMLCanvasElement,
-            frontObjects: document.getElementById("game_canvas4") as HTMLCanvasElement,
-            sprite: document.getElementById("game_canvas3") as HTMLCanvasElement,
-            text: document.getElementById("text_canvas") as HTMLCanvasElement,
-            hud: document.getElementById("hud_canvas") as HTMLCanvasElement,
-
+            background: document.getElementById(
+                'game_canvas',
+            ) as HTMLCanvasElement,
+            player: document.getElementById(
+                'game_canvas2',
+            ) as HTMLCanvasElement,
+            frontObjects: document.getElementById(
+                'game_canvas4',
+            ) as HTMLCanvasElement,
+            sprite: document.getElementById(
+                'game_canvas3',
+            ) as HTMLCanvasElement,
+            text: document.getElementById('text_canvas') as HTMLCanvasElement,
+            hud: document.getElementById('hud_canvas') as HTMLCanvasElement,
         });
         // Initial loading screen
         loadingCanvas.loadingScreen();
 
         this.loadGame();
         setTimeout(() => {
-            document.getElementById("client-container").style.opacity = "" + 1;
+            document.getElementById('client-container').style.opacity = '' + 1;
         }, 500);
     }
 
@@ -306,8 +343,9 @@ export class Game {
         pauseManager.resumeGame(true);
     }
 
-    public static update(timestamp, first = false) {
-        Game.properties.delta = (timestamp - Game.properties.timestamp) / 1000 / viewport.zoom;
+    public static update(timestamp) {
+        Game.properties.delta =
+            (timestamp - Game.properties.timestamp) / 1000 / viewport.zoom;
         if (Game.properties.delta > 0.08) {
             Game.properties.delta = Math.round(0.16 / viewport.zoom) * 2;
         }
@@ -322,7 +360,7 @@ export class Game {
         // }
         // console.log("FPS: " + Math.round(1 / secondsPassed));
 
-        if (Game.properties.gameState !== "playing") {
+        if (Game.properties.gameState !== 'playing') {
             return false;
         }
 
@@ -339,7 +377,6 @@ export class Game {
             GamePieces.player.speedY = GamePieces.player.speed;
         } else {
             GamePieces.player.speedY = 0;
-
         }
         viewport.resetSpriteLayer();
         if (
@@ -356,11 +393,18 @@ export class Game {
         }
 
         GamePieces.player.xTracker = GamePieces.player.xMovement +=
-            Math.round(GamePieces.player.movementSpeed * Game.properties.delta) * GamePieces.player.speedX;
+            Math.round(
+                GamePieces.player.movementSpeed * Game.properties.delta,
+            ) * GamePieces.player.speedX;
         GamePieces.player.yTracker = GamePieces.player.yMovement +=
-            Math.round(GamePieces.player.movementSpeed * Game.properties.delta) * GamePieces.player.speedY;
+            Math.round(
+                GamePieces.player.movementSpeed * Game.properties.delta,
+            ) * GamePieces.player.speedY;
 
-        viewport.drawBackground(GamePieces.player.xMovement, GamePieces.player.yMovement);
+        viewport.drawBackground(
+            GamePieces.player.xMovement,
+            GamePieces.player.yMovement,
+        );
 
         GamePieces.drawStaticPieces();
         GamePieces.drawDaqloons();
@@ -371,6 +415,6 @@ export class Game {
         Game.properties.duration++;
         Game.properties.requestId = window.requestAnimationFrame(Game.update);
     }
-};
+}
 
-window.addEventListener("load", () => Game.setup());
+window.addEventListener('load', () => Game.setup());
