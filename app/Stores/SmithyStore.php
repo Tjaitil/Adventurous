7cec9a3c770423db56a4116362a971b6790efabf
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-03-18 22:27:18 +0100

    Add smithystore

---
+++ app/Stores/SmithyStore.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Stores;
+
+use App\Abstracts\AbstractStore;
+use App\Http\Resources\StoreResource;
+use App\Models\SmithyItem;
+use App\Services\StoreDiscountService;
+
+class SmithyStore extends AbstractStore
+{
+    public function __construct(
+        public StoreDiscountService $storeDiscountService
+    ) {
+        parent::__construct();
+    }
+
+    public function makeStore(array $items = []): StoreResource
+    {
+        $items = SmithyItem::with('requiredItems')
+            ->when(count($items) > 0, fn ($query) => $query->whereIn('item', $items))
+            ->get();
+
+        $items = $items->sortBy(function ($value, $key) {
+            $minLevel = 1;
+            $value->skillRequirements->each(function ($skillRequirement) use (&$minLevel) {
+                if ($skillRequirement->level > $minLevel) {
+                    $minLevel = $skillRequirement->level;
+                }
+            });
+
+            return $minLevel;
+        });
+
+        return $this->StoreResource = $this->storeBuilder::create(['store_items' => $items])
+            ->setAdjustedStoreValue($this->storeDiscountService->getDiscount('smithy'))
+            ->setStoreName('smithy')
+            ->setInfiniteAmount(true)
+            ->build();
+    }
+}
