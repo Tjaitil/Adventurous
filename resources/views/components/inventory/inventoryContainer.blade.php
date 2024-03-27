49e925b8827595c52f544a2c90602a65bf50fa96
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-03-27 13:00:51 +0100

    Margin change

--- resources/views/components/inventory/inventoryContainer.blade.php
+++ resources/views/components/inventory/inventoryContainer.blade.php
@@ -1,6 +1,6 @@
 @props(['inventory'])
 <x-borderInterfaceContainer id="inventory" class="w-[29%] float-right h-[600px] mt-0 z-20">
-    <p class="text-white">Inventory <span id="inventory-status">
+    <p class="text-white mb-4">Inventory <span id="inventory-status">
             {{ '(' . count($inventory) . ' / ' . '18' . ')' }}</span>
     </p>
     <x-inventory.inventoryItems :$inventory />
