cad50d45342bb0b18e38a66b4338ba382db494fa
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-08-13 22:46:56 +0200

    refactor: use baks-card

--- resources/views/components/inventory/inventoryContainer.blade.php
+++ resources/views/components/inventory/inventoryContainer.blade.php
@@ -3,9 +3,11 @@
     $inventoryCapacity = 18;
     $isInventoryFull = count($inventory) === $inventoryCapacity;
 @endphp
-<x-borderInterfaceContainer id="inventory" class="w-[29%] float-right h-[600px] mt-0 z-20">
-    <p class="text-white mb-4">Inventory <span id="inventory-status" @class(['not-able-color' => $isInventoryFull])>
-            {{ sprintf('(%d / %d)', count($inventory), $inventoryCapacity) }}</span>
-    </p>
-    <x-inventory.inventoryItems :$inventory />
-</x-borderInterfaceContainer>
+<div class="w-[29%] float-right h-[600px] mt-0 z-20">
+    <baks-card variant="dark" id="inventory">
+        <p class="mb-4">Inventory <span id="inventory-status" @class(['not-able-color' => $isInventoryFull])>
+                {{ sprintf('(%d / %d)', count($inventory), $inventoryCapacity) }}</span>
+        </p>
+        <x-inventory.inventoryItems :$inventory />
+    </baks-card>
+</div>
