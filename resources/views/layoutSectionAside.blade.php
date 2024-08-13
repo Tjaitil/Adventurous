9cad6c0d5e85069e1f4e2e7b90e96cc95b294d48
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-08-13 22:48:56 +0200

    feat: add pixelated corner and other minor style changes

--- resources/views/layoutSectionAside.blade.php
+++ resources/views/layoutSectionAside.blade.php
@@ -1,13 +1,13 @@
 @extends('app')
 @section('content')
-<section class="col-span-5 col-start-2 row-start-2 max-h-[800px]">
-    @yield('sectionContent')
-</section>
-<aside class="col-span-1 col-start-1 row-start-2 max-h-[800px] z-20 relative">
-    @yield('asideContent')
-    @vite('resources/js/clientScripts/sidebar.ts')
-</aside>
-<footer class="col-span-5 col-start-2 row-start-3">
-    Delevoped by Kjetil Baksaas
-</footer>
+    <section class="col-span-5 col-start-2 row-start-2 max-h-[800px] py-2 px-2">
+        @yield('sectionContent')
+    </section>
+    <aside class="col-span-1 col-start-1 row-start-2 max-h-[800px] z-20 relative">
+        @yield('asideContent')
+        @vite('resources/js/clientScripts/sidebar.ts')
+    </aside>
+    <footer class="col-span-5 col-start-2 row-start-3">
+        Delevoped by Kjetil Baksaas
+    </footer>
 @endsection
--- resources/views/partials/gameScreen.blade.php
+++ resources/views/partials/gameScreen.blade.php
@@ -1,7 +1,7 @@
-<div id="game-screen">
+<div id="game-screen" class="relative">
     <div id="canvas-border"
-        class="absolute z-[7] w-[700px] h-[400px] rounded border-4 border-primary-700 after:pointer-events-none after:absolute after:left-[-1px] after:top-[-1px] after:h-[calc(100%+2px)] after:w-[calc(100%+2px)] after:rounded after:border-4 after:border-solid after:border-gray-950 after:content-['']"
-    ></div>
+        class="absolute z-[7] top-[-6px] w-[700px] h-[400px] border-8 border-primary-700 after:pointer-events-none after:absolute after:left-[-1px] after:top-[-1px] after:h-[calc(100%+2px)] after:w-[calc(100%+2px)] after:border-4 after:border-solid after:border-gray-950/60 after:content-[''] pixelated-corners">
+    </div>
     <x-client.canvas id="game_canvas" />
     <x-client.canvas id="game_canvas2" />
     <x-client.canvas id="game_canvas3" />
