f81db066b3e2044fd18fc57940945211a8af14aa
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-08-13 22:46:38 +0200

    refactor: use baks card

--- resources/views/components/borderInterfaceContainer.blade.php
+++ resources/views/components/borderInterfaceContainer.blade.php
@@ -1,12 +1,14 @@
-@props(['borderStyle' => 'brown'])
+@props(['borderStyle' => 'brown', 'variant' => 'dark'])
 @php
-    if($borderStyle === 'orange') {
+    if ($borderStyle === 'orange') {
         $color = 'border-yellow-700';
+        $variant = 'light';
     } else {
         $color = 'border-primary-700';
+        $variant = 'dark';
     }
 @endphp
-<div
-    {{ $attributes->merge(['class' => $color . " relative p-2 my-4 bg-primary-800 text-white border-8 border-ridge rounded border-default shadow-lg after:border-gray-950/60 after:pointer-events-none after:absolute after:top-0 after:left-0 after:content-[''] after:w-full after:h-full after:border-solid after:border-4 after:rounded"]) }}>
+<baks-card variant="{{ $variant }}" {{ $attributes }}>
     {!! $slot !!}
+</baks-card>
 </div>
