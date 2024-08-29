fd110b5bd665532d82c69585ab780f2bbc73b04b
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-08-29 20:12:05 +0200

    fix(InventoryContainer): fix missing z-index

--- resources/views/components/inventory/inventoryContainer.blade.php
+++ resources/views/components/inventory/inventoryContainer.blade.php
@@ -3,8 +3,8 @@
     $inventoryCapacity = 18;
     $isInventoryFull = count($inventory) === $inventoryCapacity;
 @endphp
-<div class="w-[29%] float-right h-[600px] mt-0 z-20">
-    <baks-card variant="dark" id="inventory">
+<div class="w-[29%] float-right h-[600px] mt-0">
+    <baks-card variant="dark" id="inventory" class="z-20 relative">
         <p class="mb-4">Inventory <span id="inventory-status" @class(['not-able-color' => $isInventoryFull])>
                 {{ sprintf('(%d / %d)', count($inventory), $inventoryCapacity) }}</span>
         </p>
