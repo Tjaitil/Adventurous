0d6ec233e1b06c748db8ff710fd33ad1dc170686
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-08-31 00:52:55 +0200

    fix(InventoryContainer): fix issue where div would be duplicated

--- resources/views/components/inventory/inventoryContainer.blade.php
+++ resources/views/components/inventory/inventoryContainer.blade.php
@@ -3,11 +3,9 @@
     $inventoryCapacity = 18;
     $isInventoryFull = count($inventory) === $inventoryCapacity;
 @endphp
-<div class="w-[29%] float-right h-[600px] mt-0">
-    <baks-card variant="dark" id="inventory" class="z-20 relative">
-        <p class="mb-4">Inventory <span id="inventory-status" @class(['not-able-color' => $isInventoryFull])>
-                {{ sprintf('(%d / %d)', count($inventory), $inventoryCapacity) }}</span>
-        </p>
-        <x-inventory.inventoryItems :$inventory />
-    </baks-card>
-</div>
+<baks-card variant="dark" id="inventory" class="z-20 relative w-[29%] float-right h-[600px] mt-0">
+    <p class="mb-4">Inventory <span id="inventory-status" @class(['not-able-color' => $isInventoryFull])>
+            {{ sprintf('(%d / %d)', count($inventory), $inventoryCapacity) }}</span>
+    </p>
+    <x-inventory.inventoryItems :$inventory />
+</baks-card>
