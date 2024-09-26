8cc3703adecc4805d3a0d5928e4a0e9dd079b983
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-09-26 20:51:41 +0200

    feat(BaseLoading): new loading wrapper component

---
+++ resources/js/ui/components/base/BaseLoading.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="isLoading" class="flex justify-center items-center h-full">
+        <slot name="loading-icon">
+            <img
+                src="/images/loading.png"
+                alt="loading icon"
+                class="loading-icon h-12 mx-auto"
+            />
+        </slot>
+    </div>
+    <div v-else>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    isLoading: boolean;
+}
+defineProps<Props>();
+</script>
