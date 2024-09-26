df909cbb71d1941bf297fa371416d91e5268f934
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-09-26 21:40:16 +0200

    feat(InventoryFullException): add message and code

--- app/Exceptions/InventoryFullException.php
+++ app/Exceptions/InventoryFullException.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class InventoryFullException extends Exception
 {
+    public function __construct()
+    {
+        parent::__construct('Inventory is full', 500);
+    }
+
     public function render(Request $request): JsonResponse
     {
         return (new AdvResponse([], 422))->addErrorMessage('Inventory is full')->toResponse($request);
