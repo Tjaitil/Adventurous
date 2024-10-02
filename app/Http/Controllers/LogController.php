02d4e4f2cd0a5704a4c94177ad56689e97d7b4cd
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-10-02 22:00:35 +0200

    docs(LogController): add return type

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class LogController extends Controller
 {
-    public function logFrontendError(Request $request)
+    public function logFrontendError(Request $request): JsonResponse
     {
         $text = $request->input('text');
         Log::error($text);
