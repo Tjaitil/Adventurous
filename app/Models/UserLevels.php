8ceafb7ebff842194d98f79c46bb015e3f169851
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-11-24 00:14:06 +0100

    feat(UserLevels): add factory

--- app/Models/UserLevels.php
+++ app/Models/UserLevels.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -19,13 +20,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property int $trader_xp
  * @property int $warrior_level
  * @property int $warrior_xp
+ * @property int|null $user_id
  * @property-read int $farmer_next_level_xp
  * @property-read int $miner_next_level_xp
  * @property-read int $trader_next_level_xp
  * @property-read int $warrior_next_level_xp
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\LevelData> $nextLevel
  * @property-read int|null $next_level_count
- *
+ * @method static \Database\Factories\UserLevelsFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels query()
@@ -37,14 +39,16 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels whereMinerXp($value)
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels whereTraderLevel($value)
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels whereTraderXp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserLevels whereUserId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels whereUsername($value)
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels whereWarriorLevel($value)
  * @method static \Illuminate\Database\Eloquent\Builder|UserLevels whereWarriorXp($value)
- *
  * @mixin \Eloquent
  */
 class UserLevels extends Model
 {
+    use HasFactory;
+
     protected $appends = [
         'farmer_next_level_xp',
         'miner_next_level_xp',
