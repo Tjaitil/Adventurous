d9057b5e76bd55a54c5a5747dfca0fcc55830093
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-11-23 23:08:49 +0100

    feat(Trader): add factory

--- app/Models/Trader.php
+++ app/Models/Trader.php
@@ -3,23 +3,40 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * 
- * @property int $id
+ *
  * @property string $username
- * @property Carbon $trading_countdown
- * @property int $delivered
- * @property int $cart_amount
  * @property int $assignment_id
  * @property int $cart_id
- * @property TraderAssignment $traderAssignment
- * @property TravelBureauCart $cart
+ * @property int $cart_amount
+ * @property int $delivered
+ * @property string|null $trading_countdown
+ * @property int $user_id
+ * @property int $id
+ * @property-read \App\Models\TravelBureauCart|null $cart
+ * @property-read \App\Models\TraderAssignment|null $traderAssignment
+ * @method static \Database\Factories\TraderFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereAssignmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereCartAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereCartId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereDelivered($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereTradingCountdown($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trader whereUsername($value)
  * @mixin \Eloquent
  */
 class Trader extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     public $table = 'trader';
