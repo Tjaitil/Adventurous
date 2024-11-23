64ee388e58c0f7ea2e085537515a14dfe22e4adc
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-11-23 23:00:44 +0100

    feat(Hunger): add factory

---
+++ database/factories/HungerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hunger>
+ */
+class HungerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'current' => '100',
+            'user_id' => 1,
+        ];
+    }
+}
