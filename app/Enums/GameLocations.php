75b26b7431fc3f46ef6e2840cec3a0c6c967eab6
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-11-23 23:08:06 +0100

    feat(GameLocations): add value function

--- app/Enums/GameLocations.php
+++ app/Enums/GameLocations.php
@@ -4,24 +4,21 @@ namespace App\Enums;
 
 enum GameLocations: string
 {
-    case HIRTAM_LOCATION = "hirtam";
-    case PVITUL_LOCATION = "pvitul";
-    case KHANZ_LOCATION = "khanz";
-    case TER_LOCATION = "ter";
-    case FANSALPLAINS_LOCATION = "fansalplains";
-    case KRASNUR_LOCATION = "krasnur";
-    case TOWHAR_LOCATION = "towhar";
-    case GOLBAK_LOCATION = "golbak";
-    case FAGNA_LOCATION = "fagna";
-    case CRUENDO_LOCATION = "cruendo";
-    case SNERPIIR_LOCATION = "snerpiir";
-    case TASNOBIL_LOCATION = "tasnobil";
-
-
+    case HIRTAM_LOCATION = 'hirtam';
+    case PVITUL_LOCATION = 'pvitul';
+    case KHANZ_LOCATION = 'khanz';
+    case TER_LOCATION = 'ter';
+    case FANSALPLAINS_LOCATION = 'fansalplains';
+    case KRASNUR_LOCATION = 'krasnur';
+    case TOWHAR_LOCATION = 'towhar';
+    case GOLBAK_LOCATION = 'golbak';
+    case FAGNA_LOCATION = 'fagna';
+    case CRUENDO_LOCATION = 'cruendo';
+    case SNERPIIR_LOCATION = 'snerpiir';
+    case TASNOBIL_LOCATION = 'tasnobil';
 
     /**
-     * 
-     * @return string[] 
+     * @return string[]
      */
     public static function getDiplomacyLocations()
     {
@@ -34,10 +31,7 @@ enum GameLocations: string
         ];
     }
 
-
-
     /**
-     * 
      * @return string[]
      */
     public static function getWarriorLocations()
@@ -48,10 +42,7 @@ enum GameLocations: string
         ];
     }
 
-
-
     /**
-     * 
      * @return string[]
      */
     public static function getCropLocations()
@@ -62,10 +53,7 @@ enum GameLocations: string
         ];
     }
 
-
-
     /**
-     * 
      * @return string[]
      */
     public static function getMineLocations()
@@ -73,6 +61,24 @@ enum GameLocations: string
         return [
             self::GOLBAK_LOCATION->value,
             self::SNERPIIR_LOCATION->value,
+        ];
+    }
+
+    public static function values(): array
+    {
+        return [
+            self::HIRTAM_LOCATION->value,
+            self::PVITUL_LOCATION->value,
+            self::KHANZ_LOCATION->value,
+            self::TER_LOCATION->value,
+            self::FANSALPLAINS_LOCATION->value,
+            self::KRASNUR_LOCATION->value,
+            self::TOWHAR_LOCATION->value,
+            self::GOLBAK_LOCATION->value,
+            self::FAGNA_LOCATION->value,
+            self::CRUENDO_LOCATION->value,
+            self::SNERPIIR_LOCATION->value,
+            self::TASNOBIL_LOCATION->value,
         ];
     }
 }
