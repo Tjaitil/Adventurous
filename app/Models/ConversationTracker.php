5af1c40a6977d1d8970bc62000f1b70c9452e6a3
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-11-23 22:31:35 +0100

    feat(ConversationTracker): add factory

--- app/Models/ConversationTracker.php
+++ app/Models/ConversationTracker.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -9,19 +10,23 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property int $id
  * @property string|null $conversation_option_value
- * @property int|null $user_id
+ * @property int $user_id
  * @property string|null $current_index
- *
+ * @property \Carbon\CarbonInterface|null $updated_at
+ * @property \Carbon\CarbonInterface|null $created_at
+ * @method static \Database\Factories\ConversationTrackerFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker query()
  * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker whereConversationOptionValue($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker whereCurrentIndex($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ConversationTracker whereUserId($value)
- *
  * @mixin \Eloquent
  */
 class ConversationTracker extends Model
 {
+    use HasFactory;
 }
