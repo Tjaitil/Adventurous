e9a7603306d82a95f924877ac173231c285231d2
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-12-04 23:16:06 +0100

    refactor(InventoryTrait): refactor to user_id instead of username

--- tests/Support/InventoryTrait.php
+++ tests/Support/InventoryTrait.php
@@ -9,23 +9,23 @@ trait InventoryTrait
 {
     public function insertItemToInventory(User $User, string $name, int $amount): void
     {
-        Inventory::insert(
+        Inventory::upsert(
             [
-                'username' => $User->username,
+                'user_id' => $User->id,
                 'item' => $name,
                 'amount' => $amount,
-            ],
+            ], ['user_id', 'item'],
         );
     }
 
     public function insertCurrencyToInventory(User $User, int $amount): void
     {
-        Inventory::insert(
+        Inventory::upsert(
             [
-                'username' => $User->username,
+                'user_id' => $User->id,
                 'item' => config('adventurous.currency'),
                 'amount' => $amount,
-            ],
+            ], ['user_id', 'item'],
         );
     }
 }
