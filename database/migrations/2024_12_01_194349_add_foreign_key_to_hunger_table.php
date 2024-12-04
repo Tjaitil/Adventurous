35570e75fc49e6dc235887c7f599258f96c69faf
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-12-04 23:12:41 +0100

    feat(migrations): add foreign key to user_id column

---
+++ database/migrations/2024_12_01_194349_add_foreign_key_to_hunger_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('hunger', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable(false)->change();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('hunger', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->integer('user_id')->nullable(true)->change();
+        });
+    }
+};
