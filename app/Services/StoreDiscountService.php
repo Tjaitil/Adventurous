fa3affb23440567e8e56af906dbddf18b62f53be
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-12-27 21:58:05 +0100

    refactor(StoreDiscountService): remove duplicate expression

--- app/Services/StoreDiscountService.php
+++ app/Services/StoreDiscountService.php
@@ -15,7 +15,7 @@ class StoreDiscountService
     public function getDiscount(string $storeName, User $User)
     {
         $StoreDiscount = StoreDiscount::where('store', $storeName)->first();
-        if (\is_null($StoreDiscount) || ! $StoreDiscount instanceof StoreDiscount) {
+        if (! $StoreDiscount instanceof StoreDiscount) {
             return 1.00;
         } elseif ($User->player->profiency === $StoreDiscount->profiency) {
             return $StoreDiscount->discount;
