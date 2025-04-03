f7321d7065e870bb61b25e74695452405ceb48db
Kjetil Vea Baksaas <52608380+Tjaitil@users.noreply.github.com>
2025-04-03 22:07:06 +0200

    chore: upgrade eslint configuration (#48)

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+import {
+    defineConfigWithVueTs,
+    vueTsConfigs,
+} from '@vue/eslint-config-typescript';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginVue from 'eslint-plugin-vue';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+
+export default defineConfigWithVueTs(
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
+    pluginVue.configs['flat/recommended'],
+    vueTsConfigs.strictTypeChecked,
+    skipFormatting,
+    prettierRecommended,
+    {
+        rules: {
+            'prettier/prettier': ['off'],
+        },
+    },
+);
