9edfe54767839045138483d9b39931bf0483802d
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-04-03 22:06:14 +0200

    chore: final tweaks on config

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,18 +6,13 @@ import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import pluginVue from 'eslint-plugin-vue';
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
 export default defineConfigWithVueTs(
     {
         name: 'app/files-to-lint',
         files: ['**/*.{ts,mts,tsx,vue}'],
     },
     pluginVue.configs['flat/recommended'],
-    vueTsConfigs.recommendedTypeChecked,
+    vueTsConfigs.strictTypeChecked,
     skipFormatting,
     prettierRecommended,
     {
