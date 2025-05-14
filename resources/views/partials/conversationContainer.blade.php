af92e174f069fcbca16d625f196703e8960ef712
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-05-14 19:01:37 +0200

    refactor: refactor image styling rule

--- resources/views/partials/conversationContainer.blade.php
+++ resources/views/partials/conversationContainer.blade.php
@@ -1,6 +1,6 @@
 <div id="conversation-container"
     class="invisible absolute z-20 h-[190px] rounded-sm border-4 border-primary-700 bg-orange-50 p-1 shadow-lg transition-[scale] duration-500 ease-in after:pointer-events-none after:absolute after:left-[-1px] after:top-[-1px] after:h-[calc(100%+2px)] after:w-[calc(100%+2px)] after:rounded-sm after:border-4 after:border-solid after:border-gray-950 after:content-[''] left-0 right-0">
-    <img class="cont_exit" src="{{ asset('images/exit.png') }}" />
+    <img class="cont_exit" src="{{ asset('images/exit.png') }}" width="20px" height="20px" />
     <h3 id="conversation-header" class="border-b-2 border-primary-900 py-1 text-xl"></h3>
     <div class="h-22 flex justify-center overflow-hidden">
         <img src="#" id="conversation-a" class="block h-16 w-16" />
