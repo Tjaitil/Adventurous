9d25bd0351034108448b0c61ff687a19db4e2dd6
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-05-19 22:38:43 +0200

    init events

---
+++ app/Events/InventoryUpdated.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Auth;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+
+class InventoryUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var Collection<int, Inventory>
+     */
+    public Collection $Inventory;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  Collection<int, Inventory>  $Inventory
+     */
+    public function __construct($Inventory)
+    {
+        $this->Inventory = $Inventory;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('game-state.'.Auth::user()->id),
+        ];
+    }
+}
---
+++ resources/js/bootstrap.ts
@@ -0,0 +1,52 @@
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+import axios from 'axios';
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+import Echo from 'laravel-echo';
+
+import Pusher from 'pusher-js';
+import { useInventoryStore } from './ui/stores/InventoryStore';
+import { InventoryItem } from '@/types/InventoryItem';
+import { useSkillsStore } from './ui/stores/SkillsStore';
+
+window.Pusher = Pusher;
+
+const echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+});
+echo.private(`game-state.${window.user_id.toString()}`)
+    .listen('InventoryUpdated', (e: { Inventory: InventoryItem[] }) => {
+        useInventoryStore().setInventoryItems(e.Inventory);
+    })
+    .listen('SkillsUpdated', () => {
+        useSkillsStore().setHandleXpGainedEvent(true);
+    });
+
+/** Mock values */
+window.setTimeout(async () => {
+    console.log('sending test event');
+    try {
+        const data = await axios.get('/foo');
+        console.log(data, 'data');
+    } catch (e) {
+        console.error(e);
+    }
+}, 3000);
