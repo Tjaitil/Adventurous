642ad3d18296eeb008aa036a06095e1ef2d9ca8c
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-06-01 13:31:13 +0200

    chore: call seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,8 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(ArmoryItemSeeder::class);
         $this->call(WarriorLevelsDataSeeder::class);
+
+        $this->call(TavernRecruitTypeSeeder::class);
+        $this->call(TavernRecruitSeeder::class);
     }
 }
