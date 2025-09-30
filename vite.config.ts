c3d0e7a0a67101295b775054b57b2981ee2e78ee
Kjetil Vea Baksaas <kjetil@baksaas.no>
2025-09-30 23:09:38 +0200

    build: introduce nuxt UI (#61)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import ui from '@nuxt/ui/vite';
 
 export default defineConfig({
   test: {
@@ -31,6 +32,76 @@ export default defineConfig({
           base: null,
           includeAbsolute: false,
         },
+      },
+    }),
+    ui({
+      inertia: true,
+      colorMode: false,
+      ui: {
+        button: {
+          slots: {
+            base: 'rounded px-3 py-2.5 pixelated-corners-sm',
+          },
+          variants: {
+            size: {
+              md: {
+                base: 'px-3 py-2.5',
+              },
+            },
+            color: {
+              primary: 'border-amber-900 after:content-["foo"]',
+            },
+          },
+        },
+        card: {
+          slots: {
+            root: `card relative overflow-visible after:border-primary-700 after:pointer-events-none after:absolute after:top-0 after:left-0 after:h-full after:w-full after:border-4 after:border-solid
+              before:pixelated-corners before:pointer-events-none before:absolute before:top-[-8px] before:left-[-8px] before:h-[calc(100%+16px)] before:w-[calc(100%+16px)] before:border-8 before:border-solid before:content-['']
+              `,
+          },
+          variants: {
+            variant: {
+              outline: {
+                root: 'bg-orange-100 before:border-stone-400',
+                body: 'p-4 sm:p-4',
+              },
+              soft: {
+                root: 'before:border-stone-400',
+                body: 'p-4 sm:p-4',
+              },
+              subtle: {
+                root: 'before:border-stone-400',
+                body: 'p-4 sm:p-4',
+              },
+              solid: {
+                root: 'before:border-stone-700 after:border-primary-800',
+                body: 'p-4 sm:p-4',
+              },
+            },
+          },
+        },
+        colors: {
+          primary: 'primary',
+          secondary: 'secondary',
+          tertiary: 'indigo',
+          emerald: 'emerald',
+          dark: 'indigo',
+          neutral: 'stone',
+          success: 'emerald',
+        },
+      },
+      theme: {
+        colors: [
+          'primary',
+          'secondary',
+          'tertiary',
+          'info',
+          'success',
+          'warning',
+          'error',
+          'emerald',
+          'neutral',
+        ],
       },
     }),
     laravel({
