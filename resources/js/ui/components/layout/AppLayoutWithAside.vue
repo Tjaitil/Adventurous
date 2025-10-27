9ea10fe8f28065f0b25e80cdbf3989d61b7f9f20
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-10-27 19:38:24 +0100

    feat: add layout components

---
+++ resources/js/ui/components/layout/AppLayoutWithAside.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseLayoutContainer>
+    <AppHeader class="col-span-full row-start-1" />
+    <AppSection
+      class="col-span-5 col-start-2 row-start-2 max-h-[800px] min-h-[600px] px-2 py-2"
+    >
+      <slot></slot>
+    </AppSection>
+    <aside
+      class="relative z-20 col-span-1 col-start-1 row-start-2 max-h-[800px]"
+    >
+      <slot name="aside"></slot>
+    </aside>
+    <AppFooter class="col-span-6 col-start-2 row-start-3" />
+  </BaseLayoutContainer>
+</template>
+
+<script setup lang="ts">
+import AppHeader from './AppHeader.vue';
+import AppSection from './AppSection.vue';
+import BaseLayoutContainer from './AppLayoutContainer.vue';
+import AppFooter from './AppFooter.vue';
+
+defineSlots<{
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  default(): any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  aside(): any;
+}>();
+</script>
---
+++ resources/js/ui/components/layout/AppSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+section {
+  /*background: radial-gradient(#f2e6d9 50%, #986834);*/
+  /*background: radial-gradient(#a9753d 75%, #4b341b);*/
+  background: radial-gradient(
+    var(--layoutBgColor),
+    var(--layoutBgColorShadowPerc),
+    var(--layoutBgColorShadow)
+  );
+  box-sizing: content-box;
+  /*width: 82.8%;
+    min-height: 700px;*/
+  grid-column: 2 / 3;
+  border: inset #4b341b 2px;
+  /*float: right;*/
+  text-align: center;
+  color: #392714;
+  font-weight: 200;
+  position: relative;
+}
+</style>
