dd52d16b69b0d9b63ff05fb0092c9963af17f4f7
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-10-28 19:47:58 +0100

    debug

--- app/helpers.php
+++ app/helpers.php
@@ -13,5 +13,6 @@ if (! function_exists('advResponse')) {
     function advResponse($data = [], $status = Response::HTTP_OK, $headers = [])
     {
         return new AdvResponse($data, $status, $headers);
+        var_dump('advResponse helper called');
     }
 }
