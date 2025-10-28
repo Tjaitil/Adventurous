c95e7792c0ff5912f1980713b5e3362d9faa0414
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-10-28 19:26:36 +0100

    chore: test ci

--- app/Events/SkillsUpdated.php
+++ app/Events/SkillsUpdated.php
@@ -16,10 +16,7 @@ class SkillsUpdated implements ShouldBroadcast
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
-        // this should only run laravel ci
-    }
+    public function __construct() {}
 
     /**
      * Get the channels the event should broadcast on.
