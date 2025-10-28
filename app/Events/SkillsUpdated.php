97dcbc42116083ebd88e213d0765daeb1bb2e0fc
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-10-28 18:48:40 +0100

    chore: this shouldnt run the node.js.yml

--- app/Events/SkillsUpdated.php
+++ app/Events/SkillsUpdated.php
@@ -16,10 +16,7 @@ class SkillsUpdated implements ShouldBroadcast
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
-        // Test
-    }
+    public function __construct() {}
 
     /**
      * Get the channels the event should broadcast on.
