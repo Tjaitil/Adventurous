e05b05fc0c8d816954baf32ca0c4cc26821a4b94
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-12-30 23:47:53 +0100

    make phpstan understand deprecated controller/model class

--- libs/GlobalModel.php
+++ libs/GlobalModel.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Simple wrapper to keep phpstan happy while removing outdated base model class.
+ * @deprecated Outdated base model class
+ */
 class model extends \App\libs\model
 {
     public function __construct($table = null)
