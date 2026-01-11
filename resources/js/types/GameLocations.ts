96a9964354b2563678a61bc33a0a47e2801c2ad4
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2026-01-11 23:44:58 +0100

    feat: add GameLocation type

---
+++ resources/js/types/GameLocations.ts
@@ -0,0 +1,13 @@
+export type GameLocation =
+  | 'hirtam'
+  | 'pvitul'
+  | 'khanz'
+  | 'ter'
+  | 'fansal_plains'
+  | 'krasnur'
+  | 'towhar'
+  | 'golbak'
+  | 'fagna'
+  | 'cruendo'
+  | 'snerpiir'
+  | 'tasnobil';
