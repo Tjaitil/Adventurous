5bfec0f615b979258e5d0549dc64bfd248232da2
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2026-02-05 20:50:27 +0100

    feat(TraderAssignmentType): cast type to enum

---
+++ app/Enums/TraderAssignmentTypeName.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum TraderAssignmentTypeName: string
+{
+    case SMALL = 'small';
+    case MEDIUM = 'medium';
+    case LARGE = 'large';
+    case FAVOR = 'favor';
+}
--- app/Models/TraderAssignmentType.php
+++ app/Models/TraderAssignmentType.php
@@ -2,21 +2,46 @@
 
 namespace App\Models;
 
+use App\Enums\TraderAssignmentTypeName;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property string $type
- * @property int $xp_per_cargo
- * @property int $xp_finished
- * @property int $xp_started
+ * @property int $id
+ * @property TraderAssignmentTypeName $type
+ * @property float $xp_per_cargo
  * @property int $item_reward_amount
- * @property int $currency_reward_amount
+ * @property int $xp_finished
  * @property float $diplomacy_percentage
+ * @property int $currency_reward_amount
  * @property int $required_level
+ * @property int $xp_started
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereCurrencyRewardAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereDiplomacyPercentage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereItemRewardAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereRequiredLevel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereXpFinished($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereXpPerCargo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|TraderAssignmentType whereXpStarted($value)
+ *
  * @mixin \Eloquent
  */
 class TraderAssignmentType extends Model
 {
     public $timestamps = false;
-    public static $FAVOR_TYPE = "favor";
+
+    public static $FAVOR_TYPE = 'favor';
+
+    public function casts(): array
+    {
+        return [
+            'diplomacy_percentage' => 'float',
+            'type' => TraderAssignmentTypeName::class,
+        ];
+    }
 }
