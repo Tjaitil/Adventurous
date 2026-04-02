e02c7e8547e4f045dd902fc38560dbe9e6e943ea
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2026-04-02 20:38:02 +0200

    move table css to component

--- resources/views/components/baseTable.blade.php
+++ resources/views/components/baseTable.blade.php
@@ -4,3 +4,29 @@
         {{ $body }}
     </tbody>
 </table>
+<style>
+
+section table td:not(.custom-td),
+th,
+section caption,
+aside caption,
+aside td {
+  padding: 8px;
+  background-color: #f2e6d9;
+}
+
+section tbody tr:not(.sectionTableNoPseudo, .custom-tr):after {
+  content: '';
+  position: absolute;
+  left: 10%;
+  width: 80%;
+  height: 2px;
+  background-color: #5f4121;
+}
+section table:not(#game_messages, .custom-table),
+  border: 8px ridge #5f4121;
+  border-radius: 5px;
+  position: relative;
+}
+
+</style>
