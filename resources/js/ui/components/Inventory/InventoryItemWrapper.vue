b911054a72233fe4ba7f688a8087c8b55697241f
Kjetil Vea Baksaas <kjetil@baksaas.no>
2026-04-12 17:16:18 +0200

    refactor: Unify item components (#107)

--- resources/js/ui/components/Inventory/InventoryItemWrapper.vue
+++ resources/js/ui/components/Inventory/InventoryItemWrapper.vue
@@ -1,52 +1,26 @@
 <template>
-  <div class="inventory_item">
-    <figure
-      @click="handleEvent($event, item.item)"
-      v-on="
-        inventoryStore.inventoryItemEvent === null
-          ? {
-              mouseleave: () => itemTitle.hide(),
-              mouseover: e => itemTitle.show(e),
-            }
-          : {}
-      "
-    >
-      <img :src="'/images/' + item.item + '.png'" alt="inventory-item" />
-      <figcaption class="tooltip">
-        <span class="tooltip_item">{{ jsUcWords(item.item) }}</span>
-        <br />
-        x {{ item.amount }}
-      </figcaption>
-    </figure>
-    <span class="item_amount">
-      {{ itemAmountWithDelimiter }}
-    </span>
-  </div>
+  <BaseItem
+    class="inventory_item"
+    :item="item.item"
+    :amount="item.amount"
+    @click="handleEvent($event, item.item)"
+  />
 </template>
 
 <script setup lang="ts">
 import type { InventoryItem } from '@/types/InventoryItem';
-import { jsUcWords } from '@/utilities/uppercase';
-import { computed } from 'vue';
-import { itemTitle } from '@/utilities/itemTitle';
 import { useInventoryStore } from '@/ui/stores/InventoryStore';
 import stockpileModule from '@/buildingScripts/stockpile';
+import BaseItem from '../base/BaseItem.vue';
 
 interface Props {
   item: InventoryItem;
 }
 
-const props = defineProps<Props>();
+defineProps<Props>();
 
 const inventoryStore = useInventoryStore();
 
-const itemAmountWithDelimiter = computed(() => {
-  if (props.item.amount > 1000) {
-    return `${(props.item.amount / 1000).toFixed(1)} k`;
-  }
-
-  return props.item.amount;
-});
 const handleEvent = (e: Event, itemName: string) => {
   if (inventoryStore.inventoryItemEvent === null) {
     return;
