5f5948f1d5de5d79d9594f08194f1e9431136302
Kjetil Vea Baksaas <kjetil@baksaas.no>
2026-05-10 17:43:19 +0200

    fix(InputHandler): missing type import (#115)

--- resources/js/clientScripts/inputHandler.ts
+++ resources/js/clientScripts/inputHandler.ts
@@ -5,6 +5,7 @@ import { tutorial } from './tutorial';
 import { itemTitle } from '../utilities/itemTitle';
 import { Game } from '../advclient';
 import { GameLogger } from '../utilities/GameLogger';
+import type { BuildingName } from '@/types/Building';
 import { isVuePage, type VuePage } from '@/types/Building';
 import { GamePieces } from './gamePieces';
 import type { Building } from '../gamepieces/Building';
