1c9333581892ec852d6d4accafdeb8ed199c8b26
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2026-07-02 17:46:29 +0200

    feat: upgrade config to inertia v3

--- config/inertia.php
+++ config/inertia.php
@@ -45,52 +45,17 @@ return [
     |
     */
 
-    'ensure_pages_exist' => true,
-
-    'page_paths' => [
-
-        resource_path('js/ui/Pages'),
-
-    ],
-
-    'page_extensions' => [
-
-        'js',
-        'jsx',
-        'svelte',
-        'ts',
-        'tsx',
-        'vue',
-
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Testing
-    |--------------------------------------------------------------------------
-    |
-    | The values described here are used to locate Inertia components on the
-    | filesystem. For instance, when using `assertInertia`, the assertion
-    | attempts to locate the component as a file relative to any of the
-    | paths AND with any of the extensions specified here.
-    |
-    | Note: In a future release, the `page_paths` and `page_extensions`
-    | options below will be removed. The root-level options above
-    | will be used for both application and testing purposes.
-    |
-    */
-
-    'testing' => [
+    'pages' => [
 
         'ensure_pages_exist' => true,
 
-        'page_paths' => [
+        'paths' => [
 
             resource_path('js/ui/Pages'),
 
         ],
 
-        'page_extensions' => [
+        'extensions' => [
 
             'js',
             'jsx',
