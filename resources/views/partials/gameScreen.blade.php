a56e515c84687c0c066134d78d881d0321362f2f
Kjetil Vea Baksaas <kjetil@baksaas.no>
2026-07-24 21:09:03 +0200

    chore: remove old gameScreen partial

--- resources/views/partials/gameScreen.blade.php
+++
@@ -1,9 +0,0 @@
-    <div id="canvas-border"
-        class="absolute z-7 top-[-6px] w-[700px] h-[400px] border-8 border-primary-700 after:pointer-events-none after:absolute after:left-[-1px] after:top-[-1px] after:h-[calc(100%+2px)] after:w-[calc(100%+2px)] after:border-4 after:border-solid after:border-gray-950/60 after:content-[''] pixelated-corners">
-    </div>
-    <x-client.canvas id="game_canvas" />
-    <x-client.canvas id="game_canvas2" />
-    <x-client.canvas id="game_canvas3" />
-    <x-client.canvas id="game_canvas4" />
-    <x-client.canvas id="text_canvas" />
-    <x-client.canvas id="hud_canvas" />
