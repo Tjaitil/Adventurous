21a9684b485cb3b739e27137f0a42b50c23b5d0d
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2026-08-04 23:32:33 +0200

    refactor(InventoryContainer): drop InventoryItemWrapper for BaseItem

--- resources/js/ui/components/Inventory/InventoryContainer.vue
+++ resources/js/ui/components/Inventory/InventoryContainer.vue
@@ -4,9 +4,10 @@
     variant="soft"
     class="relative z-20 float-right mt-0 h-[600px] w-full text-white"
   >
-    <BaseLoading :is-loading>
+    <BaseLoading v-if="isLoading" :is-loading> </BaseLoading>
+    <template v-else>
       <p class="mb-4">
-        Inventory
+        {{ $t('Inventory') }}
         <span
           id="inventory-status"
           :class="{
@@ -16,12 +17,22 @@
           {{ inventoryStore.inventoryItems.length }} / {{ 18 }}
         </span>
       </p>
-      <InventoryItemWrapper
-        v-for="item in inventoryStore.inventoryItems"
-        :key="item.id"
-        :item="item"
-      />
-    </BaseLoading>
+      <div
+        class="grid-tem grid grid-cols-[repeat(auto-fill,minmax(88px,1fr))] grid-rows-[minmax(90px,1fr)] gap-y-4"
+      >
+        <BaseItem
+          v-for="item in inventoryStore.inventoryItems"
+          ref="inventoryItemRefs"
+          :key="item.id"
+          :item="item.item"
+          :amount="item.amount"
+          :show-amount="true"
+          class="inventory_item"
+          :disable-tooltip="!inventoryStore.showTooltips"
+          @click="handleEvent($event, item.item)"
+        />
+      </div>
+    </template>
   </UCard>
 </template>
 
@@ -29,14 +40,13 @@
 import { CustomFetchApi } from '@/CustomFetchApi';
 import type { InventoryItem } from '@/types/InventoryItem';
 import { ref } from 'vue';
-import InventoryItemWrapper from './InventoryItemWrapper.vue';
 import BaseLoading from '@/ui/components/base/BaseLoading.vue';
 import { useInventoryStore } from '@/ui/stores/InventoryStore';
 import { itemPrices } from '@/clientScripts/inventory';
 import { reportCatchError } from '@/base/ErrorHandler';
+import BaseItem from '../base/BaseItem.vue';
 
 const inventoryStore = useInventoryStore();
-
 const isLoading = ref(true);
 
 const getInventory = async () => {
@@ -45,19 +55,44 @@ const getInventory = async () => {
     const response =
       await CustomFetchApi.get<InventoryItem[]>('/inventory/items');
     inventoryStore.inventoryItems = response.data;
-
-    isLoading.value = false;
     inventoryStore.setShouldUpdateInventory(false);
     itemPrices.get();
   } catch (e) {
     reportCatchError(e);
+  } finally {
+    isLoading.value = false;
   }
 };
-getInventory();
+void getInventory();
 
 inventoryStore.$subscribe((mutation, state) => {
   if (state.shouldUpdateInventory && !isLoading.value) {
-    getInventory();
+    void getInventory();
   }
 });
+
+const handleEvent = (e: Event, itemName: string): void => {
+  inventoryStore.handleItemClick(e, itemName);
+};
 </script>
+
+<style>
+.inventory_item {
+  display: inline-block;
+  max-width: 104px;
+  min-width: 60px;
+  width: 30%;
+  height: 90px;
+  position: relative;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.inventory_item figure {
+  position: relative;
+}
+</style>
