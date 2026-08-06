a5016c8b425f9ce5969056af2505b98db7eea892
Kjetil Vea Baksaas <kjetil@baksaas.no>
2026-08-06 22:02:33 +0200

    refactor: remove old code (#155)

--- resources/js/ui/main.ts
+++ resources/js/ui/main.ts
@@ -1,14 +1,5 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import AppVue from './components/App.vue';
-import SkillInfoList from './components/skills/SkillInfoList.vue';
 import { createI18n } from 'vue-i18n';
 import { getLanguageBundle } from './localeSetup';
-import InventoryContainer from './components/Inventory/InventoryContainer.vue';
-import { initErrorHandler } from '@/base/ErrorHandler';
-import ui from '@nuxt/ui/vue-plugin';
-import ConversationContainer from './components/ConversationContainer.vue';
-import CrashScreen from './components/CrashScreen.vue';
 
 export const i18n = createI18n({
   locale: 'en',
@@ -17,27 +8,4 @@ export const i18n = createI18n({
   missingWarn: false,
   silentFallbackWarn: true,
   silentTranslationWarn: true,
-});
-
-export const pinia = createPinia();
-
-const components = {
-  SkillInfoList,
-  ConversationContainer,
-  InventoryContainer,
-  CrashScreen,
-};
-const ErrorHandler = initErrorHandler();
-
-document.querySelectorAll('.vue-app').forEach(element => {
-  const app = createApp({ AppVue, components });
-
-  app.config.errorHandler = async (err, vm, info) => {
-    const errorToLog = err + ' ' + JSON.stringify(vm) + ' ' + info;
-    const stack = err instanceof Error ? err.stack : undefined;
-
-    ErrorHandler.logError({ text: errorToLog, stack });
-  };
-
-  app.use(pinia).use(ui).use(i18n).mount(element);
 });
