638f4c868752c45a40a35876d7da33d4a8924ac6
Kjetil Vea Baksaas <kjetil@baksaas.no>
2026-08-06 21:30:12 +0200

    chore: remove unused blade files (#142)

--- resources/views/components/client/canvas.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['id' => ''])
-<canvas id="{{ $id }}" width="700" height="400" class="absolute left-0 image-crisp">
-</canvas>
--- resources/views/components/client/logModal.blade.php
+++
@@ -1,6 +0,0 @@
-<div id="log-modal"
-    class="bg-primary-200 w-full h-0 border-primary-900 border-4 fixed bottom-0 right-0 opacity-0 overflow-hidden overflow-x-scroll z-50 flex justify-center items-center max-w-[500px]">
-    <div class="flex items-center">
-        <p class="my-0 h-full"></p>
-    </div>
-</div>
