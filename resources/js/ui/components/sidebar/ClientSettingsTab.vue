9bdc71def0d66aada9b95d57676aec0955065e5b
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2026-09-12 22:58:33 +0200

    refactor(ClientSettingsTab): move to simpler implementation

--- resources/js/ui/components/sidebar/ClientSettingsTab.vue
+++ resources/js/ui/components/sidebar/ClientSettingsTab.vue
@@ -1,77 +1,48 @@
 <template>
-  <div id="client_settings_container">
-    <label class="label-container">
-      Minimal Controls? <br />
-      This will remove "P" and "C" section
-      <input
-        id="client-settings-minimal-control"
-        type="checkbox"
-        name="client-settings-minimal-control"
-      />
-      <span class="checkmark"></span>
-    </label>
+  <div class="space-y-2">
+    <USwitch
+      v-model="settingsForm.minimalControls"
+      :ui="{
+        label: 'text-white font-bold',
+        description: 'text-white',
+      }"
+      :label="t('Minimal Controls')"
+      :description="t('This will remove P and C section')"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-const set = (settingName: string) => {
-  const targetSetting = list.find(setting => setting.name === settingName);
-  // Throw error if settingName doesn't exists
-  if (!targetSetting) return false;
+import { onMounted, reactive, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
 
-  switch (targetSetting.type) {
-    case 'switch':
-      targetSetting.value = !targetSetting.value;
-      break;
-    default:
-      break;
-  }
-  targetSetting.update();
-};
-const checkLocalStorage = () => {
-  list.forEach(setting => {
-    const storedValue = localStorage.getItem(setting.name);
-    if (storedValue) setting.setup(storedValue);
-    else setting.setup();
-  });
-};
-const list = [
-  {
-    name: 'minimalControls',
-    type: 'switch',
-    value: false,
-    targetElement: null,
-    setup(storedValue: string | null = null) {
-      // Check for storedValue
-      if (storedValue !== null)
-        this.value = storedValue != 'false' ? true : false;
-      this.targetElement = document.getElementById(
-        'client-settings-minimal-control',
-      );
-      this.targetElement.checked = this.value;
-      this.targetElement.addEventListener('change', () =>
-        set('minimalControls'),
-      );
-      this.update();
-    },
-    update() {
-      const controlPara = [
-        ...document.querySelectorAll('.extendedControls'),
-      ] as HTMLElement[];
-      let style;
-      if (this.value) {
-        style = 'hidden';
-      } else {
-        style = 'visible';
+const { t } = useI18n();
+
+const settingsForm = reactive({
+  minimalControls: false,
+});
+
+const LOCAL_STORAGE_KEY = 'advclient_client_settings';
+const syncFromLocalStorage = () => {
+  const storedSettings = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (storedSettings) {
+    const parsedSettings = JSON.parse(storedSettings);
+    Object.keys(parsedSettings).forEach(key => {
+      if (key in settingsForm) {
+        settingsForm[key] = parsedSettings[key];
       }
-      controlPara.forEach(element => (element.style.visibility = style));
-      localStorage.setItem('minimalControls', this.value.toString());
-    },
+    });
+  }
+};
+
+watch(
+  () => settingsForm,
+  newValue => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newValue));
   },
-];
+);
 
 onMounted(() => {
-  checkLocalStorage();
+  syncFromLocalStorage();
 });
 </script>
